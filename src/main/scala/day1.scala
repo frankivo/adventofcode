71174f2dfbc3cca63fcb8d7f5645e58b4e187edb
Frank Oosterhuis <frank@scriptzone.nl>
2021-12-07 23:53:49 +0100

    day 1, part 2

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,15 +1,28 @@
 object day1 {
 
   @main def main(): Unit = {
-    println(day1())
+    println(s"Part1: ${part1()}")
+    println(s"Part2: ${part2()}")
   }
 
-  def day1(): Int = {
-    scala.io.Source.fromResource("day1.txt")
-      .getLines()
-      .map(_.toInt)
+  def part1(): Int = {
+    getData
       .sliding(2, 1)
       .count(x => x(0) < x(1))
   }
 
+  def part2(): Int = {
+    getData
+      .sliding(3, 1)
+      .map(_.sum)
+      .sliding(2, 1)
+      .count(x => x(0) < x(1))
+  }
+
+  def getData: Seq[Int] = {
+    scala.io.Source.fromResource("day1.txt")
+      .getLines()
+      .map(_.toInt)
+      .toSeq
+  }
 }
