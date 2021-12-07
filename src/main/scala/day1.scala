dc4a08fd4d3fd09c061f0582ba8bfbd700327df4
Frank Oosterhuis <frank@scriptzone.nl>
2021-12-07 22:50:33 +0100

    cleanup

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,12 +1,15 @@
 object day1 {
 
-  @main def main() : Unit = {
-    Seq(scala.io.Source.fromResource("day1.txt")
+  @main def main(): Unit = {
+    println(day1())
+  }
+
+  def day1(): Int = {
+    scala.io.Source.fromResource("day1.txt")
       .getLines()
       .map(_.toInt)
       .sliding(2, 1)
       .count(x => x(0) < x(1))
-    )      .foreach(println)
   }
 
 }
