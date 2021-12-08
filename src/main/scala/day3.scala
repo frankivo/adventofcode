a7bafa1091f894fd3bac166597708d345a5b515b
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-08 11:07:51 +0100

    get significant

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,7 +1,16 @@
 object day3 {
   def main(args: Array[String]): Unit = {
-    println(getData.length)
+    val size = getData.length
+
+    val significant = getData
+      .map(_.toCharArray)
+      .foldLeft(0) { (sum, i) => sum + i(0).compare('0') }
+
+    val gammaRate1 = getSig(significant, size)
+    println(gammaRate1)
   }
+
+  def getSig(num: Long, size: Long): Char = if (num > (size / 2)) '1' else '0'
 
   def getData: Iterator[String] = scala.io.Source.fromResource("day3.txt").getLines()
 }
