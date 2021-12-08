388f3416c9c6557456b30dcf4003a512d1b153d3
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-08 12:59:09 +0100

    part1

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,16 +1,29 @@
 object day3 {
   def main(args: Array[String]): Unit = {
-    val size = getData.length
-
-    val significant = getData
-      .map(_.toCharArray)
-      .foldLeft(0) { (sum, i) => sum + i(0).compare('0') }
-
-    val gammaRate1 = getSig(significant, size)
-    println(gammaRate1)
+    println(s"Part 1: ${part1()}")
   }
 
-  def getSig(num: Long, size: Long): Char = if (num > (size / 2)) '1' else '0'
+  private def getData: Iterator[String] = scala.io.Source.fromResource("day3.txt").getLines()
 
-  def getData: Iterator[String] = scala.io.Source.fromResource("day3.txt").getLines()
+  private val IS_SIGNIFICANT = getData.length / 2
+  private val BIT_COUNT = getData.next().length
+
+  def part1(): Long = {
+    val significant = getData
+      .map(_.toCharArray)
+      .foldLeft(Seq.fill(BIT_COUNT)(0)) {
+        (sum, cur) => {
+          sum
+            .indices
+            .map(i => sum(i) + cur(i).compare('0'))
+        }
+      }
+
+    val gamma = significant
+      .map(s => if (s > IS_SIGNIFICANT) '1' else '0')
+      .mkString
+    val epsilon = gamma.map(c => if (c == '1') '0' else '1').mkString
+
+    Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)
+  }
 }
