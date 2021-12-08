1bf716854e9f72e24dda512dd01ba91645095171
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-08 09:13:49 +0100

    immutable

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -3,31 +3,31 @@ object day2 {
     println(s"Day2: ${part1()}")
   }
 
-  def part1(): Long = {
-    var pos = 0
-    var depth = 0
-
-    readFile()
-      .foreach(c => {
-        c.action match {
-          case "forward" => pos += c.positions
-          case "down" => depth += c.positions
-          case "up" => depth -= c.positions
-        }
-      })
-
-    pos * depth
-  }
-
   case class command(action: String, positions: Int)
 
-  def readFile(): Seq[command] = {
+  case class coordinates(pos: Int, depth: Int)
+
+  def part1(): Long = {
+    val finalCoordinates = readFile()
+      .foldLeft(coordinates(0, 0)) {
+        (cur, cmd) => {
+          cmd.action match {
+            case "forward" => coordinates(cur.pos + cmd.positions, cur.depth)
+            case "down" => coordinates(cur.pos - cmd.positions, cur.depth)
+            case "up" => coordinates(cur.pos, cur.depth - cmd.positions)
+          }
+        }
+      }
+
+    finalCoordinates.depth * finalCoordinates.pos
+  }
+
+  def readFile(): Iterator[command] = {
     scala.io.Source.fromResource("day2.txt")
       .getLines()
       .map(l => {
         val data = l.split(" ")
         command(data(0), data(1).toInt)
       })
-      .toSeq
   }
 }
