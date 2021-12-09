07a1559cc6684b3fe73fbf8b70851388ad3454cb
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-09 09:10:31 +0100

    part2

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,6 +1,7 @@
 object day3 {
   def main(args: Array[String]): Unit = {
     println(s"Part 1: ${part1()}")
+    println(s"Part 2: ${part2()}")
   }
 
   private def getData: Iterator[String] = scala.io.Source.fromResource("day3.txt").getLines()
@@ -26,4 +27,47 @@ object day3 {
 
     Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)
   }
+
+  def part2(): Long = oxygen * scrubbber
+
+  def oxygen: Int = {
+    var data = getData.toSeq
+    var step = 0
+    while data.length > 2
+    do {
+      data = reduce1(data, step)
+      step += 1
+    }
+    data = data.filter(_.last == '1')
+    Integer.parseInt(data.head, 2)
+  }
+
+  def scrubbber: Int = {
+    var data = getData.toSeq
+    var step = 0
+    while data.length > 2
+    do {
+      data = reduce2(data, step)
+      step += 1
+      println(data)
+    }
+    data = data.filter(_.last == '0')
+
+    Integer.parseInt(data.head, 2)
+  }
+
+  def reduce1(data: Seq[String], step: Int): Seq[String] = {
+    data
+      .groupBy(r => r.charAt(step))
+      .max((a, b) => a._2.length.compare(b._2.length))
+      ._2
+  }
+
+  def reduce2(data: Seq[String], step: Int): Seq[String] = {
+    data
+      .groupBy(r => r.charAt(step))
+      .min((a, b) => a._2.length.compare(b._2.length))
+      ._2
+  }
+
 }
