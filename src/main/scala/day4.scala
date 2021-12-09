967ee5af33edc39c1a4be12629d9f9f43e77baf4
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-09 23:19:14 +0100

    part 2 complete

--- src/main/scala/day4.scala
+++ src/main/scala/day4.scala
@@ -11,30 +11,64 @@ object day4 {
     def update(draw: Int): board = {
       board(data.map(l => l.map(f => if (f.num == draw) field(f.num, true) else f)))
     }
+
+    def score: Long = data.flatten.filterNot(_.hit).map(_.num).sum
   }
 
   def main(args: Array[String]): Unit = {
+    part1()
+    part2()
+  }
+
+  def part1(): Unit = {
     var brds = boards.toSeq
     var winDraw = -1
 
+    val drws = draws.iterator
+
     while findWin(brds).isEmpty
     do {
-      val draw = draws.next()
+      val draw = drws.next()
       brds = brds.map(b => b.update(draw))
       winDraw = draw
     }
 
-    val sum = countWin(findWin(brds).getOrElse(throw new Exception("No win found")))
+    val sum = findWin(brds).getOrElse(throw new Exception("No win found")).score
     println(s"Board sum: $sum")
     println(s"Winning draw: $winDraw")
     println(s"Answer: ${sum * winDraw}")
   }
 
+  def part2(): Unit = {
+    var brds = boards.toSeq
+
+    val finalWin = draws
+      .flatMap(d => {
+        val games = brds.map(_.update(d))
+        val ret = {
+          if (countWins(games) > 0)
+            Some((d, games.find(_.isWin).get))
+          else
+            None
+        }
+        brds = games.filterNot(_.isWin)
+        ret
+      })
+      .last
+
+    val sum = finalWin._2.score
+    val draw = finalWin._1
+
+    println(s"Board sum: $sum")
+    println(s"Winning draw: ${finalWin._1}")
+    println(s"Answer: ${sum * draw}")
+  }
+
   def allData(): Iterator[String] = scala.io.Source.fromResource("day4.txt").getLines()
 
-  val draws: Iterator[Int] = allData().next().split(",").map(_.toInt).iterator
+  val draws: Seq[Int] = allData().next().split(",").map(_.toInt)
 
-  val boards: Iterator[board] = {
+  def boards: Iterator[board] = {
     allData()
       .drop(2)
       .filterNot(_.isEmpty)
@@ -51,5 +85,5 @@ object day4 {
 
   def findWin(data: Seq[board]): Option[board] = data.find(_.isWin)
 
-  def countWin(brd: board): Long = brd.data.flatten.filterNot(_.hit).map(_.num).sum
+  def countWins(data: Seq[board]): Long = data.count(_.isWin)
 }
