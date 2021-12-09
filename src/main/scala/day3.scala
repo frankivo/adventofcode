68d29444a3588ba234b980026cfadca950828e21
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-09 12:26:03 +0100

    optimize

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -28,39 +28,32 @@ object day3 {
     Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)
   }
 
-  def part2(): Long = oxygen * scrubbber
+  def part2(): Long = {
+    val oxygen = step('1')
+    val scrubbber = step('0')
+    oxygen * scrubbber
+  }
 
-  def oxygen: Int = {
+  def step(char: Char): Int = {
     var data = getData.toSeq
     var step = 0
     while data.length > 1
     do {
-      data = reduce1(data, step, '1', 1)
+      data = reduce(data, step, char)
       step += 1
     }
     Integer.parseInt(data.head, 2)
   }
 
-  def scrubbber: Int = {
-    var data = getData.toSeq
-    var step = 0
-    while data.length > 1
-    do {
-      data = reduce1(data, step, '0', -1)
-      step += 1
-    }
+  def reduce(data: Seq[String], step: Int, char: Char): Seq[String] = {
+    val multiplier = if (char == '1') 1 else -1
 
-    Integer.parseInt(data.head, 2)
-  }
-
-  def reduce1(data: Seq[String], step: Int, char: Char, minMax: Int): Seq[String] = {
     val grouped = data
       .groupBy(r => r.charAt(step))
 
     if (grouped.head._2.length == grouped.last._2.length)
       grouped.filter(_._1 == char).head._2
-    else grouped.max((a, b) => a._2.length.compare(b._2.length) * minMax)
+    else grouped.max((a, b) => a._2.length.compare(b._2.length) * multiplier)
       ._2
   }
-
 }
