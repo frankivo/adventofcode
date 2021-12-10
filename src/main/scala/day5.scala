ca4603478b2949a2fab68e34160121de258df4e0
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-10 14:40:30 +0100

    cleanup

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -1,16 +1,17 @@
 object day5 {
   def main(args: Array[String]): Unit = {
-    data
+    getLines
       .foreach(println)
   }
 
   case class line(start: (Int, Int), end: (Int, Int))
 
-  def data(): Iterator[line] = {
-    val alldata = scala.io.Source.fromResource("day5.txt").getLines().mkString(" ")
-    """(\d)+""".r.findAllMatchIn(alldata)
+  def getLines: Iterator[line] = {
+    """(\d)+""".r.findAllMatchIn(readInput())
       .map(_.matched.toInt)
       .sliding(4, 4)
       .map(m => line((m(0), m(1)), (m(2), m(3))))
   }
+
+  def readInput(): String = scala.io.Source.fromResource("day5.txt").getLines().mkString(" ")
 }
