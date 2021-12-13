629ba9b5b4df5a269a27ee3591712860258db373
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-13 15:11:20 +0100

    part2

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -1,18 +1,45 @@
 object day6 {
   def main(args: Array[String]): Unit = {
     println(s"Part1: ${bruteForce(80)}")
+    println(s"Part2: ${countFish(256)}")
   }
 
   def bruteForce(days: Int): Long = {
     (0 until days)
       .foldLeft(lanternfish) {
-        (cur, step) => {
+        (cur, _) => {
           val countDown = cur.map(_ - 1)
           (countDown ++ Seq.fill(countDown.count(_ == -1))(8))
             .map(f => if (f == -1) 6 else f)
         }
       }
       .length
+  }
+
+  def countFish(days: Int): Long = {
+    val init: Seq[Long] = (0 to 8).map(i => lanternfish.count(_ == i))
+
+    (0 until days)
+      .foldLeft(init) {
+        (cur, _) => {
+          Seq(
+            cur(1),
+            cur(2),
+            cur(3),
+            cur(4),
+            cur(5),
+            cur(6),
+            cur(7) + cur.head,
+            cur(8),
+            cur.head
+          ).toIndexedSeq
+        }
+      }
+      .sum
+  }
+
+  def magic(data: Seq[Int]): Unit = {
+
   }
 
   def lanternfish: Seq[Int] = {
