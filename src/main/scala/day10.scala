22e83ad28efc19ece7f397413e1529cb5b519917
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-16 23:37:40 +0100

    part2

--- src/main/scala/day10.scala
+++ src/main/scala/day10.scala
@@ -1,30 +1,43 @@
 object day10 {
   def main(args: Array[String]): Unit = {
     println(s"Part 1: ${part1()}")
-    //    println(s"Part 2: ${part2()}")
+    println(s"Part 2: ${part2()}")
   }
 
-  case class pair(left: Char, right: Char, score: Int)
+  case class pair(left: Char, right: Char, score1: Int, score2: Int)
 
   def part1(): Int = {
-    data.flatMap(line => {
-      line
-        .foldLeft("")({
-          (store, cur) =>
-            if (!cur.isLeft && cur.findPair.left == store.last)
-              store.dropRight(1)
-            else
-              store + cur
-        })
-        .filterNot(_.isLeft)
-        .headOption
-    })
+    data
+      .map(readLine)
+      .flatMap(line => line.filterNot(_.isLeft).headOption)
       .map(_.findPair)
-      .map(_.score)
+      .map(_.score1)
       .sum
   }
 
-  def part2(): Int = ???
+  def part2(): Long = {
+    val sums = data
+      .map(readLine)
+      .filterNot(line => line.exists(!_.isLeft))
+      .map(_.reverse)
+      .map(_.map(_.findPair.score2))
+      .map(_.foldLeft(0L) { (total, score) => (total * 5) + score })
+      .sorted
+
+    val index = sums.length / 2
+    sums.drop(index).dropRight(index).head
+  }
+
+  def readLine(line: String): String = {
+    line
+      .foldLeft("")({
+        (store, cur) =>
+          if (!cur.isLeft && cur.findPair.left == store.last)
+            store.dropRight(1)
+          else
+            store + cur
+      })
+  }
 
   extension (c: Char) {
     def isLeft: Boolean = charPairs.exists(_.left == c)
@@ -32,20 +45,14 @@ object day10 {
     def findPair: pair = Seq(charPairs.find(_.left == c), charPairs.find(_.right == c)).flatten.head
   }
 
-  val scoringTable: Map[Char, Int] = Map(
-    ')' -> 3,
-    ']' -> 57,
-    '}' -> 1197,
-    '>' -> 25137,
-  )
-
   val charPairs: Seq[pair] = {
-    "()[]{}<>"
-      .sliding(2, 2)
-      .map(c => pair(c.head, c.last, scoringTable(c.last)))
-      .toSeq
+    Seq(
+      pair('(', ')', 3, 1),
+      pair('[', ']', 57, 2),
+      pair('{', '}', 1197, 3),
+      pair('<', '>', 25137, 4),
+    )
   }
 
-  val data: Iterator[String] = io.Source.fromResource("day10.txt").getLines()
-
+  val data: Seq[String] = io.Source.fromResource("day10.txt").getLines().toSeq
 }
