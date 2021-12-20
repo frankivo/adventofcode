86b2e07e31af210ffc8cb18c834e9a11016448ff
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-20 12:20:26 +0100

    step 1

--- src/main/scala/day13.scala
+++ src/main/scala/day13.scala
@@ -4,29 +4,45 @@ object day13 {
     println(s"Part 2: ${part2()}")
   }
 
-  def part1(): Int = {
-    coordinates.take(5).foreach(println)
-    folds.foreach(println)
-    0
-  }
+  def part1(): Int = coordinates.foldLine(folds.head).size
 
   def part2(): Int = 0
 
-  val data: Seq[String] = io.Source.fromResource("day13.txt").getLines().toSeq
+  case class Coordinate(col: Int, row: Int)
 
-  val coordinates: Seq[(Int, Int)] = {
+  type Fold = (Char, Int)
+
+  type Paper = Set[Coordinate]
+
+  extension (paper: Paper) {
+    def foldLine(fold: Fold): Paper = {
+      paper.map(c => {
+        if (fold._1 == 'y' && c.row > fold._2)
+          Coordinate(c.col, fold._2 - (c.row - fold._2))
+        else if (fold._1 == 'x' && c.col > fold._2)
+          Coordinate(fold._2 - (c.col - fold._2), c.row)
+        else
+          Coordinate(c.col, c.row)
+      })
+    }
+  }
+
+  lazy val coordinates: Paper = {
     data
       .filter(_.contains(','))
       .map(_.split(','))
-      .map(c => (c.head.toInt, c.last.toInt))
+      .map(c => Coordinate(c.head.toInt, c.last.toInt))
+      .toSet
   }
 
-  val folds: Seq[(String, Int)] = {
+  lazy val folds: Seq[Fold] = {
     data
       .filter(_.contains('='))
       .map(_.split(' '))
       .map(_.last)
       .map(_.split('='))
-      .map(c => (c.head, c.last.toInt))
+      .map(c => (c.head.head, c.last.toInt))
   }
+
+  lazy val data: Seq[String] = io.Source.fromResource("day13.txt").getLines().toSeq
 }
