e3e4c83c341465ea824d38b31894a837bb3b97ba
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-02 15:26:50 +0100

    wip

--- src/main/scala/tasks/day2.scala
+++ src/main/scala/tasks/day2.scala
@@ -2,7 +2,29 @@ package com.github.frankivo
 package tasks
 
 object day2 {
+
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println(s"Total score: ${getTotalScore()}")
   }
+
+  private val hands = Map(
+    'A' -> "rock",
+    'B' -> "paper",
+    'C' -> "scissors",
+    'X' -> "rock",
+    'Y' -> "paper",
+    'Z' -> "scissors",
+  )
+
+  private def getTotalScore(): Long = {
+    input.foreach(i => {
+      if (hands(i._1) equals hands(i._2))
+        println("draw")
+    })
+    0
+  }
+
+  private val input: Seq[(Char, Char)] = Seq("A Y", "B X", "C Z")
+    .map(_.filter(('A' to 'Z').toSet))
+    .map(l => (l(0), l(1)))
 }
