50995628fdd322bf4bdde6e4a00090421373e937
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-02 21:02:13 +0100

    day2 part 1

--- src/main/scala/tasks/day2.scala
+++ src/main/scala/tasks/day2.scala
@@ -16,24 +16,35 @@ object day2 {
     'Z' -> "scissors",
   )
 
+  private val scores = Map(
+    "rock" -> 1,
+    "paper" -> 2,
+    "scissors" -> 3,
+  )
+
   private def getTotalScore: Long = {
     input.foldLeft(0L) {
       (total, line) => {
 
         val opponent = hands(line._1)
         val player = hands(line._2)
+        val score = scores(player)
 
         if (opponent equals player)
-          total + 6
-        else if (opponent == "rock" && player == "paper")
-          total + 8
+          total + 3 + score
+        else if (
+          (opponent == "rock" && player == "paper")
+          || (opponent == "paper" && player == "scissors")
+          || (opponent == "scissors" && player == "rock")
+        )
+          total + 6 + score
         else
-          total + 1
+          total + 0 + score
       }
     }
   }
 
-  private val input: Seq[(Char, Char)] = Seq("A Y", "B X", "C Z")
+  private val input: Seq[(Char, Char)] = util.get("day2.txt")
     .map(_.filter(('A' to 'Z').toSet))
     .map(l => (l(0), l(1)))
 }
