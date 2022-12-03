1ea28c3ba5c227de056b75c3e98cc63061aafa44
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-03 14:55:38 +0100

    day 3 part 2

--- src/main/scala/tasks/day3.scala
+++ src/main/scala/tasks/day3.scala
@@ -3,13 +3,14 @@ package tasks
 
 object day3 {
   def main(args: Array[String]): Unit = {
-    println(s"Sum of priorities: ${getRucksackPrioritiesSum}")
+    println(s"Sum of priorities: $getRucksackPrioritiesSum")
+    println(s"Sum of badge priorities: $getBadgePriorities")
   }
 
   private val priorities = (('a' to 'z') ++ ('A' to 'Z')).zip(LazyList.from(1)).toMap
 
   def getRucksackPrioritiesSum: Long = {
-    input.foldLeft(0L) {
+    inputPart1.foldLeft(0L) {
       (sum, cur) => {
         val doubleChar = cur._1.distinct.filter(c => cur._2.contains(c)).head
         sum + priorities(doubleChar)
@@ -17,10 +18,23 @@ object day3 {
     }
   }
 
-  def input: Seq[(String, String)] = util.get("day3.txt")
+  def getBadgePriorities: Long = {
+    inputPart2
+      .grouped(3)
+      .foldLeft(0L) {
+        (sum, cur) => {
+          val badge = cur.head.distinct.filter(c => cur(1).contains(c) && cur(2).contains(c)).head
+          sum + priorities(badge)
+        }
+      }
+  }
+
+  def inputPart1: Seq[(String, String)] = util.get("day3.txt")
     .map(l => (l, l.length / 2))
     .map(l => (
       l._1.substring(0, l._2),
       l._1.substring(l._2)
     ))
+
+  def inputPart2: Seq[String] = util.get("day3.txt")
 }
