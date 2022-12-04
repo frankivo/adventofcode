deb3cec846cd0d83a8606719960f753f3d185bae
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-04 13:15:42 +0100

    day 4 part 1
    so much for readability

--- src/main/scala/tasks/day4.scala
+++ src/main/scala/tasks/day4.scala
@@ -3,16 +3,20 @@ package tasks
 
 object day4 {
   def main(args: Array[String]): Unit = {
-    input.foreach(i => {
-      println(i(0))
+    println(s"Amount of included ranges: $getIncludedRangeCount")
+  }
+
+  def getIncludedRangeCount: Long = {
+    input.count(i => {
+      (i(0)._1 <= i(1)._1 && i(0)._2 >= i(1)._2) ||
+        (i(1)._1 <= i(0)._1 && i(1)._2 >= i(0)._2)
     })
   }
 
-  val input: Iterator[Array[(Int, Int)]] = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
-    .split("\n")
+  val input: Iterator[Array[(Int, Int)]] = util.get("day4.txt")
     .mkString(",")
     .split(",")
-    .map(_.split("-") )
-    .map(x=> (x(0).toInt, x(1).toInt))
+    .map(_.split("-"))
+    .map(x => (x(0).toInt, x(1).toInt))
     .grouped(2)
 }
