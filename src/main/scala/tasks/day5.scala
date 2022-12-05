3c54631512acfd1525596ea2e63724b150db6419
Frank Oosterhuis <frank+github@scriptzone.nl>
2022-12-05 17:04:47 +0100

    Day5 (#5)
    * init
    
    * read stack
    
    * cleanup
    
    * parse moves
    
    * stack numbers
    
    * flatten stacks
    
    * remove debug
    
    * working example
    
    * almost there
    
    * sort the map
    
    * day 5 part 2
    
    Co-authored-by: Frank Oosterhuis <frank.oosterhuis@getthere.nl>

---
+++ src/main/scala/tasks/day5.scala
@@ -0,0 +1,75 @@
+package com.github.frankivo
+package tasks
+
+object day5 {
+  def main(args: Array[String]): Unit = {
+    println(s"Top values with CrateMover9000: ${getTops(rearrange(true))}")
+    println(s"Top values with CrateMover9001: ${getTops(rearrange(false))}")
+  }
+
+  private val input: Seq[String] = util.get("day5.txt")
+
+  private val stackPos: Map[Int, Int] = { // Pos, stacknr
+    "[0-9]".r
+      .findAllIn(input.find(_.trim.startsWith("1")).get)
+      .matchData
+      .map(m => (m.start, m.matched.toInt))
+      .toMap
+  }
+
+  private val stacks: Map[Int, String] = {
+    input
+      .foldLeft(Seq[(Int, Char)]()) {
+        (total, current) => {
+          val crates = "[A-Z]".r
+            .findAllIn(current)
+            .matchData.map(m => (stackPos(m.start), m.matched.head))
+
+          total ++ crates
+        }
+      }
+      .groupBy(_._1)
+      .map(x => (x._1, x._2.map(_._2).mkString))
+  }
+
+  case class Move(amount: Int, from: Int, to: Int)
+
+  private val moves: Seq[Move] = {
+    input
+      .filter(_.startsWith("move"))
+      .map(l => {
+        val numbers = "([0-9]+)".r
+          .findAllIn(l)
+          .map(_.toInt)
+          .toSeq
+        Move(numbers.head, numbers(1), numbers(2))
+      })
+  }
+
+  private def rearrange(oneByOne: Boolean): Map[Int, String] = {
+    moves
+      .foldLeft(stacks) {
+        (result, move) => {
+          val tomove = result(move.from).take(move.amount)
+
+          result.map {
+            case (nr, stack) =>
+              if (nr == move.from)
+                (nr, stack.takeRight(stack.length - move.amount))
+              else if (nr == move.to && oneByOne)
+                (nr, tomove.reverse ++ stack)
+              else if (nr == move.to && !oneByOne)
+                (nr, tomove ++ stack)
+              else
+                (nr, stack)
+          }
+        }
+      }
+  }
+
+  private def getTops(stacks: Map[Int, String]): String = stacks
+    .toSeq
+    .sortBy(_._1)
+    .map(_._2.head)
+    .mkString
+}
