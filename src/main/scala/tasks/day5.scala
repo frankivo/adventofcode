9335d58075af8ba6d91a680d54963d6a30822a06
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-05 14:30:54 +0100

    parse moves

--- src/main/scala/tasks/day5.scala
+++ src/main/scala/tasks/day5.scala
@@ -4,6 +4,7 @@ package tasks
 object day5 {
   def main(args: Array[String]): Unit = {
     println(stacks)
+    println(moves)
   }
 
   private val input: Array[String] = "   [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2"
@@ -24,5 +25,19 @@ object day5 {
         }
       }
       .groupBy(_._1)
+  }
+
+  case class Move(amount: Int, from: Int, to: Int)
+
+  private val moves: Seq[Move] = {
+    input
+      .filter(_.startsWith("move"))
+      .map(l => {
+        val numbers = "([0-9]+)".r
+          .findAllIn(l)
+          .map(_.toInt)
+          .toSeq
+        Move(numbers.head, numbers(1), numbers(2))
+      })
   }
 }
