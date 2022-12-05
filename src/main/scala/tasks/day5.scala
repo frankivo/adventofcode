570ec68b1a4546f284cd9a2253b0fb92619c18f1
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-05 14:11:39 +0100

    read stack

--- src/main/scala/tasks/day5.scala
+++ src/main/scala/tasks/day5.scala
@@ -3,6 +3,26 @@ package tasks
 
 object day5 {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println(stacks)
+  }
+
+  private val input: Array[String] = "   [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2"
+    .split("\n")
+
+   private val stacks: Map[Int, Seq[(Int, Char)]] = {
+    input
+      .foldLeft(Seq[(Int, Char)]()) {
+        (total, current) => {
+          val crates = "[A-Z]".r
+            .findAllIn(current)
+            .matchData.map(m => {
+            val stack = (m.start / 3) + 1
+            val crate = m.matched.head
+            (stack, crate)
+          })
+          total ++ crates
+        }
+      }
+    .groupBy(_._1)
   }
 }
