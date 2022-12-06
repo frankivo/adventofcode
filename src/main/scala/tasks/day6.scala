6a0b4fda653edcb9cf859677fa0d876f62ea6307
Frank Oosterhuis <frank+github@scriptzone.nl>
2022-12-06 14:46:43 +0100

    Day6 (#6)
    * find marker
    
    * examples
    
    * day 6 part 1
    
    * day 6 part 2
    
    Co-authored-by: Frank Oosterhuis <frank.oosterhuis@getthere.nl>

---
+++ src/main/scala/tasks/day6.scala
@@ -0,0 +1,20 @@
+package com.github.frankivo
+package tasks
+
+object day6 {
+  def main(args: Array[String]): Unit = {
+    println(s"first start-of-packet is at pos: $startOfPacketMarker")
+    println(s"first start-of-message is at pos: $startOfMessageMarker")
+  }
+
+  def startOfPacketMarker: Int = marker(util.get("day6.txt").head, 4)
+
+  def startOfMessageMarker: Int = marker(util.get("day6.txt").head, 14)
+
+  def marker(msg: String, size: Int): Int = msg
+    .sliding(size, 1)
+    .zipWithIndex
+    .find(_._1.distinct.length == size)
+    .map(_._2)
+    .get + size
+}
