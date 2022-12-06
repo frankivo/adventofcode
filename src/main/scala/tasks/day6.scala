d56d6768b02df4b9ec6626d99005e06451b2c5a8
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-06 14:18:02 +0100

    examples

--- src/main/scala/tasks/day6.scala
+++ src/main/scala/tasks/day6.scala
@@ -3,18 +3,23 @@ package tasks
 
 object day6 {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-
-    println(marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-
-
+    showMarkers()
   }
+
+  def showMarkers(): Unit = input.map(marker).foreach(println)
 
   def marker(msg: String): Int = msg
     .sliding(4, 1)
     .zipWithIndex
-    .take(4)
     .find(_._1.distinct.length == 4)
     .map(_._2)
     .get + 4
+
+  private val input: Seq[String] = Seq(
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+    "bvwbjplbgvbhsrlpgdmjqwftvncz",
+    "nppdvjthqldpwncqszvftbrmjlhg",
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+  )
 }
