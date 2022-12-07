e1252785a05063ccdb1b9190f015b4bb6d63d33f
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-07 09:57:27 +0100

    path builder

---
+++ src/main/scala/tasks/day7.scala
@@ -0,0 +1,39 @@
+package com.github.frankivo
+package tasks
+
+object day7 {
+  def main(args: Array[String]): Unit = {
+    println(tree)
+  }
+
+  val tree: Map[String, Long] = {
+    util.get("day7.txt")
+      .foldLeft(("", Map[String, Long]())) {
+        (browser, current) => {
+          val path = {
+            if (current.startsWith("$ cd"))
+              newPath(browser._1, current)
+            else browser._1
+          }
+          val files = {
+            if (current.exists(_.isDigit)) {
+              val size :: name :: _ = current.split(" ").toList
+              val file = (path + name, size.toLong)
+              browser._2 + file
+            }
+            else browser._2
+          }
+          (path, files)
+        }
+      }
+      ._2
+  }
+
+  def newPath(old: String, command: String): String = {
+    val action = command.split(" ").last
+    action match
+      case "/" => action
+      case ".." => old.split("/").dropRight(1).mkString("/") + "/"
+      case _ => old + action + "/"
+  }
+}
