f21e07ffd3a232e41c297794f0a90ae04c3066da
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-07 10:12:00 +0100

    wip

--- src/main/scala/tasks/day7.scala
+++ src/main/scala/tasks/day7.scala
@@ -3,7 +3,8 @@ package tasks
 
 object day7 {
   def main(args: Array[String]): Unit = {
-    println(tree)
+    println(dirSizes)
+    println(findSmallDirsTotal)
   }
 
   val tree: Map[String, Long] = {
@@ -17,7 +18,7 @@ object day7 {
           }
           val files = {
             if (current.exists(_.isDigit)) {
-              val data  = current.split(" ").toList
+              val data = current.split(" ").toList
               val file = (path + data.last, data.head.toLong)
               browser._2 + file
             }
@@ -28,6 +29,23 @@ object day7 {
       }
       ._2
   }
+
+  def findSmallDirsTotal: Long = {
+    dirSizes
+      .filter(_._2 <= 10000)
+      .values
+      .sum
+  }
+
+  def dirSizes: Map[String, Long] = {
+    tree
+      .toSeq
+      .map(f => (getDir(f._1), f._2))
+      .groupBy(_._1)
+      .map(d => (d._1, d._2.map(_._2).sum))
+  }
+
+  def getDir(path: String): String = path.split("/").dropRight(1).mkString("/") + "/"
 
   def newPath(old: String, command: String): String = {
     val action = command.split(" ").last
