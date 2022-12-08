81f2a92b4550548fc2a15352efe6e883e759901e
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-08 18:02:21 +0100

    find max in row

--- src/main/scala/tasks/day8.scala
+++ src/main/scala/tasks/day8.scala
@@ -4,6 +4,7 @@ package tasks
 object day8 {
   def main(args: Array[String]): Unit = {
     println(grid)
+    visibleTrees
   }
 
   private case class Coordinate(x: Int, y: Int)
@@ -15,4 +16,20 @@ object day8 {
       .flatMap(i => i._2.map(j => (Coordinate(j._2, i._1), j._1.getNumericValue)))
   }
 
+  private def visibleTrees: Seq[Coordinate] = {
+    println(visibleTreesRow(1).toSeq.sortBy(_.x))
+    Seq()
+  }
+
+  private def visibleTreesRow(y: Int): Set[Coordinate] = {
+    val row = grid.filter(_._1.y == y)
+    val topFromLeft = row.maxBy(_._2)._1.x
+    val topFromRight = row.reverse.maxBy(_._2)._1.x
+    val leftToRight = row.filter(_._1.x <= topFromLeft)
+    val rightToLeft = row.filter(_._1.x >= topFromRight)
+
+    (leftToRight ++ rightToLeft)
+      .map(_._1)
+      .toSet
+  }
 }
