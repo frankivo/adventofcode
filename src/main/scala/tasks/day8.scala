c2cc15c24ec1ce693009537390946b6c685e64f1
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-08 18:35:39 +0100

    debug :)

--- src/main/scala/tasks/day8.scala
+++ src/main/scala/tasks/day8.scala
@@ -3,8 +3,7 @@ package tasks
 
 object day8 {
   def main(args: Array[String]): Unit = {
-    println(grid)
-    visibleTrees
+    println(s"Number of visible trees: ${visibleTrees.size}")
   }
 
   private case class Coordinate(x: Int, y: Int)
@@ -16,19 +15,22 @@ object day8 {
       .flatMap(i => i._2.map(j => (Coordinate(j._2, i._1), j._1.getNumericValue)))
   }
 
-  private val gridSize: Int = grid.length / 4 - 1
+  private val gridSize: Int = grid.maxBy(_._1.x)._1.x
 
-  private def visibleTrees: Seq[Coordinate] = {
-    println(visibleTreesRow(1).toSeq.sortBy(_.x))
-    Seq()
+  private def visibleTrees: Set[Coordinate] = {
+    (0 to gridSize).flatMap(i => {
+      val x = visibleTreesRow(i)
+      println(x.size)
+      x
+    }).toSet
   }
 
   private def visibleTreesRow(y: Int): Set[Coordinate] = {
     val row = grid.filter(_._1.y == y)
-    val topFromLeft = row.maxBy(_._2)._1.x
-    val topFromRight = row.reverse.maxBy(_._2)._1.x
-    val leftToRight = row.filter(_._1.x <= topFromLeft)
-    val rightToLeft = row.filter(_._1.x >= topFromRight)
+    val topFromLeft = row.maxBy(_._2)
+    val topFromRight = row.reverse.maxBy(_._2)
+    val leftToRight = row.filter(c => c._1.x == 0 || (c._1.x <= topFromLeft._1.x) && c._2 < topFromLeft._2)
+    val rightToLeft = row.filter(c => c._1.x == gridSize || (c._1.x >= topFromRight._1.x) && c._2 < topFromRight._2)
 
     (leftToRight ++ rightToLeft)
       .map(_._1)
