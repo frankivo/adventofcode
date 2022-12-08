add90ac889d8ec835d30a3135d6c7f779ea7444e
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-08 17:23:50 +0100

    grid with coordinates

--- src/main/scala/tasks/day8.scala
+++ src/main/scala/tasks/day8.scala
@@ -3,6 +3,16 @@ package tasks
 
 object day8 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    println(grid)
   }
+
+  private case class Coordinate(x: Int, y: Int)
+
+  private val grid: Seq[(Coordinate, Int)] = {
+    util.get("day8.txt")
+      .zipWithIndex
+      .map(y => (y._2, y._1.zipWithIndex))
+      .flatMap(i => i._2.map(j => (Coordinate(j._2, i._1), j._1.getNumericValue)))
+  }
+
 }
