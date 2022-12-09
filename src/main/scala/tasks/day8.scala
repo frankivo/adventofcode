840a8fa23a7aaf3089faf063b8407a21a994a043
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-09 09:13:05 +0100

    lookDown

--- src/main/scala/tasks/day8.scala
+++ src/main/scala/tasks/day8.scala
@@ -44,10 +44,11 @@ object day8 {
   }
 
   private def bestScenicScore: Int = {
-    grid.take(5).foldLeft(0) {
+    grid.foldLeft(0) {
       (best, tree) => {
-        println(lookLeft(tree))
+        //        println(lookLeft(tree))
         //        lookLeft(tree)
+        println(lookDown(tree))
         best
       }
     }
@@ -63,11 +64,20 @@ object day8 {
   }
 
   private def lookLeft(tree: (Coordinate, Int)): Int = {
-     tree._1.x - grid
-       .filter(_._1.y == tree._1.y) // All trees on col
-       .filter(_._1.x < tree._1.x) // All trees on the left
-       .findLast(_._2 >= tree._2)
-       .map(_._1.x)
-       .getOrElse(0)
+    tree._1.x - grid
+      .filter(_._1.y == tree._1.y) // All trees on col
+      .filter(_._1.x < tree._1.x) // All trees on the left
+      .findLast(_._2 >= tree._2)
+      .map(_._1.x)
+      .getOrElse(0)
+  }
+
+  private def lookDown(tree: (Coordinate, Int)): Int = {
+    grid
+      .filter(_._1.x == tree._1.x)
+      .filter(_._1.y > tree._1.y)
+      .find(_._2 >= tree._2)
+      .map(_._1.y)
+      .getOrElse(gridSize) - tree._1.y
   }
 }
