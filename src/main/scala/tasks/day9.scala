843c30f74af34f35081c38d039faf2fd1d61d6b6
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-10 20:34:10 +0100

    parse moves

--- src/main/scala/tasks/day9.scala
+++ src/main/scala/tasks/day9.scala
@@ -3,6 +3,12 @@ package tasks
 
 object day9 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    println(moves)
   }
+
+  private val moves: String = util.get("day9.txt")
+    .map(_.split(" "))
+    .map(m => (m(0), m(1).toInt))
+    .flatMap(m => m._1 * m._2)
+    .mkString
 }
