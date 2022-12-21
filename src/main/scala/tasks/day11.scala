42c52b6ad00b2e98a67d43db3d78854766034de7
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-21 11:37:55 +0100

    day 11 part 1

--- src/main/scala/tasks/day11.scala
+++ src/main/scala/tasks/day11.scala
@@ -6,8 +6,8 @@ object day11 {
     println(play())
   }
 
-  type monkey = (Int, Seq[Int])
-  type monkeys = Map[Int, Seq[Int]]
+  type monkey = (Int, Seq[Long])
+  type monkeys = Map[Int, Seq[Long]]
 
   private def play(): Long = {
     val start = startingItems.map(m => (m._1, (m._2, 0)))
@@ -18,8 +18,8 @@ object day11 {
     end.map(_._2._2).toSeq.sorted.takeRight(2).product
   }
 
-  private def playRound(r: Map[Int, (Seq[Int], Int)]): Map[Int, (Seq[Int], Int)] = {
-    r.keys.foldLeft(r) {
+  private def playRound(r: Map[Int, (Seq[Long], Int)]): Map[Int, (Seq[Long], Int)] = {
+    r.keys.toSeq.sorted.foldLeft(r) {
       (state, roundMonkey) => {
         val monk = state.find(_._1 == roundMonkey).get
 
@@ -46,12 +46,16 @@ object day11 {
 
   private def operate(m: monkey): monkey = {
     (m._1,
-      m._2.map(i => {
+      m._2.map(old => {
         m._1 match
-          case 0 => i * 19
-          case 1 => i + 6
-          case 2 => i * i
-          case 3 => i + 3
+          case 0 => old * 2
+          case 1 => old * 13
+          case 2 => old + 5
+          case 3 => old + 6
+          case 4 => old + 1
+          case 5 => old + 4
+          case 6 => old + 2
+          case 7 => old * old
       }).map(_ / 3)
     )
   }
@@ -59,10 +63,14 @@ object day11 {
   private def divide(m: monkey): monkeys = {
     m._2.map(i => {
       (m._1 match
-        case 0 => if (i % 23 == 0) 2 else 3
-        case 1 => if (i % 19 == 0) 2 else 0
-        case 2 => if (i % 13 == 0) 1 else 3
-        case 3 => if (i % 17 == 0) 0 else 1
+        case 0 => if (i % 5 == 0) 6 else 1
+        case 1 => if (i % 2 == 0) 2 else 6
+        case 2 => if (i % 19 == 0) 7 else 5
+        case 3 => if (i % 7 == 0) 0 else 4
+        case 4 => if (i % 17 == 0) 0 else 1
+        case 5 => if (i % 13 == 0) 4 else 3
+        case 6 => if (i % 3 == 0) 2 else 7
+        case 7 => if (i % 11 == 0) 3 else 5
         , i)
     })
       .groupBy(_._1)
@@ -70,11 +78,15 @@ object day11 {
   }
 
   private val startingItems: monkeys = Seq(
-    Seq(79, 98),
-    Seq(54, 65, 75, 74),
-    Seq(79, 60, 97),
-    Seq(74),
+    Seq(98, 89, 52),
+    Seq(57, 95, 80, 92, 57, 78),
+    Seq(82, 74, 97, 75, 51, 92, 83),
+    Seq(97, 88, 51, 68, 76),
+    Seq(63),
+    Seq(94, 91, 51, 63),
+    Seq(61, 54, 94, 71, 74, 68, 98, 83),
+    Seq(90, 56),
   ).zipWithIndex
-    .map(si => (si._2, si._1))
+    .map(si => (si._2, si._1.map(_.toLong)))
     .toMap
 }
