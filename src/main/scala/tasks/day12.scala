d292af09bc1ed87f7f877eff9d8e399bb73d4265
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-21 16:12:35 +0100

    input parser

--- src/main/scala/tasks/day12.scala
+++ src/main/scala/tasks/day12.scala
@@ -3,6 +3,14 @@ package tasks
 
 object day12 {
   def main(args: Array[String]): Unit = {
-    util.get("day12.txt").foreach(println)
+    println(input)
+  }
+
+  val input: Seq[coordinate] = {
+    util.get("day12.txt")
+      .zipWithIndex.flatMap(y =>
+      y._1.zipWithIndex
+        .map(x => coordinate(x = x._2, y = y._2, name = x._1.toString))
+    )
   }
 }
