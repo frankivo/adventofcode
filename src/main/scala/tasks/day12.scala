b833ee76f316eed1fb22d3e075df9283b4656cf9
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-21 17:15:49 +0100

    start/end

--- src/main/scala/tasks/day12.scala
+++ src/main/scala/tasks/day12.scala
@@ -3,14 +3,19 @@ package tasks
 
 object day12 {
   def main(args: Array[String]): Unit = {
-    println(input)
+    println(start)
+    println(end)
   }
 
-  val input: Seq[coordinate] = {
+  private val input: Seq[coordinate] = {
     util.get("day12.txt")
       .zipWithIndex.flatMap(y =>
       y._1.zipWithIndex
         .map(x => coordinate(x = x._2, y = y._2, name = x._1.toString))
     )
   }
+
+  private val start: coordinate = input.find(_.name.equals("S")).get
+
+  private val end: coordinate = input.find(_.name.equals("E")).get
 }
