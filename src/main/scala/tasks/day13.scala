7a87e0eb55c68f81e4fdf125edf69c2c1ddbc532
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-25 13:59:27 +0100

    fold

--- src/main/scala/tasks/day13.scala
+++ src/main/scala/tasks/day13.scala
@@ -3,20 +3,16 @@ package tasks
 
 object day13 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
-
-    println(rawInput)
-    println(compare(rawInput.head))
+    println(s"Sum of sorted pair indices: ${part1}")
   }
 
-  private def compare(pair: Pair): Boolean = {
-    println(pair.left)
-    println(pair.right)
-
-    val left = pair.left.split(",").toSeq
-    val right = pair.right.split(",").toSeq
-
-    left.indices.forall(i => left(i) <= right(i))
+  def part1 : Long = {
+    rawInput
+      .foldLeft(0L) {
+        (sum, pair) => {
+          sum + pair.index // If pair is sorted, else 0
+        }
+      }
   }
 
   private case class Pair(index: Int, left: String, right: String)
