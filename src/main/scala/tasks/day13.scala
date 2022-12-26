87f7d6ac9d487a48fde0d7084ec97467bd84c6bf
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-26 13:45:24 +0100

    convert when required

--- src/main/scala/tasks/day13.scala
+++ src/main/scala/tasks/day13.scala
@@ -18,22 +18,37 @@ object day13 {
 
   private def compareLists(left: Packet, right: Packet): Boolean = {
     left.forall(l => {
-      // Assume no list and equal size
-      compareItems(l._2, right(l._1))
+      val r = right.get(l._1)
+      if (r.isEmpty)
+        true
+      else if (l._2.isJsonPrimitive && r.get.isJsonPrimitive)
+        compareItems(l._2, r.get)
+      else {
+        compareLists(l._2.asPacket, r.get.asPacket)
+      }
     })
   }
 
   private def compareItems(left: JsonElement, right: JsonElement): Boolean = left.getAsInt <= right.getAsInt
 
   extension (array: JsonArray) {
-    def asMap: Map[Int, JsonElement] = array.asList().asScala.zipWithIndex.map(i => (i._2, i._1)).toMap
+    def asPacket: Packet = array.asList().asScala.zipWithIndex.map(i => (i._2, i._1)).toMap
+  }
+
+  extension (element: JsonElement) {
+    def asPacket: Packet = {
+      if (element.isJsonArray)
+        element.getAsJsonArray.asPacket
+      else
+        JsonParser.parseString(s"[$element]").getAsJsonArray.asPacket
+    }
   }
 
   private val rawInput: Seq[Pair] = util.get("day13.txt")
     .filterNot(_.isEmpty)
     .map(JsonParser.parseString)
     .map(_.getAsJsonArray)
-    .map(_.asMap)
+    .map(_.asPacket)
     .sliding(2)
     .zip(LazyList.from(1))
     .map(p => Pair(p._2, p._1.head, p._1.last))
