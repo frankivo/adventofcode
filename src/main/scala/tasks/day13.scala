ccfa7e04cc9015b8edf7b25be741d9128bc352a7
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-26 12:56:56 +0100

    use gson for parsing

--- src/main/scala/tasks/day13.scala
+++ src/main/scala/tasks/day13.scala
@@ -1,41 +1,28 @@
 package com.github.frankivo
 package tasks
 
+import com.google.gson.reflect.TypeToken
+import com.google.gson.{JsonArray, JsonParser}
+
 object day13 {
   def main(args: Array[String]): Unit = {
     println(s"Sum of sorted pair indices: $part1")
   }
 
-  type Packet = Seq[Any]
-
-  private case class Pair(index: Int, left: Packet, right: Packet)
+  private case class Pair(index: Int, left: JsonArray, right: JsonArray)
 
   def part1: Long = rawInput.flatMap(p => Option.when(compareLists(p.left, p.right))(p.index)).sum
 
-  private def compareLists(left: Packet, right: Packet): Boolean = {
-    left.length == right.length
+  private def compareLists(left: JsonArray, right: JsonArray): Boolean = {
+    left.size() == right.size()
   }
 
   private val rawInput: Seq[Pair] = util.get("day13.txt")
     .filterNot(_.isEmpty)
-    .map(Parser().parse(_))
+    .map(JsonParser.parseString)
+    .map(_.getAsJsonArray)
     .sliding(2)
     .zip(LazyList.from(1))
     .map(p => Pair(p._2, p._1.head, p._1.last))
     .toSeq
-
-  private class Parser {
-    private var toParse: String = ""
-
-    def parse(str: String): Packet = {
-      toParse = str
-      parseList
-    }
-
-    private def parseList: Packet = {
-      toParse = toParse.drop(1)
-      Seq()
-    }
-  }
-
 }
