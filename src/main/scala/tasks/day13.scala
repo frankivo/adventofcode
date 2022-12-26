de971bfb4d52c212631223cad56ea6e76057927c
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-26 13:29:10 +0100

    compare list of ints

--- src/main/scala/tasks/day13.scala
+++ src/main/scala/tasks/day13.scala
@@ -1,26 +1,39 @@
 package com.github.frankivo
 package tasks
 
-import com.google.gson.reflect.TypeToken
-import com.google.gson.{JsonArray, JsonParser}
+import com.google.gson.{JsonArray, JsonElement, JsonParser}
+
+import scala.jdk.CollectionConverters.*
 
 object day13 {
   def main(args: Array[String]): Unit = {
     println(s"Sum of sorted pair indices: $part1")
   }
 
-  private case class Pair(index: Int, left: JsonArray, right: JsonArray)
+  type Packet = Map[Int, JsonElement]
+
+  private case class Pair(index: Int, left: Packet, right: Packet)
 
   def part1: Long = rawInput.flatMap(p => Option.when(compareLists(p.left, p.right))(p.index)).sum
 
-  private def compareLists(left: JsonArray, right: JsonArray): Boolean = {
-    left.size() == right.size()
+  private def compareLists(left: Packet, right: Packet): Boolean = {
+    left.forall(l => {
+      // Assume no list and equal size
+      compareItems(l._2, right(l._1))
+    })
+  }
+
+  private def compareItems(left: JsonElement, right: JsonElement): Boolean = left.getAsInt <= right.getAsInt
+
+  extension (array: JsonArray) {
+    def asMap: Map[Int, JsonElement] = array.asList().asScala.zipWithIndex.map(i => (i._2, i._1)).toMap
   }
 
   private val rawInput: Seq[Pair] = util.get("day13.txt")
     .filterNot(_.isEmpty)
     .map(JsonParser.parseString)
     .map(_.getAsJsonArray)
+    .map(_.asMap)
     .sliding(2)
     .zip(LazyList.from(1))
     .map(p => Pair(p._2, p._1.head, p._1.last))
