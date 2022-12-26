b2088c4a46dc1b04b30be0d34c753377c4b7abdd
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-26 01:09:59 +0100

    parser wip

--- src/main/scala/tasks/day13.scala
+++ src/main/scala/tasks/day13.scala
@@ -6,18 +6,36 @@ object day13 {
     println(s"Sum of sorted pair indices: $part1")
   }
 
+  type Packet = Seq[Any]
+
+  private case class Pair(index: Int, left: Packet, right: Packet)
+
   def part1: Long = rawInput.flatMap(p => Option.when(compareLists(p.left, p.right))(p.index)).sum
 
-  private def compareLists(left: String, right: String): Boolean = {
+  private def compareLists(left: Packet, right: Packet): Boolean = {
     left.length == right.length
   }
 
-  private case class Pair(index: Int, left: String, right: String)
-
   private val rawInput: Seq[Pair] = util.get("day13.txt")
     .filterNot(_.isEmpty)
+    .map(Parser().parse(_))
     .sliding(2)
     .zip(LazyList.from(1))
     .map(p => Pair(p._2, p._1.head, p._1.last))
     .toSeq
+
+  private class Parser {
+    private var toParse: String = ""
+
+    def parse(str: String): Packet = {
+      toParse = str
+      parseList
+    }
+
+    private def parseList: Packet = {
+      toParse = toParse.drop(1)
+      Seq()
+    }
+  }
+
 }
