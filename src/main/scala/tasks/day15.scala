07daec7347f80cab5a1b81410287a952d59cae9c
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2022-12-31 16:12:17 +0100

    distance

--- src/main/scala/tasks/day15.scala
+++ src/main/scala/tasks/day15.scala
@@ -4,18 +4,24 @@ package tasks
 object day15 {
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-    println(input)
+    input.foreach(i => {
+      println(s"$i : ${i.distance}")
+    })
   }
 
-  private val input : Set[coordinate] = {
+  private case class sensor(sensor: coordinate, beacon: coordinate) {
+    def distance: Int = (sensor.x - beacon.x).abs + (sensor.y - beacon.y).abs
+  }
+
+  private val input: Set[sensor] = {
     util.get("day15.txt")
       .flatMap("""([\-0-9]+)""".r.findAllMatchIn)
       .map(_.matched.toInt)
       .grouped(4)
-      .flatMap(g => {
-        Seq(
+      .map(g => {
+        sensor(
           coordinate(g(0), g(1), "S"),
-          coordinate(g(2), g(3), "B"),
+          coordinate(g(2), g(3), "B")
         )
       })
       .toSet
