77f796848c5925cacf5518958b52d533ed54f90f
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-01-03 14:58:55 +0100

    bfs explore

--- src/main/scala/tasks/day16.scala
+++ src/main/scala/tasks/day16.scala
@@ -4,15 +4,34 @@ package tasks
 object day16 {
   def main(args: Array[String]): Unit = {
     input.foreach(println)
+    explore()
   }
 
-  private case class Valve(name: String, flowRate: Int, tunnels: Set[String])
+  def explore(): Unit = {
+    var distances = Map[String, Int]("AA" -> 0)
+    var explorable = Set[String]("AA")
 
-  private val input: Set[Valve] = {
+    while (explorable.nonEmpty) {
+      val cur = explorable.head
+      val dist = distances(cur) + 1
+
+      val adj = input(cur).tunnels
+
+      explorable = explorable.filterNot(_ == cur) ++ adj.filterNot(distances.contains)
+      distances = distances ++ adj.filterNot(distances.contains).map(_ -> dist)
+    }
+
+    println(distances)
+
+  }
+
+  private case class Valve(flowRate: Int, tunnels: Set[String])
+
+  private val input: Map[String, Valve] = {
     util.get("day16.txt")
       .map("""([A-Z]{2}|[0-9]+)""".r.findAllIn)
       .map(_.matchData.map(_.matched).toSeq)
-      .map(m => Valve(m.head, m(1).toInt, m.drop(2).toSet))
-      .toSet
+      .map(m => (m.head, Valve(m(1).toInt, m.drop(2).toSet)))
+      .toMap
   }
 }
