9d343d46292d9a6ca4acd2e0bc105e2ff7b70d57
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-01-03 17:54:29 +0100

    moving to valves

--- src/main/scala/tasks/day16.scala
+++ src/main/scala/tasks/day16.scala
@@ -3,12 +3,49 @@ package tasks
 
 object day16 {
   def main(args: Array[String]): Unit = {
-    println(input.size)+
-    println(explore().size)
+    //    explore("AA").toSeq.sortBy(_._2).foreach(println)
+    part1()
   }
 
-  def explore(): Map[String, Int] = {
-    Seq.unfold(Set[String]("AA"), Map[String, Int]("AA" -> 0)) {
+  def part1(): Unit = {
+    var valves = input.keySet.map(v => (v, input(v).flowRate == 0))
+    var pos = "AA"
+
+    var minute = 1
+    var released = 0
+
+    while (minute <= 30) {
+      val routes = explore(pos)
+      val options = valves.filterNot(_._2).map(_._1).map(v => (v, routes(v), input(v).flowRate))
+      val togo = options.toSeq.minBy(o => (o._2, -o._3))
+
+      println(s"move to ${togo._1}")
+
+      released += pressure(valves)
+
+      if (routes(togo._1) == 1) {
+        pos = togo._1
+        valves = open(togo._1, valves)
+      }
+      else {
+        println(s"$pos --> $togo")
+      }
+
+      minute += 1
+    }
+    println(released)
+  }
+
+  private def open(v: String, valves: Set[(String, Boolean)]): Set[(String, Boolean)] = {
+    println(s"open $v")
+    valves.filterNot(_._1 == v) + (v -> true)
+  }
+
+  private def pressure(valves: Set[(String, Boolean)]): Int =
+    valves.filter(_._2).map(v => input(v._1).flowRate).sum
+
+  private def explore(start: String): Map[String, Int] = {
+    Seq.unfold(Set[String](start), Map[String, Int](start -> 0)) {
       (explorable, distances) => {
         Option.when(explorable.nonEmpty) {
           val cur = explorable.head
