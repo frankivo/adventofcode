b7f3d3eb2d0d8ebb657971d80b110668912cc226
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-01-03 01:07:58 +0100

    input parser

--- src/main/scala/tasks/day16.scala
+++ src/main/scala/tasks/day16.scala
@@ -3,6 +3,16 @@ package tasks
 
 object day16 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    input.foreach(println)
+  }
+
+  private case class Valve(name: String, flowRate: Int, tunnels: Set[String])
+
+  private val input: Set[Valve] = {
+    util.get("day16.txt")
+      .map("""([A-Z]{2}|[0-9]+)""".r.findAllIn)
+      .map(_.matchData.map(_.matched).toSeq)
+      .map(m => Valve(m.head, m(1).toInt, m.drop(2).toSet))
+      .toSet
   }
 }
