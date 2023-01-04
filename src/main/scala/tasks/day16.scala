dfded58976c949bece7716689416dad939570092
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-01-04 11:30:37 +0100

    find distances

--- src/main/scala/tasks/day16.scala
+++ src/main/scala/tasks/day16.scala
@@ -3,49 +3,15 @@ package tasks
 
 object day16 {
   def main(args: Array[String]): Unit = {
-    //    explore("AA").toSeq.sortBy(_._2).foreach(println)
     part1()
   }
 
-  def part1(): Unit = {
-    var valves = input.keySet.map(v => (v, input(v).flowRate == 0))
-    var pos = "AA"
+  private case class Valve(flowRate: Int, tunnels: Set[String])
 
-    var minute = 1
-    var released = 0
-
-    while (minute <= 30) {
-      val routes = explore(pos)
-      val options = valves.filterNot(_._2).map(_._1).map(v => (v, routes(v), input(v).flowRate))
-      val togo = options.toSeq.minBy(o => (o._2, -o._3))
-
-      println(s"move to ${togo._1}")
-
-      released += pressure(valves)
-
-      if (routes(togo._1) == 1) {
-        pos = togo._1
-        valves = open(togo._1, valves)
-      }
-      else {
-        println(s"$pos --> $togo")
-      }
-
-      minute += 1
-    }
-    println(released)
+  private def part1(): Unit = {
+    allDistances.foreach(println)
   }
 
-  private def open(v: String, valves: Set[(String, Boolean)]): Set[(String, Boolean)] = {
-    println(s"open $v")
-    valves.filterNot(_._1 == v) + (v -> true)
-  }
-
-  private def pressure(valves: Set[(String, Boolean)]): Int =
-    valves.filter(_._2).map(v => input(v._1).flowRate).sum
-
-  // TODO: track steps
-  // TODO: track gain (remaining minutes * flowrate)
   private def explore(start: String): Map[String, Int] = {
     Seq.unfold(Set[String](start), Map[String, Int](start -> 0)) {
       (explorable, distances) => {
@@ -61,13 +27,21 @@ object day16 {
     }.last
   }
 
-  private case class Valve(flowRate: Int, tunnels: Set[String])
-
   private val input: Map[String, Valve] = {
     util.get("day16.txt")
       .map("""([A-Z]{2}|[0-9]+)""".r.findAllIn)
       .map(_.matchData.map(_.matched).toSeq)
       .map(m => (m.head, Valve(m(1).toInt, m.drop(2).toSet)))
       .toMap
+  }
+
+  private val allDistances: Map[String, Map[String, Int]] = {
+    input.keys.map(k => {
+      (k,
+        explore(k)
+          .filterNot(v => input(v._1).flowRate == 0)
+          .filterNot(_._1 == k)
+      )
+    }).toMap
   }
 }
