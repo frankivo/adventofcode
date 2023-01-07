669a0e3ed3e38bd9169ff4c996b3b4b694060583
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-01-07 13:53:22 +0100

    input parser

--- src/main/scala/tasks/day17.scala
+++ src/main/scala/tasks/day17.scala
@@ -1,8 +1,21 @@
 package com.github.frankivo
 package tasks
 
+
 object day17 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    (0 to 4000).map(_ => jets.next).foreach(println)
   }
+
+  private val moveCount: Int = 2022
+
+  private class Jets {
+    private val jets: String = util.get("day17.txt").head
+
+    private val iterator: Iterator[Int] = LazyList.from(0).iterator
+
+    def next: Char = jets(iterator.next() % jets.length)
+  }
+
+  private val jets: Jets = Jets()
 }
