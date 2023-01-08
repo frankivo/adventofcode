30c9e9f1b733629e53bf068474973da8bef152f5
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-01-08 15:44:00 +0100

    no longs

--- src/main/scala/tasks/day17.scala
+++ src/main/scala/tasks/day17.scala
@@ -1,40 +1,40 @@
 package com.github.frankivo
 package tasks
 
-import java.time.Instant
-
+import scala.collection.mutable
 
 object day17 {
   def main(args: Array[String]): Unit = {
     println(s"Tower is ${part1()} units tall")
-    //    println(s"Tower is ${part2()} units tall")
+    println(s"Tower is ${part2()} units tall")
   }
 
   private val jetStream = JetStream()
   private val rockStream = RockStream()
 
-  private val moveCount = 2022
   private val fieldWidth = 7
-  private val rockStatic = '#'
-  private val rockMoving = '@'
-  private val air = '.'
+  private val rockStatic = "#"
+  private val rockMoving = "@"
+  private val air = "."
   private val bits = (0 until 7).map(i => (i, 1 << i)).toMap
 
-  private type Field = Map[Long, Int]
-  private type Rock = Seq[rockElement]
+  private type Field = Map[Int, Int]
+  private type Rock = Seq[coordinate]
 
-  private def part1(): Int = solve(Map.empty[Long, Int], moveCount).size
+  private def part1(): Int = solve(Map.empty[Int, Int], 2022).size
 
   private def part2(): Long = {
     0
   }
 
-  private def solve(start: Field, moves: Long): Field = {
-    (0L until moves).foldLeft(start) {
+  private val cache: mutable.Map[(Int, Int), Int] = scala.collection.mutable.Map.empty[(Int, Int), Int]
+
+  private def solve(start: Field, moves: Int): Field = {
+    (0 until moves).foldLeft(start) {
       (field, _) => {
         Seq.unfold(rockStream.next(field.height)) { // Simulate rock movement
           rock => {
-            Option.when(rock.exists(_.state == rockMoving)) {
+            Option.when(rock.exists(_.name == rockMoving)) {
               val updated = rock.blowJet(field).fall(field)
               (updated, updated)
             }
@@ -51,7 +51,7 @@ object day17 {
   extension (field: Field) {
     private def height: Int = field.size
 
-    private def bitmask(y: Long): Int = field.getOrElse(y, 0)
+    private def bitmask(y: Int): Int = field.getOrElse(y, 0)
 
     private def show(): Unit = {
       (-1 until height).reverse.foreach(y => {
@@ -78,14 +78,14 @@ object day17 {
         .map(r => {
           val direction = if (jet == '>') 1 else -1
           val blocked = if (isBlockedHorizontal(jet, field)) 0 else 1
-          rockElement(r.x + (direction * blocked), r.y, r.state)
+          coordinate(r.x + (direction * blocked), r.y, r.name)
         })
     }
 
     private def fall(field: Field): Rock = {
       rock
         .map(r => {
-          rockElement(
+          coordinate(
             r.x,
             r.y - (if (isBlockedVertical(field)) 0 else 1),
             if (isBlockedVertical(field)) rockStatic else rockMoving
@@ -112,19 +112,15 @@ object day17 {
   private class JetStream {
     private val jets: String = util.get("day17.txt").head
 
-    private val LongList: LazyList[Long] = 0L #:: 1L #:: LongList.tail.map { n => n + 1 }
-
-    private val iterator: Iterator[Long] = LongList.iterator
+    private val iterator: Iterator[Int] = LazyList.from(0).iterator
 
     def next: Char = jets((iterator.next() % jets.length.toLong).toInt)
   }
 
   private class RockStream {
-    private val LongList: LazyList[Long] = 0L #:: 1L #:: LongList.tail.map { n => n + 1 }
+    private val iterator: Iterator[Int] = LazyList.from(0).iterator
 
-    private val iterator: Iterator[Long] = LongList.iterator
-
-    def next(top: Long): Rock = {
+    def next(top: Int): Rock = {
       val shape = iterator.next() % 5
       val xys = shape match
         case 0 => // Horizontal line
@@ -138,9 +134,8 @@ object day17 {
         case 4 => // Square
           Seq((2, top + 3), (3, top + 3), (2, top + 4), (3, top + 4))
 
-      xys.map(xy => rockElement(xy._1, xy._2, rockMoving))
+      xys.map(xy => coordinate(xy._1, xy._2, rockMoving))
     }
   }
 
-  private case class rockElement(x: Int, y: Long, state: Char)
 }
