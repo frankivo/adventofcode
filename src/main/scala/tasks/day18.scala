e98546a1f8e2f80218504d01a1df3b8d28528766
Frank Oosterhuis <frank@scriptzone.nl>
2023-01-12 11:33:33 +0100

    poc

--- src/main/scala/tasks/day18.scala
+++ src/main/scala/tasks/day18.scala
@@ -3,9 +3,33 @@ package tasks
 
 object day18 {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val x = input.foldLeft(0) {
+      (sum, cur) => {
+        sum + cur.adjacent.count(input.contains)
+      }
+    }
+    println(x)
+    val y = (input.size * 6 ) - x
+    println(y)
+  }
 
-    println(coordinate(1,2,3))
+  extension (c: coordinate) {
+    private def adjacent: Set[coordinate] = {
+      Set(
+        coordinate(c.x - 1, c.y, c.z),
+        coordinate(c.x + 1, c.y, c.z),
+        coordinate(c.x, c.y - 1, c.z),
+        coordinate(c.x, c.y + 1, c.z),
+        coordinate(c.x, c.y, c.z - 1),
+        coordinate(c.x, c.y, c.z + 1),
+      )
+    }
+  }
 
+  def input: Set[coordinate] = {
+    Set(
+      coordinate(1, 1, 1),
+      coordinate(2, 1, 1),
+    )
   }
 }
