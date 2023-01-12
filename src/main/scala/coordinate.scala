d20e74eaa1ee20b558ccce30569ddc90fd47ea60
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-01-12 17:22:06 +0100

    Day18 (#19)
    * 3d coordinate
    
    * poc
    
    * day 17 part 1
    
    * day 18 part 2
    
    * immutable part2

--- src/main/scala/coordinate.scala
+++ src/main/scala/coordinate.scala
@@ -1,5 +1,13 @@
 package com.github.frankivo
 
-case class coordinate(x: Int, y: Int, name: String = "?") {
+case class coordinate(x: Int, y: Int, z: Int, name: String) {
   def distance(other: coordinate): Int = Seq((x - other.x).abs, (y - other.y).abs).max
+}
+
+object coordinate {
+  def apply(x: Int, y: Int, z: Int): coordinate = new coordinate(x, y, z, "?")
+
+  def apply(xyz: Seq[Int]): coordinate = new coordinate(xyz(0), xyz(1), xyz(2), "?")
+
+  def apply(x: Int, y: Int, name: String = "?"): coordinate = new coordinate(x, y, 0, name)
 }
---
+++ src/main/scala/tasks/day18.scala
@@ -0,0 +1,69 @@
+package com.github.frankivo
+package tasks
+
+object day18 {
+  def main(args: Array[String]): Unit = {
+    println(s"Number of sides that aren't connected: ${part1()}")
+    println(s"Number of exterior exposed sides: ${part2()}")
+  }
+
+  private def part1(): Int = sides - connected(input)
+
+  private def part2(): Int = {
+    Seq.unfold(Seq[coordinate](box._1), Set.empty[coordinate], 0) {
+      (explorable, visited, count) => {
+        Option.when(explorable.nonEmpty) {
+          val cur = explorable.head
+          if (input.contains(cur))
+            (count, (explorable.drop(1), visited, count + 1))
+          else if (!visited.contains(cur))
+            (count, (explorable.drop(1) ++ cur.adjacent.filter(reachable), visited + cur, count))
+          else
+            (count, (explorable.drop(1), visited, count))
+        }
+      }
+    }.last
+  }
+
+  private def sides: Int = input.size * 6
+
+  private def connected(field: Set[coordinate]): Int = {
+    field.foldLeft(0) {
+      (sum, cur) => sum + cur.adjacent.count(field.contains)
+    }
+  }
+
+  extension (c: coordinate) {
+    private def adjacent: Set[coordinate] = {
+      Set(
+        coordinate(c.x - 1, c.y, c.z),
+        coordinate(c.x + 1, c.y, c.z),
+        coordinate(c.x, c.y - 1, c.z),
+        coordinate(c.x, c.y + 1, c.z),
+        coordinate(c.x, c.y, c.z - 1),
+        coordinate(c.x, c.y, c.z + 1),
+      )
+    }
+
+    private def reachable: Boolean = {
+      util.clamp(c.x, box._1.x, box._2.x) == c.x &&
+        util.clamp(c.y, box._1.y, box._2.y) == c.y &&
+        util.clamp(c.z, box._1.z, box._2.z) == c.z
+    }
+  }
+
+  def input: Set[coordinate] = {
+    util.get("day18.txt")
+      .mkString(",")
+      .split(",").toSeq
+      .map(_.toInt)
+      .grouped(3)
+      .map(coordinate.apply)
+      .toSet
+  }
+
+  private val box: (coordinate, coordinate) = (
+    coordinate(input.minBy(_.x).x - 2, input.minBy(_.y).y - 2, input.minBy(_.z).z - 2),
+    coordinate(input.maxBy(_.x).x + 2, input.maxBy(_.y).y + 2, input.maxBy(_.z).z + 2)
+  )
+}
