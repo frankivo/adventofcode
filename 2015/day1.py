797b6fc35eb3bcc9a837929dab349c56e512ebbe
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-02 23:47:07 +0100

    read file

---
+++ 2015/day1.py
@@ -0,0 +1,6 @@
+from helper import *
+
+print("Hello, World!")
+data = getInput(1)
+for i in data:
+    print(data)
---
+++ 2015/helper.py
@@ -0,0 +1,3 @@
+def getInput(day: int):
+    filename = "input/day{0}.txt".format(day)
+    return open(filename).readlines()
