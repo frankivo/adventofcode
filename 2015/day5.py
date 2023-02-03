a98fcf607aa0326ebc1582bfd29880de6f1ee74f
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-03 23:00:16 +0100

    charRepeat

--- 2015/day5.py
+++ 2015/day5.py
@@ -6,7 +6,15 @@ input = ['ugknbfddgicrmopn', 'aaa', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvs
 def vowelCount(s: str) -> int:
     return len(re.findall('([aeiou])', s))
 
+def charRepeat(s: str) -> bool:
+    for i, c in enumerate(s):
+        if i > 0 and s[i-1] == c:
+            return True
+    return False
+
 for i in input:
     print(i)
     print(vowelCount(i))
+    print(charRepeat(i))
+    print("-----")
 
