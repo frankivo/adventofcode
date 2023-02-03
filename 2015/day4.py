775e61849031938ee1c2fdef4f222a83cce969f0
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-03 22:10:16 +0100

    day 4 part 2

--- 2015/day4.py
+++ 2015/day4.py
@@ -1,12 +1,13 @@
 from hashlib import md5
 
-def part1() -> int:
+def solve(zeroes: int) -> int:
     i = 0
     while True:
         hash = md5((input + str(i)).encode()).hexdigest()
-        if hash[0:5] == '00000':
+        if hash[0:zeroes] == ('0' * zeroes):
             return i
         i += 1
 
 input = 'ckczppom'
-print('Hash: {0}'.format(part1()))
+print('Hash: {0}'.format(solve(5)))
+print('Hash: {0}'.format(solve(6)))
