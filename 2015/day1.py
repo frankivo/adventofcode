0c320fcc12a82bf4a80531f051ad4f8a76975ae4
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-03 13:38:07 +0100

    cleanup

--- 2015/day1.py
+++ 2015/day1.py
@@ -6,15 +6,11 @@ def part1() -> int:
 
 def part2() -> int:
     sum = 0
-    index = 0
-    for c in input:
-        index += 1
+    for i, c in enumerate(input):
         sum += 1  if c == '(' else -1 
         if sum < 0:
-            break
-
-    return index
+            return i + 1
     
 input = getInput(1)[0]
 print("Santa floor: {0}".format(part1()))
-print("Santa floor: {0}".format(part2()))
+print("First basement index: {0}".format(part2()))
