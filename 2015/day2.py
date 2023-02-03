9fb5b1b8b914de420633d774640800df46bf4426
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-03 19:45:28 +0100

    process input

--- 2015/day2.py
+++ 2015/day2.py
@@ -3,7 +3,12 @@ from helper import getInput
 def part1() -> int:
     return 0
 
-input = getInput(2)
-print(input)
+def input():
+    data = []
+    for r in getInput(2):
+        data += r.strip().split("x")
+    data = list(map(int, data))
+    return [data[i:i+3] for i in range(0, len(data), 3)]
 
 print("Wrapping pare square feet: {0}".format(part1()))
+print(input())
