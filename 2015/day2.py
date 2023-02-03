7f9508a78d03c74eb949adb21987b6c771b5cff2
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-03 20:00:22 +0100

    day 2 part 1

--- 2015/day2.py
+++ 2015/day2.py
@@ -1,14 +1,18 @@
 from helper import getInput
 
 def part1() -> int:
-    return 0
+    total = 0
+    for i in input():
+        total += (2 * i[0] * i[1]) + (2 * i[0] * i[2]) + (2 * i[1] * i[2]) + (i[0] * i[1])
+    return total
 
 def input():
     data = []
     for r in getInput(2):
         data += r.strip().split("x")
-    data = list(map(int, data))
-    return [data[i:i+3] for i in range(0, len(data), 3)]
+    data = list(map(int, data)) # Cast
+    data = [data[i:i+3] for i in range(0, len(data), 3)] # Grouped
+    data = list(map(sorted, data)) # Sort
+    return data
 
 print("Wrapping pare square feet: {0}".format(part1()))
-print(input())
