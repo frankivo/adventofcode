59bcb100613e16e6070187b51dcb91f1ad04af17
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-04 14:24:24 +0100

    trial and error
    how to handle lx -> a ?

--- 2015/day7.py
+++ 2015/day7.py
@@ -1,31 +1,42 @@
+import time
 import re
 from helper import getInput
 
 input = getInput(7)
 
 signals = dict()
-for i in input:
-    match = re.findall('[A-Z]+', i)
-    cmd = match[0] if len(match) else 'PROVIDE'
-    value = -1
-    values = i.split()
-    target = values[-1]
 
-    print(target)
+while 'a' not in signals:
+    # print(signals)
+    print(len(signals))
+    # time.sleep(2)
 
-    if cmd == 'PROVIDE':
-        value = int(values[0])
-    elif cmd == 'NOT':
-        value = signals.get(values[1]) ^ 65535
-    elif cmd == 'AND':
-        value = signals.get(values[0]) & signals.get(values[2])
-    elif cmd == 'OR':
-        value = signals.get(values[0]) | signals.get(values[2])
-    elif cmd == 'LSHIFT':
-        value = signals.get(values[0]) << int(values[2])
-    elif cmd == 'RSHIFT':
-        value = signals.get(values[0]) >> int(values[2])
+    for i in input:
+        match = re.findall('[A-Z]+', i)
+        cmd = match[0] if len(match) else 'PROVIDE'
+        value = -1
+        values = i.split()
+        target = values[-1]
 
-    signals.update({target: value})
+        try:
+            if cmd == 'PROVIDE':
+                value = int(values[0])
+            elif cmd == 'NOT':
+                value = signals.get(values[1]) ^ 65535
+            elif cmd == 'AND':
+                try: left = int(values[0])
+                except: left = signals.get(values[0])
+                value = left & signals.get(values[2])
+            elif cmd == 'OR':
+                value = signals.get(values[0]) | signals.get(values[2])
+            elif cmd == 'LSHIFT':
+                value = signals.get(values[0]) << int(values[2])
+            elif cmd == 'RSHIFT':
+                value = signals.get(values[0]) >> int(values[2])
+
+            signals.update({target: value})
+        except Exception as e: 
+            print(e)
+            continue
 
 print(signals)
