0b174c13bf6b629447ad00f2c4e595ce731c29d4
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-04 16:02:12 +0100

    remove most try catch

--- 2015/day7.py
+++ 2015/day7.py
@@ -9,19 +9,16 @@ while 'a' not in signals:
     for i in input:
         match = re.findall('[A-Z]+', i)
         cmd = match[0] if len(match) else 'PROVIDE'
-        value = -1
+        value = None
         values = i.split()
         target = values[-1]
 
         try:
             if cmd == 'PROVIDE':
-                try: value = int(values[0])
-                except: 
-                    try:
-                        if values[0] in signals:
-                            value = signals.get(values[0]) 
-                        else: raise
-                    except: raise
+                if values[0].isdigit():
+                    value = int(values[0])
+                elif values[0] in signals:
+                    value = signals.get(values[0]) 
             elif cmd == 'NOT':
                 value = signals.get(values[1]) ^ 65535
             elif cmd == 'AND':
@@ -34,8 +31,9 @@ while 'a' not in signals:
                 value = signals.get(values[0]) << int(values[2])
             elif cmd == 'RSHIFT':
                 value = signals.get(values[0]) >> int(values[2])
-
-            signals.update({target: value})
         except: continue
+
+        if value is not None:
+            signals.update({target: value})
 
 print(signals.get('a'))
