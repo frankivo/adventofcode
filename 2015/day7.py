02a520b7887399b525bb917d843f1e5f3e62d5a4
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-04 13:47:15 +0100

    AND OR LSHIFT RSHIFT

--- 2015/day7.py
+++ 2015/day7.py
@@ -13,16 +13,22 @@ signals = dict()
 for i in input:
     match = re.findall('[A-Z]+', i)
     cmd = match[0] if len(match) else 'PROVIDE'
+    value = -1
     values = i.split()
     target = values[-1]
 
     if cmd == 'PROVIDE':
         value = int(values[0])
     elif cmd == 'NOT':
-        source = values[1]
-        value = signals.get(source) ^ 65535
-    else:
-        value = -1
+        value = signals.get(values[1]) ^ 65535
+    elif cmd == 'AND':
+        value = signals.get(values[0]) & signals.get(values[2])
+    elif cmd == 'OR':
+        value = signals.get(values[0]) | signals.get(values[2])
+    elif cmd == 'LSHIFT':
+        value = signals.get(values[0]) << 2
+    elif cmd == 'RSHIFT':
+        value = signals.get(values[0]) >> 2
 
     signals.update({target: value})
 
