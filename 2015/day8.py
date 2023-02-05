ce4367b4200116e8e589b68ad68afac43272d631
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-05 23:59:02 +0100

    wip

--- 2015/day8.py
+++ 2015/day8.py
@@ -10,4 +10,16 @@ def part1() -> int:
     return literalLength() - evalLength()
 
 input = getInput(8)
-print(part1())
+# print(part1())
+
+for i in input:
+    extra = 4 # start + end
+    short = i[1:-1]
+    extra += short.count('\\')
+    extra += short.count('\"')
+    extra += short.count('\\x')
+    
+    print(extra)
+
+
+
