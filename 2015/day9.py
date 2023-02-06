29febc4edd1efaa51927f69cf4027943572a9f9d
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-06 22:57:49 +0100

    bfs init

--- 2015/day9.py
+++ 2015/day9.py
@@ -4,10 +4,23 @@ input = getInput(9)
 cities = dict()
 for i in input:
     split = i.split(' ')
-    source, to, dist = split[0] , split[2] , split[4]
+    source, to, dist = split[0], split[2], split[4]
 
-    target = cities[source] if source in cities else []
-    target.append({to: dist})
+    target = cities[source] if source in cities else dict()
+    target.update({to: dist})
     cities.update({source: target})
 
-print(cities)
+def bfs(start: str) -> int:
+    explorable = [start]
+    distances = []
+
+    while explorable:
+        current = explorable[0]
+        print(explorable[0])
+        explorable.remove(current)
+        if current in cities:
+            for adj in cities[current].keys():
+                if adj not in distances:
+                    explorable.append(adj)
+
+bfs('London')
