385d56ac867203df7b213deb2ca2f69e0a9d8423
Frank Oosterhuis <frank@scriptzone.nl>
2023-02-06 17:13:30 +0100

    map input

--- 2015/day9.py
+++ 2015/day9.py
@@ -1,8 +1,13 @@
 from helper import getInput
 
 input = getInput(9)
-data = []
+cities = dict()
 for i in input:
-    s = i.split(' ')
-    data.append({'from': s[0], 'to': s[2], 'distance': int(s[4])})
-print(data)
+    split = i.split(' ')
+    source, to, dist = split[0] , split[2] , split[4]
+
+    target = cities[source] if source in cities else []
+    target.append({to: dist})
+    cities.update({source: target})
+
+print(cities)
