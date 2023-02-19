bf93e755784b016b0af5bfa8481bd2373a2c9253
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-19 22:53:50 +0100

    part2 wip

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,4 +1,5 @@
 from helper import getInput
+import json
 import re
 
 input = getInput(12)[0]
@@ -7,4 +8,11 @@ def part1() -> int:
     nums = map(int,re.findall('[\d-]+', input))
     return sum(nums)
 
+def part2() -> int:
+    data = json.loads(input)
+    if 'red' in data.values() and type(data) == dict:
+        print('yes')
+
+
 print(part1())
+print(part2())
