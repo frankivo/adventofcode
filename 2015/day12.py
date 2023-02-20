d32ca6f070f322c118596bcd983e192edb031eea
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-20 12:59:29 +0100

    part 2 examples

--- 2015/day12.py
+++ 2015/day12.py
@@ -8,11 +8,28 @@ def part1() -> int:
     nums = map(int,re.findall('[\d-]+', input))
     return sum(nums)
 
+def count(obj) -> int:
+    sum = 0
+    
+    if type(obj) == dict and 'red' not in obj.values():
+        for _, v in obj.items():
+            if type(v) in (dict, list):
+                for o in v:
+                    sum += count(o)
+            elif type(v) == int:
+                sum += v
+    if type(obj) == list:
+        for v in obj:
+            if type(v) == int:
+                sum += v
+    elif type(obj) == int:
+        sum += obj
+
+    return sum
+
 def part2() -> int:
     data = json.loads(input)
-    if 'red' in data.values() and type(data) == dict:
-        print('yes')
-
+    return count(data)
 
 print(part1())
 print(part2())
