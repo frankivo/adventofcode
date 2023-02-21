f23b5420e0a69fcdd5d94837819cd54e34b8e9cc
Frank Oosterhuis <frank+github@scriptzone.nl>
2023-02-21 20:28:34 +0100

    part 1

--- 2015/day13.py
+++ 2015/day13.py
@@ -19,10 +19,17 @@ for i in input:
 def getScore(n1: str, n2: str) -> int:
     for d in data:
         if d['name1'] == n1 and d['name2'] == n2:
-            return d['score'] * 1 if d['state'] == 'gain' else -1
+            return d['score'] * (1 if d['state'] == 'gain' else -1)
 
 def totalscore(seating: list) -> int:
-    return sum([getScore(n1, list(seating)[i-1]) for i, n1 in enumerate(seating)])
+    seating = list(seating)
+    total = 0
+    for i, n1 in enumerate(seating):
+        prev = i -1
+        next = i + 1 if i < (len(names) -1) else 0
+        total += getScore(n1, seating[prev])
+        total += getScore(n1, seating[next])
+    return total
 
 def bestScore() -> int:
     return max([totalscore(n) for n in permutations(names,len(names))])
