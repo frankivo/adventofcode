e8ab490e6cc96d95343cb6af03638894e9f49e4c
Frank Oosterhuis <frank@scriptzone.nl>
2023-03-07 12:14:46 +0100

    status for all reindeer

--- 2015/day14.py
+++ 2015/day14.py
@@ -11,7 +11,7 @@ def distance(name: str) -> int:
     dist, seconds, modeSeconds, mode = 0, 0, 0, 'move'
     data = reindeer[name]
 
-    while seconds < 2503:
+    while seconds < 1000:
         if mode == 'move':
             dist += data['speed']
 
@@ -28,4 +28,24 @@ def distance(name: str) -> int:
 
 def part1() -> int:
     return max([distance(r) for r in reindeer])
+
+def alldistance():
+    empty = {'modeSeconds': 0, 'mode': 'move', 'dist': 0}
+    status = {}
+    seconds = 0
+
+    while seconds < 140:
+        seconds += 1
+        for r in reindeer:
+            data = reindeer[name]
+            rs = status[r] if r in status else empty.copy()
+            status.update({r : rs})
+
+    print(status)
+
+def part2() -> int:
+    alldistance()
+    return 0
+
 print(part1())
+print(part2())
