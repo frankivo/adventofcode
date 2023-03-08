680b9a25f8af08d48ee346047d0ac07d2ba21ba9
Frank Oosterhuis <frank@scriptzone.nl>
2023-03-08 08:26:14 +0100

    day 15 part 2

--- 2015/day14.py
+++ 2015/day14.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from helper import getInput
 from re import findall
 
@@ -14,34 +13,43 @@ def distance(name: str) -> int:
     dist, seconds, modeSeconds, mode = 0, 0, 0, 'move'
     data = reindeer[name]
 
-    while seconds <= maxSeconds:
-        if mode == 'move':
-            dist += data['speed']
-
-        seconds += 1
-        modeSeconds += 1
-
+    while seconds < maxSeconds:
         if mode == 'move' and modeSeconds == data['duration']:
             modeSeconds = 0
             mode = 'rest'
         if mode == 'rest' and modeSeconds == data['rest']:
             modeSeconds = 0
-            mode = 'move'        
+            mode = 'move'
+        if mode == 'move':
+            dist += data['speed']
+        seconds += 1
+        modeSeconds += 1
     return dist
 
 def part1() -> int:
     return max([distance(r) for r in reindeer])
 
-def bestScoring(status: dict) -> str:
-    return reduce(lambda x, y: x if status[x]['dist'] > status[y]['dist'] else y, status)
+def bestScoring(status: dict) -> list:
+    bestDist = 0
+    bestNames = []
+
+    for name, state in status.items():
+        if state['dist'] > bestDist:
+            bestNames = []
+            bestDist = state['dist']
+
+        if state['dist'] == bestDist:
+            bestNames.append(name)
+    return bestNames
 
 def alldistance() -> dict:
     empty = {'modeSeconds': 0, 'mode': 'move', 'dist': 0, 'score': 0}
     status = {}
     seconds = 0
 
-    while seconds <= maxSeconds:
-        
+    while seconds < maxSeconds:
+        seconds += 1
+
         for r in reindeer:
             data = reindeer[r]
             rs = status[r] if r in status else empty.copy()
@@ -55,21 +63,17 @@ def alldistance() -> dict:
             if rs['mode'] == 'rest' and rs['modeSeconds'] == data['rest']:
                 rs.update({'modeSeconds': 0 })
                 rs.update({'mode': 'move' })
-
             status.update({r : rs})
-        rs = status[bestScoring(status)]
-        rs.update({'score': rs['score'] + 1})
-        seconds += 1
+
+        for name in bestScoring(status):            
+            rs = status[name]
+            rs.update({'score': rs['score'] + 1})
 
     return status
     
-# 1074 too low
-# 1168 too high
 def part2() -> int:
     res = alldistance()
-    print([r['score'] for r in res.values()])
-
-    return 0
+    return max([r['score'] for r in res.values()])
 
 print(part1())
 print(part2()) 
