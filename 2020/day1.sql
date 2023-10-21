f382072b33450c71cb63998cd8068f2fca354bb2
frankivo <frankivo>
2023-10-21 20:04:53 +0000

    part 1

--- 2020/day1.sql
+++ 2020/day1.sql
@@ -1,16 +1,27 @@
 -- Databricks notebook source
-with raw as (
-select explode(split('1721
+-- MAGIC %md # AOC2020 - Day 1
+
+-- COMMAND ----------
+
+update frank.adventofcode.inputdata set example_data = '1721
 979
 366
 299
 675
-1456', '\n')) as data
+1456' where year = 2020 and day = 1 
+
+-- COMMAND ----------
+
+-- MAGIC %md ## Part 1
+
+-- COMMAND ----------
+
+with input_data as (
+  select explode(split(data, '\n')) as data from frank.adventofcode.inputdata where year = 2020 and day = 1
+  qualify rank() over (partition by year, day order by loaded desc) = 1
 ),
 
-ints as (
-  select int(data) from raw
-),
+ints as (select int(data) from input_data),
 
 combinations as (
   select distinct l.data as left, r.data as right
