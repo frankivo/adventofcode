a9c55cda76cfbb5f5c9e9502d3f84e6c574ed634
frankivo <frankivo>
2023-10-21 21:39:01 +0000

    day 2 demo

--- 2020/day2.sql
+++ 2020/day2.sql
@@ -37,3 +37,36 @@ policies as (
 select count(*) as valid from policies
 where keycount >= low and keycount <= high
 
+-- COMMAND ----------
+
+-- MAGIC %md ## Part 2
+-- MAGIC How many passwords are valid according to the new interpretation of the policies?
+
+-- COMMAND ----------
+
+with input_data as (
+  select explode(split(example_data, '\n')) as data from frank.adventofcode.inputdata where year = 2020 and day = 2
+  qualify rank() over (partition by year, day order by loaded desc) = 1
+),
+
+extract as (
+  select regexp_extract_all(data, "(\\d+|[a-z]+)") as values from input_data
+),
+
+policies as (
+  select
+    element_at(values, 1) as low,
+    element_at(values, 2) as high,
+    element_at(values, 3) as key,
+    element_at(values, 4) as password    
+  from extract
+),
+ 
+valid as (
+  select
+    *,
+    substring(password, low, 1) = key and not substring(password, high, 1) = key as valid
+  from policies
+)
+
+select * from valid
