9508e832f7239c34f7136910b07ba573f3d2cfa1
frankivo <frankivo>
2023-10-23 12:55:31 +0000

    day 2 improved?

--- 2020/day6.sql
+++ 2020/day6.sql
@@ -50,15 +50,15 @@ select sum(unique_answers) as result from answers
 -- COMMAND ----------
 
 with input_data as (
-  select explode(split(data, '\n\n')) as groups from frank.adventofcode.inputdata where year = 2020 and day = 6
-  qualify rank() over (partition by year, day order by loaded desc) = 1 
+  select explode(split(example_data, '\n\n')) as groups from frank.adventofcode.inputdata where year = 2020 and day = 6
+  qualify rank() over (partition by year, day order by loaded desc) = 1
 ),
 
 answers as (
   select
     groups,
     row_number() over (order by 1) as group_id,
-    split(groups, '\n') as group_answers
+    split(trim(groups), '\n') as group_answers
   from input_data
 ),
 
@@ -66,26 +66,25 @@ per_group as (
   select group_id, explode(group_answers) as answers from answers
 ), 
 
-group_ansers as (
+group_answers as (
   select
     group_id,
-    row_number() over (partition by group_id order by 1 desc) as answer_id,
+    row_number() over (partition by group_id order by answers) as answer_id,
     split(answers, '') as answers
   from per_group
 ), 
 
-intersect as (
+unique_per_group as (
   select
     group_id,
     answer_id,
     answers,
-    lag(answers) over (partition by group_id order by 1) as prev,
-    array_intersect(answers, coalesce(prev, answers)) as unique
-  from group_ansers
-),
-
-unique_per_group as (
-  select group_id, size(last(unique)) as unique from intersect group by all
+    lag(answers) over (partition by group_id order by answer_id desc) as prev,
+    array_intersect(answers, coalesce(prev, answers)) as unique,
+    size(unique) as unique_count
+  from group_answers
 )
 
-select sum(unique) as result from unique_per_group
+select sum(unique_count) as result
+from unique_per_group
+where answer_id = 1
