81da3aaef378c09596de94d9aed09e3c17aece46
frankivo <frankivo>
2023-10-23 10:31:19 +0000

    filter empty input

--- 2020/day5.sql
+++ 2020/day5.sql
@@ -20,52 +20,52 @@ where year = 2020 and day = 5
 
 
 with input_data as (
-  select explode(split(example_data, '\n')) as data from frank.adventofcode.inputdata where year = 2020 and day = 5
-  qualify rank() over (partition by year, day order by loaded desc) = 1
+  select explode(split(data, '\n')) as lines from frank.adventofcode.inputdata where year = 2020 and day = 5
+  qualify rank() over (partition by year, day order by loaded desc) = 1 and lines <> ''
 ),
 
 selector as (
   select
-    data,
+    lines,
     sequence(0, 127) as rows,
-    case when substring(data, 1, 1) = 'F'
+    case when substring(lines, 1, 1) = 'F'
       then filter(rows, i -> i < (array_min(rows) + array_max(rows)) / 2 )
       else filter(rows, i -> i > (array_min(rows) + array_max(rows)) / 2 )
     end as rs1, -- Step 1
-    case when substring(data, 2, 1) = 'F'
+    case when substring(lines, 2, 1) = 'F'
       then filter(rs1, i -> i < (array_min(rs1) + array_max(rs1)) / 2 )
       else filter(rs1, i -> i > (array_min(rs1) + array_max(rs1)) / 2 )
     end as rs2,
-    case when substring(data, 3, 1) = 'F'
+    case when substring(lines, 3, 1) = 'F'
       then filter(rs2, i -> i < (array_min(rs2) + array_max(rs2)) / 2 )
       else filter(rs2, i -> i > (array_min(rs2) + array_max(rs2)) / 2 )
     end as rs3,
-    case when substring(data, 4, 1) = 'F'
+    case when substring(lines, 4, 1) = 'F'
       then filter(rs3, i -> i < (array_min(rs3) + array_max(rs3)) / 2 )
       else filter(rs3, i -> i > (array_min(rs3) + array_max(rs3)) / 2 )
     end as rs4,
-    case when substring(data, 5, 1) = 'F'
+    case when substring(lines, 5, 1) = 'F'
       then filter(rs4, i -> i < (array_min(rs4) + array_max(rs4)) / 2 )
       else filter(rs4, i -> i > (array_min(rs4) + array_max(rs4)) / 2 )
     end as rs5,
-    case when substring(data, 6, 1) = 'F'
+    case when substring(lines, 6, 1) = 'F'
       then filter(rs5, i -> i < (array_min(rs5) + array_max(rs5)) / 2 )
       else filter(rs5, i -> i > (array_min(rs5) + array_max(rs5)) / 2 )
     end as rs6,
-    case when substring(data, 7, 1) = 'F'
+    case when substring(lines, 7, 1) = 'F'
       then array_min(rs6)
       else array_max(rs6)
     end as row,
     sequence(0, 7) as columns,
-    case when substring(data, 8, 1) = 'L'
+    case when substring(lines, 8, 1) = 'L'
       then filter(columns, i -> i < (array_min(columns) + array_max(columns)) / 2 )
       else filter(columns, i -> i > (array_min(columns) + array_max(columns)) / 2 )
     end as cs1, -- Step 1
-    case when substring(data, 9, 1) = 'L'
+    case when substring(lines, 9, 1) = 'L'
       then filter(cs1, i -> i < (array_min(cs1) + array_max(cs1)) / 2 )
       else filter(cs1, i -> i > (array_min(cs1) + array_max(cs1)) / 2 )
     end as cs2,
-    case when substring(data, 10, 1) = 'L'
+    case when substring(lines, 10, 1) = 'L'
       then array_min(cs2)
       else array_max(cs2)
     end as column,
