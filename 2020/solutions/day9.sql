101d328c30c5f9221b935455d7d86e425ec49756
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-11-03 11:33:25 +0100

    validator

--- 2020/solutions/day9.sql
+++ 2020/solutions/day9.sql
@@ -7,29 +7,47 @@ with src_data as (
     where day = 9
 ),
 
+numbers as (
+    select
+        row_number() over (order by 1) as id,
+        cast(src as int)               as num
+    from src_data
+),
+
 -- Part 1: What is the first number that does not have this property?
 part1 as (
-    with recursive with_id as (
-        select
-            row_number() over (order by 1) as id,
-            src                            as num
-        from src_data
-    ),
-
-    lookback (id, num) as (
+    with recursive lookback (id, num) as (
         select
             id,
             num
-        from with_id where id <= 5
+        from numbers where id <= 5
         union
         select
             w.id,
             w.num
-        from with_id as w
+        from numbers as w
         where w.id = (select max(id) + 1 from lookback)
     )
 
     select * from lookback
+),
+
+prev as (
+    select w.num from numbers as cur
+    left join numbers as w
+        on w.id >= (cur.id - 5) and cur.id > w.id
+    where cur.id = 6
+),
+
+sums as (
+    select distinct a.num + b.num as sum
+    from prev as a
+    cross join prev as b
+    where a.num != b.num -- TODO: remove probably
 )
 
-select * from part1
+select
+    w.*,
+    exists(select s.sum from sums as s where s.sum = w.num) as valid
+from numbers as w
+where w.id = 6
