1ae4e4d6bb4e1194ec3b4cc99c1a55d81fb34170
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-11-19 12:14:21 +0100

    pathfinder wip

--- 2020/solutions/day10.sql
+++ 2020/solutions/day10.sql
@@ -7,10 +7,12 @@ with src_data as (
     where day = 10
 ),
 
+jolts as (select cast(src as int) as jolt from src_data),
+
 -- Part 1: What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
 part1 as (
-    with jolts as (
-        select cast(src as int) as jolt from src_data
+    with jolt_with_start as (
+        select jolt from jolts
         union
         select 0
     ),
@@ -20,7 +22,7 @@ part1 as (
             jolt,
             lead(jolt, 1) over (order by jolt) as next_jolt,
             coalesce(next_jolt - jolt, 3)      as dist
-        from jolts
+        from jolt_with_start
     ),
 
     sums as (
@@ -38,7 +40,43 @@ part1 as (
 
 -- Part 2: What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
 part2 as (
-    select 1
+    with recursive jolts_with_target as (
+        select jolt
+        from jolts
+        union
+        select max(jolt) + 3
+        from jolts
+        union
+        select 0
+    ),
+
+    egde as (
+        select
+            jolt                            as node1id,
+            lead(jolt) over (order by jolt) as node2id
+        from jolts_with_target
+    ),
+
+    paths (startNode, endNode, path) as (
+        select
+            node1id,
+            node2id,
+            [node1id, node2id]
+        from egde where node1id = 0
+
+        union all
+
+        select
+            paths.startNode,
+            egde.node2id,
+            list_append(paths.path, egde.node2id)
+        from paths
+        join egde on paths.endNode = egde.node1id
+        -- where egde.node2id != all(paths.path)
+
+    )
+
+    select * from paths
 )
 
-select * from part1
+select * from part2
