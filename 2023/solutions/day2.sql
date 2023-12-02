87871d56b40d53dbe514b16f1d72ee78c3d64000
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-12-02 11:55:14 +0100

    part 2

--- 2023/solutions/day2.sql
+++ 2023/solutions/day2.sql
@@ -17,7 +17,9 @@ parser as (
     from src_data
 ),
 
--- Part 1: What is the sum of the IDs of those games?
+-- Part 1: Determine which games would have been possible if the bag had been loaded 
+-- with only 12 red cubes, 13 green cubes, and 14 blue cubes.
+-- What is the sum of the IDs of those games?
 part1 as (
     with validator as (
         select game_id
@@ -32,6 +34,18 @@ part1 as (
         1            as part,
         sum(game_id) as result
     from validator
+),
+
+-- For each game, find the minimum set of cubes that must have been present.
+-- What is the sum of the power of these sets?
+part2 as (
+    select
+        2                                                     as part,
+        sum(list_max(red) * list_max(green) * list_max(blue)) as result
+    from parser
 )
 
 select * from part1
+union
+select * from part2
+order by part
