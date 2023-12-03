35434b5215bd7284d0c4c564a8ae21e34dd8a746
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-12-03 12:31:31 +0100

    part 1 demo

--- 2023/solutions/day3.sql
+++ 2023/solutions/day3.sql
@@ -9,14 +9,28 @@ with src_data as (
 
 parser as (
     select
-        row_number() over (order by 1) as row,
-        src,
-        regexp_extract_all(src, '\d+') as numbers,
-        unnest(numbers)                as number,
-        strpos(src, number)            as pos_left,
-        pos_left + length(number) - 1  as pos_right
+        lag(src) over (order by 1)                       as l1,
+        src                                              as l2,
+        lead(src) over (order by 1)                      as l3,
+        regexp_extract_all(src, '\d+')                   as numbers,
+        cast(unnest(numbers) as int)                     as number,
+        strpos(src, number)                              as pos_left,
+        pos_left + length(number) - 1                    as pos_right,
+        pos_left - 1                                     as search_left,
+        length(number) + 2                               as search_length,
+        substring(l1, search_left, search_length)        as prev,
+        substring(l2, search_left, search_length)        as cur,
+        substring(l3, search_left, search_length)        as next,
+        list_has_any(split(prev, ''), split('*+#$', '')) as prev_valid,
+        list_has_any(split(cur, ''), split('*+#$', ''))  as cur_valid,
+        list_has_any(split(next, ''), split('*+#$', '')) as next_valid,
+        prev_valid or cur_valid or next_valid            as valid
     from src_data
 )
 
 -- Part 1: What is the sum of all of the part numbers in the engine schematic?
-select * from parser
+select
+    1           as part,
+    sum(number) as result
+from parser
+where valid
