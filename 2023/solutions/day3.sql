3adba795e823cfa93706a9cffb1d56e900378c8b
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-12-03 13:34:40 +0100

    cleanup

--- 2023/solutions/day3.sql
+++ 2023/solutions/day3.sql
@@ -7,37 +7,34 @@ with src_data as (
     where day = 3
 ),
 
-numbered as (
-    select
-        row_number() over (order by 1) as id,
-        src
-    from src_data
-),
+-- Part 1: What is the sum of all of the part numbers in the engine schematic?
+part1 as (
+    with parser as (
+        select
+            lag(src) over (order by 1)                                    as l1,
+            src                                                           as l2,
+            lead(src) over (order by 1)                                   as l3,
+            regexp_extract_all(src, '\d+')                                as numbers,
+            cast(unnest(numbers) as int)                                  as number,
+            concat('(^|[^0-9])', number, '([^0-9]|$)')                    as search_string,
+            length(list_first(string_split_regex(l2, search_string))) + 2 as pos_left,
+            pos_left - 1                                                  as search_left,
+            length(number) + 2                                            as search_length,
+            substring(l1, search_left, search_length)                     as prev,
+            substring(l2, search_left, search_length)                     as cur,
+            substring(l3, search_left, search_length)                     as next,
+            regexp_matches(prev, '[^0-9.]')                               as prev_valid,
+            regexp_matches(cur, '[^0-9.]')                                as cur_valid,
+            regexp_matches(next, '[^0-9.]')                               as next_valid,
+            prev_valid or cur_valid or next_valid                         as valid
+        from src_data
+    )
 
-parser as (
     select
-        lag(src) over (order by 1)                                    as l1,
-        src                                                           as l2,
-        lead(src) over (order by 1)                                   as l3,
-        regexp_extract_all(src, '\d+')                                as numbers,
-        cast(unnest(numbers) as int)                                  as number,
-        concat('(^|[^0-9])', number, '([^0-9]|$)')                    as search_string,
-        length(list_first(string_split_regex(l2, search_string))) + 2 as pos_left,
-        pos_left - 1                                                  as search_left,
-        length(number) + 2                                            as search_length,
-        substring(l1, search_left, search_length)                     as prev,
-        substring(l2, search_left, search_length)                     as cur,
-        substring(l3, search_left, search_length)                     as next,
-        regexp_matches(prev, '[^0-9.]')                               as prev_valid,
-        regexp_matches(cur, '[^0-9.]')                                as cur_valid,
-        regexp_matches(next, '[^0-9.]')                               as next_valid,
-        prev_valid or cur_valid or next_valid                         as valid
-    from src_data
+        1           as part,
+        sum(number) as result
+    from parser
+    where valid
 )
 
--- Part 1: What is the sum of all of the part numbers in the engine schematic?
-select
-    1           as part,
-    sum(number) as result
-from parser
-where valid
+select * from part1
