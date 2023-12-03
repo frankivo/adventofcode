45a31c3ad247eeb502b0015e6e0caa68981e3f06
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-12-03 13:59:21 +0100

    part 2 init

--- 2023/solutions/day3.sql
+++ 2023/solutions/day3.sql
@@ -7,14 +7,19 @@ with src_data as (
     where day = 3
 ),
 
+lead_lag as (
+    select
+        lag(src) over (order by 1)  as l1,
+        src                         as l2,
+        lead(src) over (order by 1) as l3
+    from src_data
+),
+
 -- Part 1: What is the sum of all of the part numbers in the engine schematic?
 part1 as (
     with parser as (
         select
-            lag(src) over (order by 1)                                    as l1,
-            src                                                           as l2,
-            lead(src) over (order by 1)                                   as l3,
-            regexp_extract_all(src, '\d+')                                as numbers,
+            regexp_extract_all(l2, '\d+')                                 as numbers,
             cast(unnest(numbers) as int)                                  as number,
             concat('(^|[^0-9])', number, '([^0-9]|$)')                    as search_string,
             length(list_first(string_split_regex(l2, search_string))) + 2 as pos_left,
@@ -27,7 +32,7 @@ part1 as (
             regexp_matches(cur, '[^0-9.]')                                as cur_valid,
             regexp_matches(next, '[^0-9.]')                               as next_valid,
             prev_valid or cur_valid or next_valid                         as valid
-        from src_data
+        from lead_lag
     )
 
     select
@@ -35,6 +40,16 @@ part1 as (
         sum(number) as result
     from parser
     where valid
+),
+
+part2 as (
+    select
+        regexp_extract_all(l2, '\*') as stars,
+        range(1, length(stars) + 1)      as count,
+        unnest(count)                as star,
+        length(string_split(l2, '*')[star]) + 1 as pos_left,
+        ''
+    from lead_lag
 )
 
-select * from part1
+select * from part2
