d6e935cbe604bcb258a68b2f0a9aa3fe9b439e7f
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-12-04 17:27:19 +0100

    cleanup

--- 2023/solutions/day4.sql
+++ 2023/solutions/day4.sql
@@ -7,21 +7,22 @@ with src_data as (
     where day = 4
 ),
 
-parser as (
-    select
-        split(split(src, ':')[2], '|')                          as card_info,
-        cast(regexp_extract_all(card_info[1], '\d+') as int []) as winning_numbers,
-        cast(regexp_extract_all(card_info[2], '\d+') as int []) as card_numbers,
-        length(list_intersect(winning_numbers, card_numbers))   as winning,
-        cast(case
-            when winning in (0, 1) then winning
-            else pow(2, winning) / 2
-        end as int)                                             as score
+-- Part 1: How many points are they worth in total?
+part1 as (
+    with parser as (
+        select
+            split(split(src, ':')[2], '|')                          as card_info,
+            cast(regexp_extract_all(card_info[1], '\d+') as int []) as winning_numbers,
+            cast(regexp_extract_all(card_info[2], '\d+') as int []) as card_numbers,
+            length(list_intersect(winning_numbers, card_numbers))   as winning,
+            cast(pow(2, winning - 1) as int)                        as score
+        from src_data
+    )
 
-    from src_data
+    select
+        1          as part,
+        sum(score) as result
+    from parser
 )
 
-select
-    1          as part,
-    sum(score) as result
-from parser
+select * from part1
