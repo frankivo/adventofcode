15a424120e8612b193b3c0868b3243d61a590e6f
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-13 12:09:07 +0200

    cleanup

--- 2023/solutions/day1.py
+++ 2023/solutions/day1.py
@@ -1,4 +1,3 @@
-import input
 import re
 
 def part1(input: list[str]) -> None:
@@ -6,12 +5,12 @@ def part1(input: list[str]) -> None:
     print(sum([int(d[0] + d[-1]) for d in digits]))
 
 def part2(input: list[str]) -> None:
-    digits = [re.findall("(?=(\d|one|two|three|four|five|six|seven|eight|nine))", i) for i in input]
+    search = "(?=(\d|" + "|".join(one_to_nine) + "))"
+    digits = [re.findall(search, i) for i in input]
     numbers = [[to_number(j) for j in i] for i in digits]
     print(sum([int(str(d[0]) + str(d[-1])) for d in numbers]))
 
 def to_number(digit: str) -> int:
-    if digit.isdigit():
-        return digit
-    else:
-        return ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].index(digit) + 1
+    return digit if digit.isdigit() else one_to_nine.index(digit) + 1
+
+one_to_nine = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
