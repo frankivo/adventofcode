88aa84f914663f7e145e56dcd41fd40bb02e4f86
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-13 11:17:04 +0200

    part2

--- 2023/solutions/day1.py
+++ 2023/solutions/day1.py
@@ -2,7 +2,19 @@ import re
 
 def run(input: list[str]) -> None:
     part1(input)
+    part2(input)
 
 def part1(input: list[str]) -> None:
     digits = [re.findall("\d", i) for i in input]
     print(sum([int(d[0] + d[-1]) for d in digits]))
+
+def part2(input: list[str]) -> None:
+    digits = [re.findall("(?=(\d|one|two|three|four|five|six|seven|eight|nine))", i) for i in input]
+    numbers = [[to_number(j) for j in i] for i in digits]
+    print(sum([int(str(d[0]) + str(d[-1])) for d in numbers]))
+
+def to_number(digit: str) -> int:
+    if digit.isdigit():
+        return digit
+    else:
+        return ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].index(digit) + 1
