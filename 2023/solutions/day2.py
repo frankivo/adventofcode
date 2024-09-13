e2e5c0ee6dbdffd16e26f307da2fa4272f686630
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-13 15:25:10 +0200

    part1

--- 2023/solutions/day2.py
+++ 2023/solutions/day2.py
@@ -10,28 +10,33 @@ def part1(input: list[str]) -> None:
 
 class game:
     def __init__(self, raw: str) -> None:
-        self.marbles = game.game_count(raw)
+        self.set_counts = game.game_count(raw)
         self.id = game.game_id(raw)
 
     @staticmethod
-    def game_id(raw: str) -> None:
+    def game_id(raw: str) -> int:
         return int(re.search(r'(Game\s(\d+):)', raw).group(2))
 
     @staticmethod
-    def game_count(raw: str) -> None:
-        result = {}
+    def game_count(raw: str) -> dict:
+        return [ game.set_count(set) for set in raw.split(";") ]
 
+    @staticmethod
+    def set_count(raw: str) -> dict:
+        result = {}
         for m in re.finditer(r'(\d+)\s*(\w+)', raw):
             colour = m.group(2)
             count = int(m.group(1))
 
             old = result.get(colour, 0)
             result.update ( { colour : count + old } )
-
         return result
 
     def valid(self) -> bool:
-        return self.marbles["blue"] <= 14 and self.marbles["green"] <=13 and self.marbles["red"] <=12
+        for s in self.set_counts:
+            if not (s.get("blue", 0) <= 14 and s.get("green", 0) <= 13 and s.get("red", 0) <= 12):
+                return False
+        return True
 
 def part2(input: list[str]) -> None:
     pass
