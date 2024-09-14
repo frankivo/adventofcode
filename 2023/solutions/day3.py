55035505f9a8bf93ba28cb6519ac32da466f6def
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-14 15:41:13 +0200

    fix smaller substrings (finditer), fix matching newlines

--- 2023/solutions/day3.py
+++ 2023/solutions/day3.py
@@ -11,44 +11,42 @@ def part1(input: list[str]) -> None:
 
     for i in range(field_height):
         line = input[i]
-        numbers = re.findall(r"\d+", line)
+        numbers = re.finditer(r"\d+", line)
 
-        for n in numbers:
-            pos_left = line.index(n)
+        for ni in numbers:
+            n = ni.group()
+            pos_left = ni.start()
             pos_right = pos_left + len(n)
-
             found_symbol = False
 
             # Left
-            if pos_left > 0 and has_symbol(line[pos_left - 1 : pos_left]): 
+            if has_symbol(line[guard_left(pos_left - 1) : pos_left]): 
                 found_symbol = True
             # Right
-            if pos_right < field_width and has_symbol(line[pos_right : pos_right + 1]):
+            if has_symbol(line[pos_right : guard_right(pos_right + 1, field_width)]):
                 found_symbol = True
             # Above
             if i > 0:
                 prev_line = input[i - 1]
-                if has_symbol(prev_line[pos_left - 1 : pos_right + 1]):
+                if has_symbol(prev_line[guard_left(pos_left - 1) : guard_right(pos_right + 1, field_width)]):
                     found_symbol = True
             # Under
             if i < field_height - 1:
                 next_line = input[i + 1]
                 if has_symbol(next_line[guard_left(pos_left - 1) : guard_right(pos_right + 1, field_width)]):
                     found_symbol = True
-
             if found_symbol:
                 found.append(int(n))
-    
     print(sum(found))
 
 def guard_left(num: int) -> int:
     return max(0, num)
 
 def guard_right(num: int, width: int) -> int:
-    return min(width, num)
+    return min(width - 1, num)
 
-def has_symbol(data: str) -> bool:
-    return len(re.findall(r"[^\d.]", data)) > 0
+def has_symbol(input: str) -> bool:
+    return len(re.findall(r"[^\d.]", input)) > 0
 
 def part2(input: list[str]) -> None:
     pass
