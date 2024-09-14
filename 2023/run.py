49635626c3f1354b36da88dab3cc6d121bd5355a
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-14 21:23:48 +0200

    2023 day3 part2 (#57)
    * run only one part
    
    * part2
    
    * clean

--- 2023/run.py
+++ 2023/run.py
@@ -5,14 +5,18 @@ import argparse
 parser = argparse.ArgumentParser(description='Run Advent of Code solutions')
 parser.add_argument('-d', '--day', type=int, help='Day to run', required=True)
 parser.add_argument('--demo', action=argparse.BooleanOptionalAction, help='Use demo data', default=False)
+parser.add_argument('--p1', '-1', action=argparse.BooleanOptionalAction, help='Run only part 1', default=False)
+parser.add_argument('--p2', '-2', action=argparse.BooleanOptionalAction, help='Run only part 2', default=False)
 
 args = parser.parse_args()
 
 def main():
     data = input(day=args.day, demo=args.demo)
 
-    globals()[f"day{args.day}"].part1(data.get(part=1))
-    globals()[f"day{args.day}"].part2(data.get(part=2))
+    if not args.p2:
+        globals()[f"day{args.day}"].part1(data.get(part=1))
+    if not args.p1:
+        globals()[f"day{args.day}"].part2(data.get(part=2))
 
 if __name__ == '__main__':
     main()
--- 2023/solutions/day3.py
+++ 2023/solutions/day3.py
@@ -1,10 +1,11 @@
 # --- Day 3: Gear Ratios ---
 # https://adventofcode.com/2023/day/3
 
+from itertools import chain
 import re
 
 def part1(input: list[str]) -> None:
-    field_width, field_height, found = len(input[0]), len(input), []
+    field_width, field_height, total = len(input[0]), len(input), 0
 
     def guard(num: int, limit: int) -> int:
         return max(0, min(limit - 1, num))
@@ -22,9 +23,21 @@ def part1(input: list[str]) -> None:
                 input[i + 1][guard(pos_left - 1, field_width): guard(pos_right + 1, field_width)] if i < field_height - 1 else ""
             ]
             if any(has_symbol(c) for c in checks):
-                found.append(int(n))
-
-    print(sum(found))
+                total += (int(n))
+    print(total)
 
 def part2(input: list[str]) -> None:
-    pass
+    total = 0
+    for i, line in enumerate(input):
+        for ni in re.finditer(r"\*", line):
+            pos = ni.start()
+            lines = [
+                input[i -1 ] if i > 0 else "",
+                line,
+                input[i + 1] if i < len(input) else ""
+            ]
+            numbers = chain(*[list(re.finditer(r"\d+", l)) for l in lines])
+            adj = [int(n.group()) for n in numbers if pos - len(n.group()) <= n.start() <= pos + 1]
+            if len(adj) == 2:
+                total += adj[0] * adj[1]
+    print(total)
