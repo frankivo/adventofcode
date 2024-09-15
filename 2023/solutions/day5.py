f4b6baa3d4f3d2a4fd8678056a55788eaaea2900
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-15 12:56:54 +0200

    init

---
+++ 2023/solutions/day5.py
@@ -0,0 +1,10 @@
+# --- Day 5: If You Give A Seed A Fertilizer ---
+# https://adventofcode.com/2023/day/5
+
+
+def part1(data: list[str]) -> None:
+    pass
+
+
+def part2(data: list[str]) -> None:
+    pass
