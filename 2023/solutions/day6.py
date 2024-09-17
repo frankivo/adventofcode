ea06f1245c5cea449ae8caddae8f8b9390ef6b9f
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-17 20:31:21 +0200

    part2

--- 2023/solutions/day6.py
+++ 2023/solutions/day6.py
@@ -7,21 +7,26 @@ import re
 
 
 def part1(data: data) -> None:
-    game_data = data.getlines(part=1)
-
-    def get_distances(limit: int) -> iter:
-        for speed in range(limit + 1):
-            yield (limit - speed) * speed
-
-    def get_wins(limit: int, dist: int) -> int:
-        return len([d for d in get_distances(limit) if d > dist])
-
+    game_data = data.getlines()
     t = list(map(int, re.findall(r"\d+", game_data[0])))
     d = list(map(int, re.findall(r"\d+", game_data[1])))
 
-    wins = [ get_wins(t[i], d[i]) for i in range(len(t)) ]
-    print(reduce((lambda x, y: x *y), wins))
+    wins = [get_wins(t[i], d[i]) for i in range(len(t))]
+    print(reduce((lambda x, y: x * y), wins))
 
 
 def part2(data: data) -> None:
-    pass
+    game_data = data.getlines()
+    t = int("".join(re.findall(r"\d+", game_data[0])))
+    d = int("".join(re.findall(r"\d+", game_data[1])))
+
+    print(get_wins(t, d))
+
+
+def get_distances(lower, upper) -> iter:
+    for speed in range(lower, upper + 1):
+        yield (upper - speed) * speed
+
+
+def get_wins(limit: int, dist: int) -> int:
+    return len([d for d in get_distances(0, limit) if d > dist])
