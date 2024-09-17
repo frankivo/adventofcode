24f7d3113df9ad5b42384685f2c1bde4f0971511
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-17 13:59:51 +0200

    shorten

--- 2023/solutions/day5.py
+++ 2023/solutions/day5.py
@@ -1,7 +1,6 @@
 # --- Day 5: If You Give A Seed A Fertilizer ---
 # https://adventofcode.com/2023/day/5
 
-from sys import maxsize as int_maxsize
 from data import data
 import re
 
@@ -29,26 +28,22 @@ def part2(data: data) -> None:
 
 
 def get_minimum_location(data: list[str], seeds: iter) -> int:
-    seed_to_soil = list(get_data(data, "seed-to-soil"))
-    soil_to_fertilizer = list(get_data(data, "soil-to-fertilizer"))
-    fertilizer_to_water = list(get_data(data, "fertilizer-to-water"))
-    water_to_light = list(get_data(data, "water-to-light"))
-    light_to_temperature = list(get_data(data, "light-to-temperature"))
-    temperature_to_humidity = list(get_data(data, "temperature-to-humidity"))
-    humidity_to_location = list(get_data(data, "humidity-to-location"))
+    maps = [
+        list(get_data(data, "seed-to-soil")),
+        list(get_data(data, "soil-to-fertilizer")),
+        list(get_data(data, "fertilizer-to-water")),
+        list(get_data(data, "water-to-light")),
+        list(get_data(data, "light-to-temperature")),
+        list(get_data(data, "temperature-to-humidity")),
+        list(get_data(data, "humidity-to-location")),
+    ]
 
-    location = int_maxsize
-
-    for seed in seeds:
-        soil = get(seed_to_soil, seed)
-        fert = get(soil_to_fertilizer, soil)
-        water = get(fertilizer_to_water, fert)
-        light = get(water_to_light, water)
-        temp = get(light_to_temperature, light)
-        hum = get(temperature_to_humidity, temp)
-        loc = get(humidity_to_location, hum)
-        if loc < location:
-            location = loc
+    location = None
+    for nr in seeds:
+        for m in maps:
+            nr = get(m, nr)
+        if not location or nr < location:
+            location = nr
     return location
 
 
