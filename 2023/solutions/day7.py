9545bd051a4cd0ae27b8fa7edf6b4cfdbbd1b292
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-19 00:30:10 +0200

    p2

--- 2023/solutions/day7.py
+++ 2023/solutions/day7.py
@@ -11,13 +11,15 @@ CARDS_J = {c: i for i, c in enumerate(DECK_J)}
 
 
 def part1(data: data) -> None:
-    hands = [hand(sub, False) for sub in data.getlines()]
-    scores = [h.bid * (r) for r, h in enumerate(sorted(hands), 1)]
-    print(sum(scores))
+    solve(data, False)
 
 
 def part2(data: data) -> None:
-    hands = [hand(sub, True) for sub in data.getlines()]
+    solve(data, True)
+
+
+def solve(data: data, jokers: bool) -> None:
+    hands = [hand(sub, jokers) for sub in data.getlines()]
     scores = [h.bid * (r) for r, h in enumerate(sorted(hands), 1)]
     print(sum(scores))
 
@@ -31,20 +33,20 @@ class hand:
         self.bid = int(tmp[1])
 
     def rank(self) -> int:
-        cards = hand.joker(self.cards) if self.jokers else self.cards
-        c = sorted([cards.count(c) for c in set(cards)])
+        jokers = self.cards.count("J") if self.jokers else 0
+        c = sorted([self.cards.count(c) for c in set(self.cards) if c != "J" or not self.jokers], reverse=True)
 
-        if 5 in c:  # Five of a kind
+        if not c or c[0] + jokers == 5:  # Five of a kind
             return 7
-        if 4 in c:  # Four of a kind
+        if c[0] + jokers == 4:  # Four of a kind
             return 6
-        if 2 in c and 3 in c:  # Full house
+        if c[0] + jokers == 3 and c[1] == 2:  # Full house
             return 5
-        if 3 in c:  # Three of a kind
+        if c[0] + jokers == 3:  # Three of a kind
             return 4
-        if c[0] == 1 and c[1] == 2 and c[2] == 2:  # Two pair
+        if c[0] == 2 and c[1] == 2:  # Two pair
             return 3
-        if c[-1] == 2:  # One pair
+        if c[0] + jokers == 2:  # One pair
             return 2
         return 1
 
