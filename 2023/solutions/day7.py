49fcf0c7ebfabd565cf1f886a4015134e20b5a79
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-09-19 00:31:49 +0200

    2023 part 2 (#63)
    * demo, sometimes
    
    * p2
    
    * clean

--- 2023/solutions/day7.py
+++ 2023/solutions/day7.py
@@ -3,47 +3,55 @@
 
 from data import data
 
-CARDS = {c: i for i, c in enumerate(reversed("AKQJT98765432"))}
+
+DECK = "23456789TJQKA"
+DECK_J = "J" + DECK.replace("J", "")
+CARDS = {c: i for i, c in enumerate(DECK)}
+CARDS_J = {c: i for i, c in enumerate(DECK_J)}
 
 
 def part1(data: data) -> None:
-    hands = [hand(sub) for sub in data.getlines()]
+    solve(data, False)
+
+
+def part2(data: data) -> None:
+    solve(data, True)
+
+
+def solve(data: data, jokers: bool) -> None:
+    hands = [hand(sub, jokers) for sub in data.getlines()]
     scores = [h.bid * (r) for r, h in enumerate(sorted(hands), 1)]
     print(sum(scores))
 
 
-def part2(data: data) -> None:
-    pass
-
-
 class hand:
-    def __init__(self, raw: str) -> None:
+    def __init__(self, raw: str, jokers: bool) -> None:
         tmp = raw.split(" ")
         self.cards = tmp[0]
         self.bid = int(tmp[1])
-
-    def __str__(self) -> str:
-        return self.cards
+        self.jokers = jokers
 
     def rank(self) -> int:
-        c = sorted([self.cards.count(c) for c in set(self.cards)])
+        jokers = self.cards.count("J") if self.jokers else 0
+        c = sorted([self.cards.count(c) for c in set(self.cards) if c != "J" or not self.jokers], reverse=True)
 
-        if 5 in c:  # Five of a kind
+        if not c or c[0] + jokers == 5:  # Five of a kind
             return 7
-        if 4 in c:  # Four of a kind
+        if c[0] + jokers == 4:  # Four of a kind
             return 6
-        if 2 in c and 3 in c:  # Full house
+        if c[0] + jokers == 3 and c[1] == 2:  # Full house
             return 5
-        if 3 in c:  # Three of a kind
+        if c[0] + jokers == 3:  # Three of a kind
             return 4
-        if c[0] == 1 and c[1] == 2 and c[2] == 2:  # Two pair
+        if c[0] == 2 and c[1] == 2:  # Two pair
             return 3
-        if c[-1] == 2:  # One pair
+        if c[0] + jokers == 2:  # One pair
             return 2
         return 1
 
     def card_score(self, pos: int) -> int:
-        return CARDS[self.cards[pos]]
+        cards = CARDS_J if self.jokers else CARDS
+        return cards[self.cards[pos]]
 
     def __gt__(self, other):
         if self.rank() != other.rank():
