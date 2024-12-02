d430c1bccbe3ed1bf86c1a2c291e05e433b61854
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-12-02 12:35:02 +0100

    part1

--- 2024/solutions/day2.py
+++ 2024/solutions/day2.py
@@ -13,18 +13,20 @@ def part1(data: data) -> None:
         increasing = None
 
         for i in range(len(report) + 1):
-            slice = report[i : i + 2]
+            slice = list(map(int, report[i : i + 2]))
 
             if len(slice) != 2:  # Ignore edge
                 continue
             if increasing == None:  # Determine direction
                 increasing = slice[1] > slice[0]
-            elif increasing and slice[1] < slice[0]:  # Check direction
-                safe = False
-            if not 1 <= abs(int(slice[0]) - int(slice[1])) <= 3:  # Check steps
+            else:  # Check direction
+                if increasing and slice[1] < slice[0]:
+                    safe = False
+                if not increasing and slice[1] > slice[0]:
+                    safe = False
+            if not 1 <= abs(slice[0] - slice[1]) <= 3:  # Check steps
                 safe = False
 
         if safe:
             safeCount += 1
-
     print(safeCount)
