c57c9f5aec6c3c645fca0179604ef4f9ab4bae32
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-12-03 12:05:53 +0100

    demodata p2

--- 2024/solutions/day3.py
+++ 2024/solutions/day3.py
@@ -6,6 +6,11 @@ import re
 
 
 def part1(data: data) -> None:
-    matches = re.findall(r"(mul\((\d+),(\d+)\))", data.get())
+    matches = re.findall(r"(mul\((\d+),(\d+)\))", data.get(part=1))
     muls = [int(m[1]) * int(m[2]) for m in matches]
     print(sum(muls))
+
+
+def part2(data: data) -> None:
+    matches = re.match(r"(mul\((\d+),(\d+)\))", data.get(part=2))
+    print(matches)
