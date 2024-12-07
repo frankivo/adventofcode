3eaa94a56a3f77a83e82f639897d618d56187500
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-12-07 16:38:06 +0100

    part2 demo

--- 2024/solutions/day7.py
+++ 2024/solutions/day7.py
@@ -17,7 +17,7 @@ def part1(data: data) -> None:
 def part2(data: data) -> None:
     cali_res = 0
     for test_value, operators in parse(data.getlines()):
-        res = solve(operators, target=test_value)
+        res = solve(operators, target=test_value, concat=True)
         if res:
             cali_res += test_value
     print(cali_res)
@@ -29,19 +29,20 @@ def parse(input: list[str]) -> iter:
         yield int(test_value), list(map(int, operators_raw.strip().split(" ")))
 
 
-def solve(numbers: list[int], target: int, res: int = 0, depth: int = 0, results=None) -> bool:
-    # print(target, depth, res)
+def solve(numbers: list[int], target: int, res: int = 0, depth: int = 0, results=None, concat=False) -> bool:
     if results is None:
         results = []
     calc = True
-    if res > target or depth >= len(numbers):
+    if res >= target or depth >= len(numbers):
         results.append(res == target)
         calc = False
 
     if calc and depth == 0:
-        solve(numbers, target, numbers[depth], depth + 1, results)
+        solve(numbers, target, numbers[depth], depth + 1, results, concat)
     elif calc and depth < len(numbers):
-        solve(numbers, target, res + numbers[depth], depth + 1, results)
-        solve(numbers, target, res * numbers[depth], depth + 1, results)
+        solve(numbers, target, res + numbers[depth], depth + 1, results, concat)
+        solve(numbers, target, res * numbers[depth], depth + 1, results, concat)
+        if concat:
+            solve(numbers, target, int(f"{res}{numbers[depth]}"), depth + 1, results, concat)
 
     return sum(results)
