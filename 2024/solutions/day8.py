e7cf28081b9c05dd78c475294542a7b13ddb2997
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-12-09 14:21:23 +0100

    fix edge

--- 2024/solutions/day8.py
+++ 2024/solutions/day8.py
@@ -9,25 +9,20 @@ import re
 def part1(data: data) -> None:
     antennas, width, height = parse(data.getlines())
 
+    def antinode(a, b):
+        dx = a[0] - b[0]
+        dy = a[1] - b[1]
+        return (b[0] - dx, b[1] - dy)
+
     def valid(an):
         return 0 <= an[0] <= width and 0 <= an[1] <= height
 
     anti_nodes = []
     for _, antennas in antennas.items():
         for a, b in list(itertools.combinations(antennas, 2)):
-            ans = [antinode(a, b), antinode(b, a)]
-            anti_nodes = anti_nodes + [an for an in ans if valid(an)]
+            anti_nodes = anti_nodes + [an for an in [antinode(a, b), antinode(b, a)] if valid(an)]
 
     print(len(set(anti_nodes)))
-
-
-def antinode(a, b):
-    dx = a[0] - b[0]
-    dy = a[1] - b[1]
-
-    nx = b[0] - dx
-    ny = b[1] - dy
-    return (nx, ny)
 
 
 def part2(data: data) -> None:
@@ -36,8 +31,8 @@ def part2(data: data) -> None:
 
 def parse(input: list[str]) -> dict:
     antennas = {}
-    width = len(input[0])
-    height = len(input)
+    width = len(input[0]) - 1
+    height = len(input) - 1
 
     for y, line in enumerate(input):
         ants = list(re.finditer(r"[\da-zA-Z]", line))
