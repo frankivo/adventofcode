039410d54dc8c76296091d53b6d0c4240acc10ae
Frank Oosterhuis <frank+github@scriptzone.nl>
2024-12-09 13:34:08 +0100

    p1 demo

--- 2024/solutions/day8.py
+++ 2024/solutions/day8.py
@@ -2,11 +2,32 @@
 # https://adventofcode.com/2024/day/
 
 from data import data
+import itertools
 import re
 
 
 def part1(data: data) -> None:
     antennas, width, height = parse(data.getlines())
+
+    def valid(an):
+        return 0 <= an[0] <= width and 0 <= an[1] <= height
+
+    anti_nodes = []
+    for _, antennas in antennas.items():
+        for a, b in list(itertools.combinations(antennas, 2)):
+            ans = [antinode(a, b), antinode(b, a)]
+            anti_nodes = anti_nodes + [an for an in ans if valid(an)]
+
+    print(len(set(anti_nodes)))
+
+
+def antinode(a, b):
+    dx = a[0] - b[0]
+    dy = a[1] - b[1]
+
+    nx = b[0] - dx
+    ny = b[1] - dy
+    return (nx, ny)
 
 
 def part2(data: data) -> None:
