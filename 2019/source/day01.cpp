90f7f8acbd8df6dcaf3463d8dc970808cc2ddb9e
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-10 17:13:40 +0100

    day 1 part 1

--- 2019/source/day01.cpp
+++ 2019/source/day01.cpp
@@ -1,8 +1,22 @@
 #include "day01.h"
 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
 class day1 {
 	public:
 		int run() {
-			return 1;
+			std::string line;
+			int fuel_req = 0;
+			std::ifstream infile("input/day01.txt");
+
+			while (getline(infile, line)) {
+				auto num = std::stoi(line);
+				fuel_req += std::floor(num / 3) - 2;
+			}
+			infile.close();
+
+			return fuel_req;
 		}
 };
