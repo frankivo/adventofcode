451f419aeb39f770ca0f0ac20a9eb1e9d5e5abef
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-11 14:21:33 +0100

    part 2

--- 2019/source/day01.cpp
+++ 2019/source/day01.cpp
@@ -1,31 +1,53 @@
-
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <vector>
 
 #include "day.h"
 
 class Day01 : public Day {
 	public:
 		int part1() {
-			std::string line;
 			int fuel_req = 0;
-			std::ifstream infile("input/day01.txt");
 
-			while (getline(infile, line)) {
-				auto num = std::stoi(line);
-				fuel_req += fuel(num);
-			}
-			infile.close();
+			auto lines = data();
+			std::for_each(lines.begin(), lines.end(), [&fuel_req,this](int mass) {
+				fuel_req += fuel(mass);
+			});
 
 			return fuel_req;
-		}
+		};
 
 		int part2() {
-			return fuel(14);
-		}
+                        int fuel_req = 0;
+
+                        auto lines = data();
+			std::for_each(lines.begin(), lines.end(), [&fuel_req,this](int mass) {
+				while (mass >= 0) {
+					mass = fuel(mass);
+					if (mass > 0)
+						fuel_req += mass;
+				}
+                        });
+
+			return fuel_req;
+		};
+
 	private:
-		int fuel(int mass) {
+		std::vector<int> data() {
+			std::vector<int> data;
+			std::string line;
+
+			std::ifstream infile("input/day01.txt");
+			while (getline(infile, line))
+                                data.push_back(std::stoi(line));
+			infile.close();
+
+			return data;
+		};
+
+		int fuel(int mass) const {
 			return std::floor(mass / 3) - 2;
-		}
+		};
 };
