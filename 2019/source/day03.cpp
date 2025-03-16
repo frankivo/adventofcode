d238cf2fcb53b228666c9a30992e45df7fc8681d
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-16 16:49:52 +0100

    init

---
+++ 2019/source/day03.cpp
@@ -0,0 +1,14 @@
+#include "day.h"
+
+class Day03 : public Day {
+    public:
+        Day03() : Day(3) {};
+
+        int part1() const {
+            return -1;
+        };
+
+        int part2() const {
+            return -2;
+        };
+};
