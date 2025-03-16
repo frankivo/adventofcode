bb4d44cf4856cf658697f6c81ee9157c908aa9c6
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-16 15:47:48 +0100

    spaces

--- 2019/source/aoc.cpp
+++ 2019/source/aoc.cpp
@@ -2,20 +2,20 @@
 #include "days.h"
 
 int main(int, char *argv[]) {
-	auto day_no = std::stoi(argv[1]);
-	std::cout << "Running day " << day_no << std::endl;
+    auto day_no = std::stoi(argv[1]);
+    std::cout << "Running day " << day_no << std::endl;
 
-	auto days = Days();
-	auto day = days.by_number(day_no);
+    auto days = Days();
+    auto day = days.by_number(day_no);
 
-	if (!day) {
-		std::cerr << "No solution found!" << std::endl;
-		return 1;
-	}
+    if (!day) {
+        std::cerr << "No solution found!" << std::endl;
+        return 1;
+    }
 
-	auto rp1 = day->part1();
-	std::cout << "Result part 1: " << rp1 << std::endl;
-	auto rp2 = day->part2();
-	std::cout << "Result part 2: " << rp2 << std::endl;
-	return 0;
+    auto rp1 = day->part1();
+    std::cout << "Result part 1: " << rp1 << std::endl;
+    auto rp2 = day->part2();
+    std::cout << "Result part 2: " << rp2 << std::endl;
+    return 0;
 }
--- 2019/source/days.h
+++ 2019/source/days.h
@@ -4,9 +4,9 @@
 #include <memory>
 
 class Days {
-	public:
-		Days();
-		const std::unique_ptr<Day> by_number(int day_no);
-	private:
-		std::map<int, std::unique_ptr<Day>> days;
+    public:
+        Days();
+        const std::unique_ptr<Day> by_number(int day_no);
+    private:
+        std::map<int, std::unique_ptr<Day>> days;
 };
