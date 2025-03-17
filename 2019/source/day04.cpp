9cfb0537e5036e0c613d415494d162e0ab06660a
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-17 15:32:57 +0100

    clean

--- 2019/source/day04.cpp
+++ 2019/source/day04.cpp
@@ -7,21 +7,22 @@ class Day04 : public Day
 {
 
 public:
-    Day04() : Day(4) {};
+    Day04() : Day(4),
+        range(boost::irange(256310, 732736)) {};
 
     int part1() const
     {
-        auto range = boost::irange(256310, 732736);
-        auto res = std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid, this, _1));
-        return res;
+        return std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid, this, _1));
     };
 
     int part2() const
     {
-        return -2;
+        return -1;
     };
 
 private:
+    const boost::integer_range<int> range;
+
     bool valid(int password) const
     {
         auto pw = std::to_string(password);
@@ -29,13 +30,11 @@ private:
         for (size_t i = 0; i <= pw.size() - 2; ++i)
             slices.push_back(pw.substr(i, 2));
 
-        bool result_same = std::any_of(slices.begin(), slices.end(), [](std::string pair) {
-            return pair[0] == pair[1];
-        });
+        bool result_same = std::any_of(slices.begin(), slices.end(), [](std::string pair)
+            { return pair[0] == pair[1]; });
 
-        bool result_inc = std::all_of(slices.begin(), slices.end(), [](std::string pair) {
-            return static_cast<int>(pair[1]) >= static_cast<int>(pair[0]);
-        });
+        bool result_inc = std::all_of(slices.begin(), slices.end(), [](std::string pair)
+            { return static_cast<int>(pair[1]) >= static_cast<int>(pair[0]); });
 
         return result_same && result_inc;
     };
