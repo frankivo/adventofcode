69f3028815b5c9967800eb73e843eb7b3954657a
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-17 13:07:58 +0100

    Revert "Merge remote-tracking branch 'origin/2019_3' into 2019_3"
    This reverts commit cb74bd558e13c00eddf15b35432b087caaea5a90, reversing
    changes made to 47242dd91e8e8b01a90103425e6b5abd6a106009.

--- 2019/source/day03.cpp
+++ 2019/source/day03.cpp
@@ -39,13 +39,8 @@ class Day03 : public Day {
     typedef std::pair<int, int> coordinate;
     typedef std::unordered_map<coordinate, int, pair_hash> coordinate_dist;
 
-        const std::pair<std::set<coordinate>, std::unordered_map<coordinate, int, pair_hash>> wire1 = wire(0);
-        const std::pair<std::set<coordinate>, std::unordered_map<coordinate, int, pair_hash>> wire2 = wire(1);
-
-        std::vector<command> cmds(const int wire) const {
-            auto line = data()[wire];
-            std::vector<command> data;
-            boost::regex pattern("([A-Z])([0-9]+)");
+    const std::pair<std::set<coordinate>, coordinate_dist> wire1 = wire(0);
+    const std::pair<std::set<coordinate>, coordinate_dist> wire2 = wire(1);
 
     std::vector<command> cmds(const int wire) const {
         auto line = data()[wire];
