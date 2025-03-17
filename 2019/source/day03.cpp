851687ab87b46df54a6aff11339c3df07069d216
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-17 13:18:54 +0100

    format

--- 2019/source/day03.cpp
+++ 2019/source/day03.cpp
@@ -3,16 +3,19 @@
 #include <boost/regex.hpp>
 #include <unordered_map>
 
-class Day03 : public Day {
-    public:
-    Day03() :
-        Day(3),
-        wire1(wire(0)),
-        wire2(wire(1)) {};
+class Day03 : public Day
+{
 
-    int part1() const {
+public:
+    Day03() : Day(3),
+              wire1(wire(0)),
+              wire2(wire(1)) {};
+
+    int part1() const
+    {
         auto nearest = std::numeric_limits<int>::max();
-        for (auto& pos : intersections(wire1.first, wire2.first)) {
+        for (auto &pos : intersections(wire1.first, wire2.first))
+        {
             auto dist = std::abs(pos.first) + std::abs(pos.second);
             if (dist < nearest)
                 nearest = dist;
@@ -20,9 +23,11 @@ class Day03 : public Day {
         return nearest;
     };
 
-    int part2() const {
+    int part2() const
+    {
         auto nearest = std::numeric_limits<int>::max();
-        for (auto& pos : intersections(wire1.first, wire2.first)) {
+        for (auto &pos : intersections(wire1.first, wire2.first))
+        {
             auto dist = wire1.second.at(pos) + wire2.second.at(pos);
             if (dist < nearest)
                 nearest = dist;
@@ -30,10 +35,12 @@ class Day03 : public Day {
         return nearest;
     };
 
-    private:
-    struct pair_hash {
+private:
+    struct pair_hash
+    {
         template <class T1, class T2>
-        std::size_t operator () (const std::pair<T1,T2> &pair) const {
+        std::size_t operator()(const std::pair<T1, T2> &pair) const
+        {
             return std::hash<T1>{}(pair.first) ^ std::hash<T2>{}(pair.second);
         }
     };
@@ -45,12 +52,14 @@ class Day03 : public Day {
     const std::pair<std::set<coordinate>, coordinate_distance> wire1;
     const std::pair<std::set<coordinate>, coordinate_distance> wire2;
 
-    std::vector<command> cmds(const int wire) const {
+    std::vector<command> cmds(const int wire) const
+    {
         auto line = data()[wire];
         std::vector<command> data;
         boost::regex pattern("([A-Z])([0-9]+)");
 
-        for (auto i = boost::sregex_iterator(line.begin(), line.end(), pattern); i != boost::sregex_iterator(); ++i) {
+        for (auto i = boost::sregex_iterator(line.begin(), line.end(), pattern); i != boost::sregex_iterator(); ++i)
+        {
             auto match = *i;
             auto cmd = match[1].str();
             auto val = std::stoi(match[2].str());
@@ -60,16 +69,18 @@ class Day03 : public Day {
         return data;
     };
 
-
-    std::pair<std::set<coordinate>, coordinate_distance> wire(const int wire) const {
+    std::pair<std::set<coordinate>, coordinate_distance> wire(const int wire) const
+    {
         auto x = 0;
         auto y = 0;
         auto d = 0;
         std::set<coordinate> positions;
         std::unordered_map<coordinate, int, pair_hash> distances;
 
-        for (auto& cmd : cmds(wire)) {
-            for (auto i = 0; i < cmd.second; i++) {
+        for (auto &cmd : cmds(wire))
+        {
+            for (auto i = 0; i < cmd.second; i++)
+            {
                 d++;
                 if (cmd.first == "R")
                     x += 1;
@@ -90,7 +101,8 @@ class Day03 : public Day {
         return std::pair<std::set<coordinate>, coordinate_distance>(positions, distances);
     };
 
-    std::vector<coordinate> intersections(const std::set<coordinate> wire1, const std::set<coordinate> wire2) const {
+    std::vector<coordinate> intersections(const std::set<coordinate> wire1, const std::set<coordinate> wire2) const
+    {
         std::vector<coordinate> duplicates;
         std::set_intersection(wire1.begin(), wire1.end(), wire2.begin(), wire2.end(), std::back_inserter(duplicates));
         return duplicates;
