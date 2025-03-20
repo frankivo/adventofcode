45605ed3b87fd125aa58b5c729f20597bf65cf72
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-20 15:01:52 +0100

    cleanup

--- 2019/source/day04.cpp
+++ 2019/source/day04.cpp
@@ -14,73 +14,50 @@ public:
 
     int part1() const
     {
-        return std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid, this, _1));
+        return std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid, this, _1, 1));
     };
 
     int part2() const
     {
-        std::cout << valid_extended(112233) << std::endl;
-        std::cout << valid_extended(123444) << std::endl;
-        std::cout << valid_extended(111122) << std::endl;
-
-        return std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid_extended, this, _1));
+        return std::count_if(range.begin(), range.end(), boost::bind(&Day04::valid, this, _1, 2));
     };
 
 private:
     const boost::integer_range<int> range;
 
-    typedef std::pair<int, int> tint;
-
-    bool valid(int password) const
+    bool valid(int password, int part) const
     {
         auto pw = std::to_string(password);
-        std::vector<std::string> slices;
+
+        std::vector<std::pair<int, std::string>> slices;
         for (auto i = 0; i <= 6 - 2; i++)
-            slices.push_back(pw.substr(i, 2));
+            slices.push_back(std::pair(i, pw.substr(i, 2)));
 
-        bool result_same = std::any_of(slices.begin(), slices.end(), [](auto& pair)
-            { return pair[0] == pair[1]; }
-        );
-
-        bool result_inc = std::all_of(slices.begin(), slices.end(), [](auto& pair)
-            { return static_cast<int>(pair[1]) >= static_cast<int>(pair[0]); }
-        );
-
-        return result_same && result_inc;
-    };
-
-    bool valid_extended(int password) const
-    {
-        auto pw = std::to_string(password);
-
-        std::vector<int> gps; // Groups of two
-        for (
-            auto i = boost::sregex_iterator(pw.begin(), pw.end(), boost::regex(R"((.)\1{1})"));
-            i != boost::sregex_iterator();
-            i++
-        ) gps.push_back(i->position());
-
-        std::vector<tint> gpb; // Larger groups
+        std::vector<std::pair<int,int>> gpb; // Larger groups
         for (
             auto i = boost::sregex_iterator(pw.begin(), pw.end(), boost::regex(R"((.)\1{2,})"));
             i != boost::sregex_iterator();
             i++
-        ) gpb.push_back(std::pair<int,int>(i->position(), i->str().length()));
+        ) gpb.push_back(std::pair(i->position(), i->str().length()));
 
-        auto has_pair_not_in_group = std::any_of(gps.begin(), gps.end(), [gpb](auto& sml) {
-            return std::none_of(gpb.begin(), gpb.end(), [sml](auto& big) {
-                return sml >= big.first && sml <= big.first + big.second -1;
+        bool result_inc = std::all_of(slices.begin(), slices.end(), [](auto& slice)
+            { return static_cast<int>(slice.second[1]) >= static_cast<int>(slice.second[0]); }
+        );
+        if (result_inc == false)
+            return false;
+
+        if (part == 1) {
+            return std::any_of(slices.begin(), slices.end(), [](auto& slice)
+                { return slice.second[0] == slice.second[1]; }
+            );
+        }
+
+        return std::any_of(slices.begin(), slices.end(), [gpb](auto& slice) {
+            if (slice.second[0] != slice.second[1])
+                return false;
+            return std::none_of(gpb.begin(), gpb.end(), [slice](auto& big) {
+                return slice.first >= big.first && slice.first <= big.first + big.second -1;
             });
         });
-
-        std::vector<std::string> slices;
-        for (auto i = 0; i <= 6 - 2; i++)
-            slices.push_back(pw.substr(i, 2));
-
-        bool result_inc = std::all_of(slices.begin(), slices.end(), [](auto& pair)
-            { return static_cast<int>(pair[1]) >= static_cast<int>(pair[0]); }
-        );
-
-        return result_inc && has_pair_not_in_group;
     };
 };
