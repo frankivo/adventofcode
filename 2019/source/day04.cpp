ca28557fced11868cf170b335a6bfbf0aa9ecb59
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-20 15:10:31 +0100

    cleanup

--- 2019/source/day04.cpp
+++ 2019/source/day04.cpp
@@ -3,7 +3,6 @@
 #include <boost/bind.hpp>
 #include <boost/range/irange.hpp>
 #include <boost/regex.hpp>
-#include <iostream>
 
 class Day04 : public Day
 {
@@ -33,29 +32,33 @@ private:
         for (auto i = 0; i <= 6 - 2; i++)
             slices.push_back(std::pair(i, pw.substr(i, 2)));
 
-        std::vector<std::pair<int,int>> gpb; // Larger groups
+        std::vector<std::pair<int,int>> group; // Repeating digit groups (larger than 2)
         for (
             auto i = boost::sregex_iterator(pw.begin(), pw.end(), boost::regex(R"((.)\1{2,})"));
             i != boost::sregex_iterator();
             i++
-        ) gpb.push_back(std::pair(i->position(), i->str().length()));
+        ) group.push_back(std::pair(i->position(), i->str().length()));
 
+        // Test "Going from left to right, the digits never decrease"
         bool result_inc = std::all_of(slices.begin(), slices.end(), [](auto& slice)
             { return static_cast<int>(slice.second[1]) >= static_cast<int>(slice.second[0]); }
         );
+
         if (result_inc == false)
             return false;
 
         if (part == 1) {
+            // Test "Two adjacent digits are the same"
             return std::any_of(slices.begin(), slices.end(), [](auto& slice)
                 { return slice.second[0] == slice.second[1]; }
             );
         }
 
-        return std::any_of(slices.begin(), slices.end(), [gpb](auto& slice) {
+        // Test "the two adjacent matching digits are not part of a larger group of matching digits"
+        return std::any_of(slices.begin(), slices.end(), [group](auto& slice) {
             if (slice.second[0] != slice.second[1])
                 return false;
-            return std::none_of(gpb.begin(), gpb.end(), [slice](auto& big) {
+            return std::none_of(group.begin(), group.end(), [slice](auto& big) {
                 return slice.first >= big.first && slice.first <= big.first + big.second -1;
             });
         });
