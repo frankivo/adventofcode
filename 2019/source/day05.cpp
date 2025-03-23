5f211ce4582049bb74381ef36f68120ae8181f6e
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-23 19:29:17 +0100

    init

---
+++ 2019/source/day05.cpp
@@ -0,0 +1,21 @@
+#include "day.hpp"
+
+class Day05 : public Day
+{
+
+public:
+    Day05() : Day(5) {}
+
+    int part1() const
+    {
+        return -1;
+    };
+
+    int part2() const
+    {
+        return -1;
+    };
+
+private:
+
+};
