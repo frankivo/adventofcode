418de85f13e11a6f4cf0a21b15f8ace3518e2f4e
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-24 17:30:11 +0100

    JumpIfTrue
    Signed-off-by: Frank Oosterhuis <frank+github@scriptzone.nl>

--- 2019/source/day05.cpp
+++ 2019/source/day05.cpp
@@ -11,6 +11,38 @@ public:
 
     int part1() const
     {
+        return solve(1);
+    }
+
+    int part2() const
+    {
+        return solve(5);
+    };
+
+private:
+    enum opcodes {
+        Start, Add, Multiply, Input, Output, JumpIfTrue, JumpIfFalse, LessThan, Equals, Exit = 99,
+    };
+
+    enum modi {
+        Position, Immediate,
+    };
+
+    const std::vector<int> input;
+
+    const std::vector<int> parse_data() const {
+        auto line = data()[0];
+        // line = "1002,4,3,4,33";
+        // line = "1101,100,-1,4,0";
+        std::vector<std::string> values;
+        std::vector<int> nums;
+
+        boost::split(values, line, boost::is_any_of(","));
+        for (auto& i : values) nums.push_back(std::stoi(i));
+            return nums;
+    };
+
+    int solve(const int systemid) const {
         std::vector<int> clone(input.begin(), input.end());
 
         auto i = 0;
@@ -32,10 +64,16 @@ public:
                     clone[clone[i+3]] = ((paramC == Position) ? clone[clone[i+1]] : clone[i+1]) * ((paramB == Position) ? clone[clone[i+2]] : clone[i+2]);
                     break;
                 case Input:
-                    ((paramC == Position) ? clone[clone[i+1]] : clone[i+1]) = 1;
+                    ((paramC == Position) ? clone[clone[i+1]] : clone[i+1]) = systemid;
                     break;
                 case Output:
                     output = ((paramC == Position) ? clone[clone[i+1]] : clone[i+1]);
+                    break;
+                case JumpIfTrue:
+                    if ((paramC == Position) ? clone[clone[i+1]] : clone[i+1] != 0) {
+                        i = (paramB == Position) ? clone[clone[i+2]] : clone[i+2];
+                        continue;
+                    }
                     break;
                 default:
                     break;
@@ -46,6 +84,9 @@ public:
                 case Multiply:
                     i+=4;
                     break;
+                case JumpIfTrue:
+                    i+=3;
+                    break;
                 default:
                     i+=2;
                     break;
@@ -53,33 +94,5 @@ public:
         }
 
         return output;
-    };
-
-    int part2() const
-    {
-        return -1;
-    };
-
-private:
-    enum opcodes {
-        Start, Add, Multiply, Input, Output, Exit = 99,
-    };
-
-    enum modi {
-        Position, Immediate,
-    };
-
-    const std::vector<int> input;
-
-    const std::vector<int> parse_data() const {
-        auto line = data()[0];
-        // line = "1002,4,3,4,33";
-        // line = "1101,100,-1,4,0";
-        std::vector<std::string> values;
-        std::vector<int> nums;
-
-        boost::split(values, line, boost::is_any_of(","));
-        for (auto& i : values) nums.push_back(std::stoi(i));
-            return nums;
     };
 };
