411928af3c2b543860e82d87eae39a972c6ccb85
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-25 23:20:00 +0100

    error handling (#83)

--- 2019/source/aoc.cpp
+++ 2019/source/aoc.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 #include "days.hpp"
 
-int main(const int, const char *argv[]) {
-    auto day_no = std::stoi(argv[1]);
+void run(const int day_no) {
     std::cout << "Running day " << day_no << std::endl;
 
     auto days = Days();
     auto day = days.by_number(day_no);
 
-    if (!day) {
-        std::cerr << "No solution found!" << std::endl;
-        return 1;
-    }
+    if (!day)
+        throw std::invalid_argument("Solution not found!");
 
+    auto rp1 = day->part1();
+    std::cout << "Result part 1: " << rp1 << std::endl;
+    auto rp2 = day->part2();
+    std::cout << "Result part 2: " << rp2 << std::endl;
+};
+
+int main(const int, const char *argv[]) {
     try {
-        auto rp1 = day->part1();
-        std::cout << "Result part 1: " << rp1 << std::endl;
-        auto rp2 = day->part2();
-        std::cout << "Result part 2: " << rp2 << std::endl;
+        run(std::stoi(argv[1]));
         return 0;
     }
     catch (const std::exception& ex) {
         std::cerr << "Error occured: " << ex.what() << std::endl;
         return -1;
     }
-}
+    catch (...) {
+        std::cerr << "Unknown error occured!" << std::endl;
+        return -2;
+    }
+};
