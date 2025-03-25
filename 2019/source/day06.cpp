48ad44685bd05912ee11b1b4e470f260ab73e083
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-25 14:01:46 +0100

    init

---
+++ 2019/source/day06.cpp
@@ -0,0 +1,23 @@
+#include "day.hpp"
+
+#include <boost/algorithm/string.hpp>
+
+class Day06 : public Day
+{
+
+public:
+    Day06() : Day(6) {};
+
+    int part1() const
+    {
+        return -1;
+    }
+
+    int part2() const
+    {
+        return -1;
+    };
+
+private:
+
+};
