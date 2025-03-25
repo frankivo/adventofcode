b5f9d9889aa2ec68c5bf8d88311c8be273d75709
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-25 23:22:31 +0100

    init

---
+++ 2019/source/day07.cpp
@@ -0,0 +1,24 @@
+#include "day.hpp"
+
+#include <boost/algorithm/string.hpp>
+#include <unordered_map>
+
+class Day07 : public Day
+{
+
+public:
+    Day07() : Day(7) {};
+
+    int part1() const
+    {
+        return -1;
+    }
+
+    int part2() const
+    {
+        return -1;
+    };
+
+private:
+
+};
--- 2019/source/days.cpp
+++ 2019/source/days.cpp
@@ -6,6 +6,7 @@
 #include "day04.cpp"
 #include "day05.cpp"
 #include "day06.cpp"
+#include "day07.cpp"
 
 Days::Days() {
     days[1] = std::make_unique<Day01>();
@@ -13,11 +14,12 @@ Days::Days() {
     days[3] = std::make_unique<Day03>();
     days[4] = std::make_unique<Day04>();
     days[6] = std::make_unique<Day06>();
+    days[7] = std::make_unique<Day07>();
 };
 
 const std::unique_ptr<Day> Days::by_number(int day_no) {
     auto d = days.find(day_no);
-        if (d == days.end())
-            return nullptr;
+    if (d == days.end())
+        return nullptr;
     return std::move(d->second);
 };
