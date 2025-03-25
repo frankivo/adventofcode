4578dbac676ec06abc27bb14230ee579eae180cc
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-03-25 22:35:47 +0100

    cleanup

--- 2019/source/day06.cpp
+++ 2019/source/day06.cpp
@@ -1,7 +1,6 @@
 #include "day.hpp"
 
 #include <boost/algorithm/string.hpp>
-#include <iostream>
 #include <unordered_map>
 
 class Day06 : public Day
@@ -14,7 +13,7 @@ public:
     {
         auto total = 0;
         for (const auto& o : orbits)
-            total += distance(o.first);
+            total += path(o.first).size();
         return total;
     }
 
@@ -22,16 +21,13 @@ public:
     {
         auto you = path("YOU");
         auto san = path("SAN");
-
         auto dist = 0;
         for (auto& y : you) {
-            std::cout<< y << "::" << y <<std::endl;
             auto it = std::find(san.begin(), san.end(), y);
             if (it != san.end())
                 return dist + std::distance(san.begin(), it);
             dist++;
         }
-
         return -1;
     };
 
@@ -43,37 +39,21 @@ private:
 
     const orbit_map parse_data() const {
         orbit_map orbs;
-
         for (const auto& line: data()) {
             std::vector<std::string> values;
             boost::split(values, line, boost::is_any_of(")"));
             orbs[values[1]] = values[0];
         }
-
         return orbs;
-    };
-
-    int distance(const std::string& orbit) const {
-        auto dist = 0;
-        auto cur = orbit;
-
-        while (cur != "COM") {
-            cur = orbits.at(cur);
-            dist+=1;
-        }
-
-        return dist;
     };
 
     orbit_distance path(const std::string& orbit) const {
         orbit_distance distances;
         auto cur = orbit;
-
         while (cur != "COM") {
             cur = orbits.at(cur);
             distances.push_back(cur);
         }
-
         return distances;
     };
 };
