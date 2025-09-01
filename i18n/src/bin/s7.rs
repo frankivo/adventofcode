ac857bed7383c6405dc5766983487185303719e2
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-01 23:26:49 +0200

    parse

---
+++ i18n/src/bin/s7.rs
@@ -0,0 +1,19 @@
+use i18n::util::file::input;
+use itertools::Itertools;
+
+fn main() {
+    let input = input(7);
+    let rows = input
+        .lines()
+        .filter_map(|l|
+            l
+            .split("\t")
+            .next_tuple::<(&str, &str, &str)>()
+            .and_then(|(a,b,c)| Some((a, b.parse::<i16>().unwrap(), c.parse::<i16>().unwrap()))
+        )
+    );
+
+    for l in rows {
+        dbg!(l);
+    }
+}
