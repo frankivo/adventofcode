8939cb16a66479aae9cbea6302346117568cb9e3
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-02 00:26:25 +0200

    cleanup 2

--- i18n/src/bin/s7.rs
+++ i18n/src/bin/s7.rs
@@ -1,7 +1,13 @@
-use chrono::{DateTime, Duration, Timelike};
 use chrono_tz::America::{Halifax, Santiago};
+use chrono::{DateTime, Duration, FixedOffset, Timelike};
 use i18n::util::file::input;
 use itertools::Itertools;
+
+fn is_maybe_halifax(dt: DateTime<FixedOffset>) -> bool {
+    let hal = dt.with_timezone(&Halifax).naive_local().and_utc();
+    let utc = dt.naive_local().and_utc();
+    hal == utc
+}
 
 fn main() {
     let input = input(7);
@@ -10,21 +16,18 @@ fn main() {
             .next_tuple::<(&str, &str, &str)>()
             .and_then(|(a, b, c)| {
                 Some((
-                    DateTime::parse_from_rfc3339(a).unwrap(),
-                    b.parse::<i64>().unwrap(),
-                    c.parse::<i64>().unwrap(),
+                    DateTime::parse_from_rfc3339(a).ok()?,
+                    b.parse::<i64>().ok()? - c.parse::<i64>().ok()?
                 ))
             })
     });
 
-    let hours = rows.map(|(dt, correct, wrong)| {
-        let hal = dt.with_timezone(&Halifax).naive_local().and_utc();
-        let utc = dt.naive_local().and_utc();
-        let off = dt - Duration::minutes(wrong) + Duration::minutes(correct);
-        return match utc == hal {
-            true => off.with_timezone(&Halifax),
-            _ => off.with_timezone(&Santiago)
-        }.hour();
+    let hours = rows.map(|(dt, offset)| {
+        let timezoned = match is_maybe_halifax(dt) {
+            true => dt.with_timezone(&Halifax),
+            _ => dt.with_timezone(&Santiago)
+        };
+        (timezoned + Duration::minutes(offset)).hour()
     });
 
     let sum : usize = hours.enumerate().map(|(ln, hours)| (ln + 1) * hours as usize).sum();
