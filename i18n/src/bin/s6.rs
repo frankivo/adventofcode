fe7d07c1420c8bedeac6418d79930e26a3ddb02f
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-01 11:53:24 +0200

    I18n 6 (#90)
    * parse words
    
    * working
    
    * clean
    
    * format

---
+++ i18n/src/bin/s6.rs
@@ -0,0 +1,39 @@
+use encoding::all::ISO_8859_1;
+use encoding::{EncoderTrap, Encoding};
+use i18n::util::file;
+use regex::Regex;
+
+fn latin(word: &str) -> String {
+    return String::from_utf8(ISO_8859_1.encode(word, EncoderTrap::Ignore).unwrap()).unwrap();
+}
+
+fn main() {
+    let input = file::input(6);
+    let (words, crossword) = input.split_once("\n\n").unwrap();
+
+    let words: Vec<String> = words
+        .lines()
+        .enumerate()
+        .map(|(idx, word)| {
+            let idx = idx + 1;
+            match (idx % 3 == 0, idx % 5 == 0) {
+                (true, true) => latin(&latin(word)),
+                (true, false) | (false, true) => latin(word),
+                _ => word.to_string(),
+            }
+        })
+        .collect();
+
+    let solution: usize = crossword
+        .lines()
+        .map(|q| {
+            let rx = Regex::new(&format!("^{}$", q.trim())).unwrap();
+            words
+                .iter()
+                .position(|word| rx.is_match(&word))
+                .map_or(0, |idx| idx + 1)
+        })
+        .sum();
+
+    println!("Soltion: {}", solution);
+}
