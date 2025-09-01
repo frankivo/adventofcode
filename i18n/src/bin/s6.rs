9d2ba63938c49f95448a62a775e307d999f598f6
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-01 11:40:20 +0200

    working

--- i18n/src/bin/s6.rs
+++ i18n/src/bin/s6.rs
@@ -1,6 +1,7 @@
-use i18n::util::file;
 use encoding::{Encoding, EncoderTrap};
 use encoding::all::ISO_8859_1;
+use i18n::util::file;
+use regex::Regex;
 
 fn latin(word: &str) -> String {
     return String::from_utf8(ISO_8859_1.encode(word, EncoderTrap::Ignore).unwrap()).unwrap();
@@ -8,18 +9,26 @@ fn latin(word: &str) -> String {
 
 fn main() {
     let input = file::input(6);
-    let (words, _crossword) = input.split_once("\n\n").unwrap();
+    let (words, crossword) = input.split_once("\n\n").unwrap();
 
-    let fixed  = words.lines().enumerate().map(|(idx, word)| {
+    let words : Vec<String> = words.lines().enumerate().map(|(idx, word)| {
         let idx = idx +1;
         match (idx % 3 == 0, idx % 5 == 0) {
             (true, true) => latin(&latin(word)),
             (true, false) | (false, true) => latin(word),
             _ => word.to_string(),
         }
-    });
+    }).collect();
 
-    for w in fixed {
-        dbg!(w);
-    }
+    let solution = crossword.lines().map(|q| {
+        let rx = Regex::new(&format!("^{}$", q.trim())).unwrap();
+        for (ln, word) in words.iter().enumerate() {
+            if rx.is_match(&word) {
+                return ln + 1;
+            }
+        }
+        return 0;
+    }).sum::<usize>();
+
+    println!("Soltion: {}", solution);
 }
