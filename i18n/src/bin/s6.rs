6f662047ccad43900b11436a8194e1ee9bbbc385
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-01 11:44:04 +0200

    format

--- i18n/src/bin/s6.rs
+++ i18n/src/bin/s6.rs
@@ -1,5 +1,5 @@
-use encoding::{Encoding, EncoderTrap};
 use encoding::all::ISO_8859_1;
+use encoding::{EncoderTrap, Encoding};
 use i18n::util::file;
 use regex::Regex;
 
@@ -11,21 +11,29 @@ fn main() {
     let input = file::input(6);
     let (words, crossword) = input.split_once("\n\n").unwrap();
 
-    let words : Vec<String> = words.lines().enumerate().map(|(idx, word)| {
-        let idx = idx +1;
-        match (idx % 3 == 0, idx % 5 == 0) {
-            (true, true) => latin(&latin(word)),
-            (true, false) | (false, true) => latin(word),
-            _ => word.to_string(),
-        }
-    }).collect();
+    let words: Vec<String> = words
+        .lines()
+        .enumerate()
+        .map(|(idx, word)| {
+            let idx = idx + 1;
+            match (idx % 3 == 0, idx % 5 == 0) {
+                (true, true) => latin(&latin(word)),
+                (true, false) | (false, true) => latin(word),
+                _ => word.to_string(),
+            }
+        })
+        .collect();
 
-    let solution : usize = crossword.lines().map(|q| {
-        let rx = Regex::new(&format!("^{}$", q.trim())).unwrap();
-        words.iter()
-        .position(|word| rx.is_match(&word))
-        .map_or(0, |idx| idx+ 1)
-    }).sum();
+    let solution: usize = crossword
+        .lines()
+        .map(|q| {
+            let rx = Regex::new(&format!("^{}$", q.trim())).unwrap();
+            words
+                .iter()
+                .position(|word| rx.is_match(&word))
+                .map_or(0, |idx| idx + 1)
+        })
+        .sum();
 
     println!("Soltion: {}", solution);
 }
