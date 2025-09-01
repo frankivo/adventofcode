3ca8f7a385d7644ef9f8617dbe12509d624ec251
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-01 23:29:36 +0200

    parse dt

--- i18n/src/bin/s7.rs
+++ i18n/src/bin/s7.rs
@@ -1,19 +1,23 @@
+use chrono::DateTime;
 use i18n::util::file::input;
 use itertools::Itertools;
 
 fn main() {
     let input = input(7);
-    let rows = input
-        .lines()
-        .filter_map(|l|
-            l
-            .split("\t")
+    let rows = input.lines().filter_map(|l| {
+        l.split("\t")
             .next_tuple::<(&str, &str, &str)>()
-            .and_then(|(a,b,c)| Some((a, b.parse::<i16>().unwrap(), c.parse::<i16>().unwrap()))
-        )
-    );
+            .and_then(|(a, b, c)| {
+                Some((
+                    DateTime::parse_from_rfc3339(a).unwrap(),
+                    b.parse::<i16>().unwrap(),
+                    c.parse::<i16>().unwrap(),
+                ))
+            })
+    });
 
     for l in rows {
         dbg!(l);
     }
+
 }
