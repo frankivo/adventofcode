0808a3861d6a8006c7fd7db06b706d912bab18b4
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-02 13:36:44 +0200

    some speed

--- i18n/src/bin/s8.rs
+++ i18n/src/bin/s8.rs
@@ -4,16 +4,18 @@ use regex::Regex;
 use unidecode::unidecode;
 
 fn valid(pw: &str) -> bool {
+    let ascii_str = unidecode(pw).to_lowercase();
+
     // a length of at least 4 and at most 12
     (4..=12).contains(&pw.chars().count())
     // at least one digit
-    && Regex::new(r"[\d]").unwrap().is_match(pw)
+    && pw.chars().any(|c| c.is_ascii_digit())
     // at least one accented or unaccented vowel1 (a, e, i, o, u) (examples: i, Á or ë).
-    && Regex::new(r"[aeiou]").unwrap().is_match(&unidecode(pw).to_lowercase())
+    && Regex::new(r"[aeiou]").unwrap().is_match(&ascii_str)
     // at least one accented or unaccented consonant, examples: s, ñ or ŷ
-    && Regex::new(r"[b-df-hj-np-tv-z]").unwrap().is_match(&unidecode(pw).to_lowercase())
+    && Regex::new(r"[b-df-hj-np-tv-z]").unwrap().is_match(&ascii_str)
     // no recurring letters in any form.
-    && !unidecode(pw).to_lowercase().chars().counts().values().any(|&c| c > 1)
+    && !ascii_str.chars().counts().values().any(|&c| c > 1)
 }
 
 fn main() {
