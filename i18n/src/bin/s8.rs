9ab7beebef7fabe1781ff0e42870a855f758951c
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-02 13:38:44 +0200

    more speed

--- i18n/src/bin/s8.rs
+++ i18n/src/bin/s8.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 use regex::Regex;
 use unidecode::unidecode;
 
-fn valid(pw: &str) -> bool {
+fn valid(pw: &str, vowels: &Regex, consonants: &Regex) -> bool {
     let ascii_str = unidecode(pw).to_lowercase();
 
     // a length of at least 4 and at most 12
@@ -11,14 +11,20 @@ fn valid(pw: &str) -> bool {
     // at least one digit
     && pw.chars().any(|c| c.is_ascii_digit())
     // at least one accented or unaccented vowel1 (a, e, i, o, u) (examples: i, Á or ë).
-    && Regex::new(r"[aeiou]").unwrap().is_match(&ascii_str)
+    && vowels.is_match(&ascii_str)
     // at least one accented or unaccented consonant, examples: s, ñ or ŷ
-    && Regex::new(r"[b-df-hj-np-tv-z]").unwrap().is_match(&ascii_str)
+    && consonants.is_match(&ascii_str)
     // no recurring letters in any form.
     && !ascii_str.chars().counts().values().any(|&c| c > 1)
 }
 
 fn main() {
-    let valid = file::input(8).lines().filter(|pw| valid(pw)).count();
+    let vowels = Regex::new(r"[aeiou]").unwrap();
+    let consonats = Regex::new(r"[b-df-hj-np-tv-z]").unwrap();
+
+    let valid = file::input(8)
+        .lines()
+        .filter(|pw| valid(pw, &vowels, &consonats))
+        .count();
     println!("valid: {}", valid);
 }
