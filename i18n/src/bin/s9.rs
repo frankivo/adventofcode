316e95e626dad042ff0ec1441d6f353a61ed45b3
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-04 23:50:48 +0200

    some sort of progress

--- i18n/src/bin/s9.rs
+++ i18n/src/bin/s9.rs
@@ -2,10 +2,6 @@ use std::collections::HashMap;
 use chrono::NaiveDate;
 use i18n::util::file;
 
-fn valid(date: &str, fmt: &str) -> bool {
-    NaiveDate::parse_from_str(date, fmt).is_ok()
-}
-
 fn main() {
     let input = file::input(9);
     println!("{}", input);
@@ -21,8 +17,27 @@ fn main() {
 
     let fmts = vec!["%d-%m-%y", "%m-%d-%y", "%y-%m-%d", "%y-%d-%m"];
 
-    for fmt in fmts {
-        let x = pd.get("Frank").unwrap().iter().all(|d| valid(d, fmt));
-        dbg!(fmt, x);
+
+    let dates  = pd.get("Frank").unwrap();
+    dbg!(dates);
+
+    let validator = fmts.iter().filter_map(|f| {
+        let x = dates.iter().all(|d| {
+            NaiveDate::parse_from_str(d, f).is_ok()
+        });
+
+        if x {
+            return Some(dates.iter().map(|d| NaiveDate::parse_from_str(d, f).unwrap()));
+        }
+        else {
+            return None;
+        }
+    });
+
+    for v in validator {
+        for d in v{
+            dbg!(d);
+        }
     }
+
 }
