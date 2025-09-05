3905362a1c17abf19db3c7466443a16191c79997
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-05 23:42:17 +0200

    solved

--- i18n/src/bin/s9.rs
+++ i18n/src/bin/s9.rs
@@ -1,10 +1,31 @@
 use chrono::NaiveDate;
 use i18n::util::file;
+use itertools::Itertools;
 use std::collections::HashMap;
+
+fn wrote_on_911(dates: Vec<&str>, fmts: Vec<&str>) -> bool {
+    let nine11 = NaiveDate::parse_from_str("01-09-11", "%y-%m-%d").unwrap();
+    fmts.iter()
+        .find_map(|f| {
+            if dates
+                .iter()
+                .all(|d| NaiveDate::parse_from_str(d, f).is_ok())
+            {
+                Some(
+                    dates
+                        .iter()
+                        .map(|d| NaiveDate::parse_from_str(d, f).unwrap())
+                        .any(|d| d == nine11),
+                )
+            } else {
+                None
+            }
+        })
+        .is_some_and(|b| b)
+}
 
 fn main() {
     let input = file::input(9);
-    println!("{}", input);
 
     let mut pd = HashMap::<&str, Vec<&str>>::new();
 
@@ -16,26 +37,12 @@ fn main() {
     }
 
     let fmts = vec!["%d-%m-%y", "%m-%d-%y", "%y-%m-%d", "%y-%d-%m"];
-    let dates = pd.get("Peter").unwrap();
-    let nine11 = NaiveDate::parse_from_str("01-09-11", "%y-%m-%d").unwrap();
 
-    if let Some(is_911) = fmts.iter().find_map(|f| {
-        if dates
-            .iter()
-            .all(|d| NaiveDate::parse_from_str(d, f).is_ok())
-        {
-            Some(
-                dates
-                    .iter()
-                    .map(|d| NaiveDate::parse_from_str(d, f).unwrap())
-                    .any(|d| d == nine11)
-            )
-        } else {
-            None
-        }
-    }) {
-        dbg!(is_911);
-    } else {
-        eprintln!("No format matched all dates");
-    }
+    let names = pd
+        .iter()
+        .filter(|(_, dates)| wrote_on_911(dates.to_vec(), fmts.to_vec()))
+        .map(|f| f.0)
+        .sorted();
+
+    print!("{}", names.cloned().collect::<Vec<&str>>().join(" "));
 }
