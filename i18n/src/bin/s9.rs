2cda7cd8711593629a62fda38246b7a7acc99318
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-05 23:49:02 +0200

    chatgpt optimize

--- i18n/src/bin/s9.rs
+++ i18n/src/bin/s9.rs
@@ -3,25 +3,19 @@ use i18n::util::file;
 use itertools::Itertools;
 use std::collections::HashMap;
 
-fn wrote_on_911(dates: Vec<&str>, fmts: Vec<&str>) -> bool {
+fn wrote_on_911(dates: &[&str], fmts: &[&str]) -> bool {
     let nine11 = NaiveDate::parse_from_str("01-09-11", "%y-%m-%d").unwrap();
-    fmts.iter()
-        .find_map(|f| {
-            if dates
-                .iter()
-                .all(|d| NaiveDate::parse_from_str(d, f).is_ok())
-            {
-                Some(
-                    dates
-                        .iter()
-                        .map(|d| NaiveDate::parse_from_str(d, f).unwrap())
-                        .any(|d| d == nine11),
-                )
-            } else {
-                None
-            }
-        })
-        .is_some_and(|b| b)
+
+    fmts.iter().any(|f| {
+        let parsed: Option<Vec<_>> = dates
+            .iter()
+            .map(|d| NaiveDate::parse_from_str(d, f).ok())
+            .collect();
+        match parsed {
+            Some(ds) => ds.iter().any(|d| *d == nine11),
+            None => false,
+        }
+    })
 }
 
 fn main() {
@@ -30,19 +24,21 @@ fn main() {
     let mut pd = HashMap::<&str, Vec<&str>>::new();
 
     for line in input.lines() {
-        let (date, names) = line.split_once(":").unwrap();
-        names.trim().split(", ").for_each(|name| {
-            pd.entry(name).or_insert(Vec::new()).insert(0, date);
-        });
+        if let Some((date, names)) = line.split_once(':') {
+            for name in names.trim().split(", ") {
+                pd.entry(name).or_default().push(date);
+            }
+        }
     }
 
     let fmts = vec!["%d-%m-%y", "%m-%d-%y", "%y-%m-%d", "%y-%d-%m"];
 
     let names = pd
         .iter()
-        .filter(|(_, dates)| wrote_on_911(dates.to_vec(), fmts.to_vec()))
-        .map(|f| f.0)
-        .sorted();
+        .filter(|(_, dates)| wrote_on_911(dates, &fmts))
+        .map(|(name, _)| *name)
+        .sorted()
+        .collect::<Vec<&str>>();
 
-    print!("{}", names.cloned().collect::<Vec<&str>>().join(" "));
+    println!("{}", names.join(" "));
 }
