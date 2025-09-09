8383a1a0ca3ff2fbfe2cea249fcd47582e9f1e9c
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-09 20:42:23 +0200

    I18n 10 (#94)

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Solutions build in various languages (per year):
 * [2020](2020): SQL (duckdb)
 * [2021](2022): scala3
 * [2022](2022): scala3
+* [2023](2023): python3
+* [2024](2024): python3
+
+# Internationali­zation Puzzles
+https://i18n-puzzles.com/
+
+Trying to learn Rust on the side.
---
+++ i18n/src/bin/s10.rs
@@ -0,0 +1,81 @@
+use i18n::util::file::input;
+use itertools::Itertools;
+use std::collections::HashMap;
+use unicode_normalization::UnicodeNormalization;
+
+fn line_parse(lines: &str) -> Vec<(&str, &str)> {
+    lines
+        .split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| l.split_once(" ").unwrap())
+        .collect()
+}
+
+// Finds all the character positions of the once that are willing to mutate
+fn possible_mutations(word: &str) -> Vec<usize> {
+    word.chars()
+        .enumerate()
+        .filter_map(|(pos, c)| (c.nfd().collect::<String>() != c.to_string()).then_some(pos))
+        .collect::<Vec<usize>>()
+}
+
+// Returns all the unicode variants of a (pass)word
+fn get_mutations(word: &str) -> Vec<String> {
+    let options = possible_mutations(&word);
+    let base = std::iter::once(word.to_string());
+
+    let variants = (1..=options.len())
+        .flat_map(|i| options.iter().combinations(i))
+        .map(|opts| {
+            word.chars()
+                .enumerate()
+                .map(|(o, c)| {
+                    if opts.contains(&&o) {
+                        c.nfd().collect::<String>()
+                    } else {
+                        c.to_string()
+                    }
+                })
+                .collect::<String>()
+        });
+
+    return variants.chain(base).collect();
+}
+
+fn main() {
+    let input = input(10);
+    let (auth, login_attempts) = input.split_once("\n\n").unwrap();
+
+    let auth: HashMap<_, _> = line_parse(auth).into_iter().collect();
+
+    // The key for this map is password, but is ugly. Users with the same password could provide funny results.
+    // The vector should probably be expanded if the user logs in with a completely different password.
+    let mut password_map: HashMap<String, (Vec<String>, bool)> = HashMap::new();
+
+    let valid: i32 = line_parse(login_attempts)
+        .iter()
+        .map(|(usr, pwd)| {
+            let simple = pwd.nfc().collect::<String>();
+
+            if !password_map.contains_key(&simple) {
+                // No cache present. Find all virants and check if there is a valid one.
+                let variants = get_mutations(&simple);
+
+                let hash = *auth.get(usr).expect("Hash not found");
+                let has_valid = variants
+                    .iter()
+                    .any(|v| bcrypt::verify(v, hash).expect("Invalid hash"));
+
+                password_map.insert(simple.clone(), (variants, has_valid));
+            }
+
+            // Now we can check normalized passwords :)
+            password_map
+                .get(&simple)
+                .expect("Expected password to be present")
+                .1 as i32
+        })
+        .sum();
+
+    print!("Amount of valid attempts: {}", valid);
+}
