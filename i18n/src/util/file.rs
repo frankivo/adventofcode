1465a9d6834d6d83cdc49f8fc0a372b6e9beda1c
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-11 15:38:54 +0200

    download files

--- i18n/src/util/file.rs
+++ i18n/src/util/file.rs
@@ -1,6 +1,7 @@
-
-use std::env;
-use std::fs;
+use dotenv::dotenv;
+use std::{env, fs};
+use std::fs::File;
+use std::io::Write;
 
 pub fn input(day: i32) -> String {
     let demo = env::args().any(|a| a == "test");
@@ -10,6 +11,38 @@ pub fn input(day: i32) -> String {
         format!("input/{}.txt", day)
     };
 
-    let err = format!("read failed! {}", input);
-    return fs::read_to_string(input).expect(&err);
+    if !fs::exists(&input).unwrap() {
+        download(day, demo, &input);
+    }
+
+    return fs::read_to_string(&input).expect(&format!("read failed! {}", &input));
+}
+
+fn get_cookie() -> String {
+    dotenv().expect("Could net get env-file");
+    env::var("COOKIE").expect("COOKIE var not found")
+}
+
+fn download(day: i32, demo: bool, target: &str) {
+    let url = format!(
+        "https://i18n-puzzles.com/puzzle/{}/{}input",
+        day,
+        demo.then_some("test-").unwrap_or("")
+    );
+    dbg!(&url);
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let resp = rt.block_on(async {
+        let client = reqwest::Client::new();
+        client
+            .get(&url)
+            .header("Cookie", get_cookie())
+            .send()
+            .await
+    }).expect("Failed to download").error_for_status().expect("Download error");
+
+    let body = rt.block_on(resp.bytes()).expect("Failed to get body");
+
+    let mut f = File::create(target).expect("Cannot open target");
+    f.write_all(&body).expect("Write failed");
 }
