d74b17611983e9c14a162856fe476605948f0b83
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-11 17:05:46 +0200

    struct fun

--- i18n/src/bin/s12.rs
+++ i18n/src/bin/s12.rs
@@ -1,11 +1,37 @@
 use i18n::util::file::input;
+use std::fmt;
 use unidecode::unidecode;
 
-fn split_line(line: &str) -> (&str, &str, u32) {
+struct Person {
+    firstname: String,
+    lastname: String,
+    phone: u32,
+}
+
+impl fmt::Debug for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Person")
+            .field("lastname", &self.lastname)
+            .field("firstname", &self.firstname)
+            .finish()
+    }
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}: {}", self.lastname, self.firstname, self.phone)
+    }
+}
+
+fn split_line(line: &str) -> Person {
     let (last_name, rest) = line.split_once(",").unwrap();
     let (first_name, phone) = rest.split_once(":").unwrap();
     let phone = phone.trim().parse::<u32>().expect("can't parse phone");
-    (last_name.trim(), first_name.trim(), phone)
+    Person {
+        firstname: first_name.trim().to_string(),
+        lastname: last_name.trim().to_string(),
+        phone: phone,
+    }
 }
 
 fn fix(name: &str) -> String {
@@ -18,10 +44,16 @@ fn fix(name: &str) -> String {
 fn main() {
     let input = input(12);
 
-    let mut names: Vec<(_, _, _)> = input.lines().map(split_line).collect();
+    let mut names: Vec<Person> = input.lines().map(split_line).collect();
 
-    dbg!(&names);
+    names.sort_by(|a, b| fix(&a.lastname).cmp(&fix(&b.lastname)));
 
-    names.sort_by(|a, b| fix(a.0).cmp(&fix(b.0)));
-    dbg!(names);
+    for n in names {
+        println!("{}", n);
+    }
+
+
+    for c in 'A'..'Ö' {
+        dbg!(c);
+    }
 }
