dae856335f83023ae50b762eb6ba225e6936c32d
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-16 10:35:47 +0200

    try_parse

--- i18n/src/bin/s13.rs
+++ i18n/src/bin/s13.rs
@@ -1,12 +1,23 @@
+use i18n::util::file::input;
 use itertools::Itertools;
 
-fn main() {
-    let x = "616e77c3a4686c65";
-    let pairs = x.chars().tuples::<(char, char)>();
-    let word :Vec<u8> = pairs.map(|p| {
-        let m= format!("{}{}", p.0, p.1);
-        u8::from_str_radix(&m, 16).unwrap()
-    }).collect();
+fn decode(raw: &str) -> String {
+    let pairs = raw.chars().tuples::<(char, char)>();
+    let word: Vec<u8> = pairs
+        .map(|p| {
+            let m = format!("{}{}", p.0, p.1);
+            u8::from_str_radix(&m, 16).unwrap()
+        })
+        .collect();
+    String::from_utf8(word).unwrap_or("error".to_owned())
+}
 
-    dbg!(String::from_utf8(word).unwrap());
+fn main() {
+    let input = input(13);
+    let (words, _crossword) = input.split_once("\n\n").expect("Failed to read data");
+
+    let data = words.lines().map(decode);
+    for w in data {
+        dbg!(w);
+    }
 }
