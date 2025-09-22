28bd81a3b2eaaa3ba8027153bdb24a9549fef7c1
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-09-22 19:19:18 +0200

    chars

--- i18n/src/bin/s14.rs
+++ i18n/src/bin/s14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use i18n::util::file::input;
 
 fn main() {
@@ -6,7 +8,29 @@ fn main() {
         .lines()
         .map(|l| l.split_once(" × ").unwrap());
 
-    for l in input {
-        dbg!(&l);
+
+
+    let char_map : HashMap<char, i32> = HashMap::from([
+        ('一', 1),
+        ('二', 2),
+        ('三', 3),
+        ('四', 4),
+        ('五', 5),
+        ('六', 6),
+        ('七', 7),
+        ('八', 8),
+        ('九', 9),
+        ('十', 10),
+        ('百', 100),
+        ('千', 1000),
+        ('万', 10_000),
+        ]);
+
+    // for l in input {
+    //     dbg!(&l);
+    // }
+
+    for (c, n) in char_map {
+        dbg!(n, c.escape_unicode() as u32);
     }
 }
