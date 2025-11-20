e12a32a58c52bb66baa9f6a993f7a6f1ae17373a
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-11-20 13:33:23 +0100

    p1

--- everybodycodes/2025/src/bin/s1.rs
+++ everybodycodes/2025/src/bin/s1.rs
@@ -4,19 +4,24 @@ fn get_num(instr: &str) -> i8 {
     instr.chars().nth(1).unwrap().to_digit(10).unwrap() as i8
 }
 
-fn main() {
-    let data = file::input(1);
-    let lines: Vec<_> = data.lines().collect();
-    let names: Vec<_> = lines[0].split(",").collect();
-    let instructions = lines[2].split(",");
+fn part1(names: Vec<&str>, instructions: Vec<&str>) -> String {
     let max_idx = names.len() as i8 - 1;
 
-    let index = instructions.fold(0, |i, instr| {
+    let index = instructions.into_iter().fold(0, |i, instr| {
         let go_right = instr.starts_with('R');
         let steps = get_num(&instr);
         let next = if go_right { i + steps } else { i - steps };
         next.clamp(0, max_idx)
     }) as usize;
 
-    print!("My name is: {}", names.get(index).unwrap());
+    names.get(index).unwrap().to_string()
+}
+
+fn main() {
+    let data = file::input(1);
+    let lines: Vec<_> = data.lines().collect();
+    let names: Vec<_> = lines[0].split(",").collect();
+    let instructions: Vec<_> = lines[2].split(",").collect();
+
+    print!("Part1: {}", part1(names, instructions));
 }
