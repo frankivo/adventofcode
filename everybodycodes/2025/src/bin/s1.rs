de426e580d6f1d0ed7e892459fff4752b462d662
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-11-20 14:31:17 +0100

    solve part2

--- everybodycodes/2025/src/bin/s1.rs
+++ everybodycodes/2025/src/bin/s1.rs
@@ -1,27 +1,49 @@
 use everybodycodes::util::file;
 
 fn get_num(instr: &str) -> i8 {
-    instr.chars().nth(1).unwrap().to_digit(10).unwrap() as i8
+    instr
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse()
+        .unwrap()
 }
 
-fn part1(names: Vec<&str>, instructions: Vec<&str>) -> String {
-    let max_idx = names.len() as i8 - 1;
-
-    let index = instructions.into_iter().fold(0, |i, instr| {
-        let go_right = instr.starts_with('R');
-        let steps = get_num(&instr);
-        let next = if go_right { i + steps } else { i - steps };
-        next.clamp(0, max_idx)
-    }) as usize;
-
-    names.get(index).unwrap().to_string()
-}
-
-fn main() {
-    let data = file::input(1);
+fn part1() -> String {
+    let data = file::input(1, 1);
     let lines: Vec<_> = data.lines().collect();
     let names: Vec<_> = lines[0].split(",").collect();
     let instructions: Vec<_> = lines[2].split(",").collect();
 
-    print!("Part1: {}", part1(names, instructions));
+    let index = instructions.iter().fold(0, |i, instr| {
+        let go_right = instr.starts_with('R');
+        let steps = get_num(&instr);
+        let next = if go_right { i + steps } else { i - steps };
+        next.clamp(0, names.len() as i8 - 1)
+    });
+
+    names.get(index as usize).unwrap().to_string()
+}
+
+fn part2() -> String {
+    let data = file::input(1, 2);
+    let lines: Vec<_> = data.lines().collect();
+    let names: Vec<_> = lines[0].split(",").collect();
+    let instructions: Vec<_> = lines[2].split(",").collect();
+
+    let index: i32 = instructions.iter().fold(0, |i, instr| {
+        let go_right = instr.starts_with('R');
+        let steps = get_num(&instr) as i32;
+        if go_right { i + steps } else { i - steps }
+    });
+
+    let n = names.len() as i32;
+    let idx = ((index % n) + n) % n;
+
+    names.get(idx as usize).unwrap().to_string()
+}
+
+fn main() {
+    println!("My name is: {}", part1());
+    println!("My parent is: {}", part2());
 }
--- everybodycodes/2025/src/util/file.rs
+++ everybodycodes/2025/src/util/file.rs
@@ -1,11 +1,11 @@
 use std::{env, fs};
 
-pub fn input(day: i32) -> String {
+pub fn input(day: i32, part: i32) -> String {
     let demo = env::args().any(|a| a == "test");
     let input = if demo {
         format!("test-input/{}.txt", day)
     } else {
-        format!("input/{}.txt", day)
+        format!("input/{}_{}.txt", day, part)
     };
 
     return fs::read_to_string(&input).expect(&format!("read failed! {}", &input));
