05ad30d16d1c29d9b669f898f2c9e5420fa2ad80
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-11-29 23:55:36 +0100

    files

---
+++ everybodycodes/2025/src/bin/s3.rs
@@ -0,0 +1,35 @@
+use everybodycodes::util::file;
+use std::collections::{BTreeSet, HashSet};
+
+fn part1() -> i16 {
+    let crates: HashSet<i16> = file::input(3, 1)
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    crates.iter().sum()
+}
+
+fn part2() -> i16 {
+    let crates: BTreeSet<i16> = file::input(3, 2)
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    dbg!(crates);
+    0
+}
+
+fn main() {
+    println!(
+        "What is the largest possible set of crates that can be formed from a given list? {}",
+        part1()
+    );
+    println!("Pack the mushroom into exactly 20 crates. What is the smallest possible set of the crates that can be used for this purpose? {}", part2());
+}
