cbcf81f0e0cdd8c3f3a810af4260944bd2f0d211
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-01 00:03:03 +0100

    shorter

--- everybodycodes/2025/src/bin/s4.rs
+++ everybodycodes/2025/src/bin/s4.rs
@@ -1,17 +1,13 @@
 use everybodycodes::util::file;
 
 fn main() {
-    let gears: Vec<f32> = file::input(4, 1)
+    let last_mul: f32 = file::input(4, 1)
         .lines()
         .filter_map(|s| s.parse().ok())
-        .collect();
+        .collect::<Vec<f32>>()
+        .windows(2)
+        .map(|w| w[0] / w[1])
+        .product();
 
-    let last_mul = gears.iter().enumerate().fold(1.0, |mul, (i, _)| {
-        if i > 0 {
-            (gears[i - 1] / gears[i]) * mul
-        } else {
-            mul
-        }
-    });
     dbg!((last_mul * 2025.0) as i16);
 }
