43d51fd0de300dd5ff72daf7a380408130661b1b
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-02 00:31:40 +0100

    part 3 wip

--- everybodycodes/2025/src/bin/s4.rs
+++ everybodycodes/2025/src/bin/s4.rs
@@ -18,10 +18,24 @@ fn part2() -> i64 {
         .filter_map(|s| s.parse().ok())
         .rev()
         .collect();
-    (10000000000000.0 * gears[0] / gears[gears.len()-1]).ceil() as i64
+    (10000000000000.0 * gears[0] / gears[gears.len() - 1]).ceil() as i64
+}
+
+fn part3() -> i16 {
+    let data = file::input(4, 3);
+    let lines: Vec<_> = data.lines().collect();
+
+    let (first, last) = (lines[0], lines[lines.len() - 1]);
+    dbg!(first, last);
+
+    for l in lines {
+        dbg!(l);
+    }
+    0
 }
 
 fn main() {
     dbg!(part1());
     dbg!(part2());
+    dbg!(part3());
 }
