621545ff858ab2116d5c13305a11013d4d8b2705
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-01 15:55:16 +0100

    cleanup

--- advendofcode/2025/src/bin/s1.rs
+++ advendofcode/2025/src/bin/s1.rs
@@ -4,40 +4,29 @@ fn instructions() -> Vec<i16> {
     let data = file::input(1, 1);
     data.lines()
         .map(|line| {
-            let left = line.starts_with('L');
             let num: i16 = line[1..].parse().unwrap();
-
-            if left {
-                -num
-            } else {
-                num
-            }
+            if line.starts_with('L') { -num } else { num }
         })
         .collect()
 }
 
-fn part1() -> i16 {
+fn solve(part: i8) -> i16 {
     let (_, zeroes) = instructions().iter().fold((50, 0), |(pos, zeroes), instr| {
         let next = pos + instr;
         let clamped = ((next % 100) + 100) % 100;
-        (clamped, if clamped == 0 { zeroes + 1 } else { zeroes })
-    });
 
-    zeroes
-}
-
-fn part2() -> i16 {
-    let (_, zeroes) = instructions().iter().fold((50, 0), |(pos, zeroes), instr| {
-        let next = pos + instr;
-        let clamped = ((next % 100) + 100) % 100;
-        let wraps = next.div_euclid(100);
-        (clamped, zeroes + wraps.abs())
+        let wraps = if part == 1 {
+            (clamped == 0) as i16
+        } else {
+            next.div_euclid(100).abs()
+        };
+        (clamped, zeroes + wraps)
     });
 
     zeroes
 }
 
 fn main() {
-    dbg!(part1());
-    dbg!(part2());
+    dbg!(solve(1));
+    dbg!(solve(2));
 }
