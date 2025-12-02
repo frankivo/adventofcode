519c485ea187385af4825c77b3b42f994a518a72
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-03 00:11:59 +0100

    builder

--- everybodycodes/2025/src/bin/ec5.rs
+++ everybodycodes/2025/src/bin/ec5.rs
@@ -1,14 +1,19 @@
 use everybodycodes::util::file;
 
 struct Row {
-    id: i16,
+    base: i16,
     left: Option<i16>,
     right: Option<i16>,
 }
 
+struct Sword {
+    id: i16,
+    strength: Vec<Row>,
+}
+
 impl Row {
     fn try_insert(&mut self, n: i16) -> bool {
-        match n.cmp(&self.id) {
+        match n.cmp(&self.base) {
             std::cmp::Ordering::Less if self.left.is_none() => {
                 self.left = Some(n);
                 true
@@ -22,19 +27,26 @@ impl Row {
     }
 }
 
-fn part1() -> String {
-    let binding = file::input(5, 1);
-    let (_, numbers) = &binding.split_once(":").unwrap();
+fn build_sword(input: &str) -> Sword {
+    let (id, numbers) = input.split_once(":").unwrap();
+    let id = id.parse::<i16>().unwrap();
     let numbers = numbers.trim().split(",").map(|s| s.parse::<i16>().unwrap());
 
     let mut sword: Vec<Row> = Vec::with_capacity(10);
     for n in numbers {
         if !sword.iter_mut().any(|row| row.try_insert(n)) {
-            sword.push(Row { id: n, left: None, right: None, });
+            sword.push(Row { base: n, left: None, right: None, });
         }
     }
 
-    sword.into_iter().map(|r| r.id.to_string()).collect::<Vec<_>>().join("")
+    Sword{id: id, strength: sword}
+}
+
+fn part1() -> String {
+    let binding = file::input(5, 1);
+    let sword = build_sword(&binding);
+
+    sword.strength.into_iter().map(|r| r.base.to_string()).collect::<Vec<_>>().join("")
 }
 
 fn main() {
