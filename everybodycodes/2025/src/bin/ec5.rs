0c3a4e7998ddcd4588868187ee6f01f318852e6a
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-03 00:04:10 +0100

    Ordering

--- everybodycodes/2025/src/bin/ec5.rs
+++ everybodycodes/2025/src/bin/ec5.rs
@@ -1,58 +1,42 @@
 use everybodycodes::util::file;
 
-#[derive(Debug, Clone, Copy)]
 struct Row {
     id: i16,
     left: Option<i16>,
     right: Option<i16>,
 }
 
-fn main() {
+impl Row {
+    fn try_insert(&mut self, n: i16) -> bool {
+        match n.cmp(&self.id) {
+            std::cmp::Ordering::Less if self.left.is_none() => {
+                self.left = Some(n);
+                true
+            }
+            std::cmp::Ordering::Greater if self.right.is_none() => {
+                self.right = Some(n);
+                true
+            }
+            _ => false,
+        }
+    }
+}
+
+fn part1() -> String {
     let binding = file::input(5, 1);
     let (_, numbers) = &binding.split_once(":").unwrap();
-    let numbers: Vec<i16> = numbers
-        .trim()
-        .split(",")
-        .map(|s| s.parse::<i16>().unwrap())
-        .collect();
+    let numbers = numbers.trim().split(",").map(|s| s.parse::<i16>().unwrap());
 
     let mut sword: Vec<Row> = Vec::with_capacity(10);
     for n in numbers {
-        let mut pushed = false;
-
-        for (i, r) in sword.clone().into_iter().enumerate() {
-            if n < r.id && r.left == None {
-                let tmp = Row {
-                    id: r.id,
-                    left: Some(n),
-                    right: r.right,
-                };
-                sword[i] = tmp;
-                pushed = true;
-                break;
-            } else if n > r.id && r.right == None {
-                let tmp = Row {
-                    id: r.id,
-                    left: r.left,
-                    right: Some(n),
-                };
-                sword[i] = tmp;
-                pushed = true;
-                break;
-            }
-        }
-
-        if !pushed {
-            sword.push(Row {
-                id: n,
-                left: None,
-                right: None,
-            });
+        if !sword.iter_mut().any(|row| row.try_insert(n)) {
+            sword.push(Row { id: n, left: None, right: None, });
         }
     }
 
-    let quality: Vec<_> = sword.into_iter().map(|r| r.id).collect();
-    for q in quality {
-        print!("{}", q);
-    }
+    sword.into_iter().map(|r| r.id.to_string()).collect::<Vec<_>>().join("")
+}
+
+fn main() {
+    dbg!(part1());
 }
