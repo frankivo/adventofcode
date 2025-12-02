e268b853e8c044b04660e4d54698bfe3414d2df3
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-02 11:24:24 +0100

    part2

--- advendofcode/2025/src/bin/s2.rs
+++ advendofcode/2025/src/bin/s2.rs
@@ -5,20 +5,44 @@ fn split_range(range: &str) -> (i64, i64) {
     (start.parse::<i64>().unwrap(), end.parse::<i64>().unwrap())
 }
 
-fn valid(id: i64) -> bool {
+fn valid(part: i8, id: i64) -> bool {
+    if part == 1 {
+        valid1(id)
+    } else {
+        valid2(id)
+    }
+}
+
+fn valid1(id: i64) -> bool {
     let str = id.to_string();
     let mid = str.len() / 2;
     &str[0..mid] != &str[mid..]
 }
 
-fn part1() -> i64 {
+fn valid2(id: i64) -> bool {
+    let str = id.to_string();
+    let mid = str.len() / 2;
+
+    for i in 0..mid {
+        let sub = &str[0..=i];
+        let v: Vec<_> = str.match_indices(sub).collect();
+        if v.len() * sub.len() == str.len() {
+            return false;
+        };
+    }
+
+    true
+}
+
+fn solve(part: i8) -> i64 {
     let binding = file::input(2, 1).replace("\r\n", "");
     let pairs: Vec<_> = binding.split(",").map(split_range).collect();
     pairs.into_iter().fold(0, |sum, pair| {
-        sum + (pair.0..=pair.1).fold(0, |sum, i| if valid(i) { sum } else { sum + i })
+        sum + (pair.0..=pair.1).fold(0, |sum, i| if valid(part, i) { sum } else { sum + i })
     })
 }
 
 fn main() {
-    dbg!(part1());
+    dbg!(solve(1));
+    dbg!(solve(2));
 }
