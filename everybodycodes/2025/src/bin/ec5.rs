d28e4871d419e77e4e764da4205dc5ddc70800cb
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-02 23:54:32 +0100

    part1 ugly

--- everybodycodes/2025/src/bin/ec5.rs
+++ everybodycodes/2025/src/bin/ec5.rs
@@ -1,5 +1,12 @@
 use everybodycodes::util::file;
 
+#[derive(Debug, Clone, Copy)]
+struct Row {
+    id: i16,
+    left: Option<i16>,
+    right: Option<i16>,
+}
+
 fn main() {
     let binding = file::input(5, 1);
     let (_, numbers) = &binding.split_once(":").unwrap();
@@ -8,14 +15,44 @@ fn main() {
         .split(",")
         .map(|s| s.parse::<i16>().unwrap())
         .collect();
-    let bone = numbers.into_iter().fold("".to_owned(), |bone, num| {
-        let prev = bone.chars().last();
-        match prev {
-            Some(p) => {
-                bone
+
+    let mut sword: Vec<Row> = Vec::with_capacity(10);
+    for n in numbers {
+        let mut pushed = false;
+
+        for (i, r) in sword.clone().into_iter().enumerate() {
+            if n < r.id && r.left == None {
+                let tmp = Row {
+                    id: r.id,
+                    left: Some(n),
+                    right: r.right,
+                };
+                sword[i] = tmp;
+                pushed = true;
+                break;
+            } else if n > r.id && r.right == None {
+                let tmp = Row {
+                    id: r.id,
+                    left: r.left,
+                    right: Some(n),
+                };
+                sword[i] = tmp;
+                pushed = true;
+                break;
             }
-            None => bone + &num.to_string(),
         }
-    });
-    dbg!(bone);
+
+        if !pushed {
+            sword.push(Row {
+                id: n,
+                left: None,
+                right: None,
+            });
+        }
+    }
+
+    let quality: Vec<_> = sword.into_iter().map(|r| r.id).collect();
+    for q in quality {
+        print!("{}", q);
+    }
 }
