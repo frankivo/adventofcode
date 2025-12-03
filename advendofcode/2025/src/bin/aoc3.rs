c43e5b77a93e7c77c88e1d42f69e9b7c064163fe
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-03 23:54:50 +0100

    simple

--- advendofcode/2025/src/bin/aoc3.rs
+++ advendofcode/2025/src/bin/aoc3.rs
@@ -1,17 +1,23 @@
 use aoc::util::file;
 
-fn joltage(bank: &str) -> u32 {
-    let batteries: Vec<u32> = bank.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    let &bat1 = batteries[..batteries.len() - 1].iter().max().unwrap();
-    let pos = batteries.iter().position(|i| *i == bat1).unwrap();
-    let &bat2 = batteries[pos + 1..].iter().max().unwrap();
-    bat1 * 10 + bat2
+fn part1() -> u32 {
+    file::input(3, 1)
+        .lines()
+        .map(|bank| {
+            let batteries: Vec<u32> = bank.chars().map(|c| c.to_digit(10).unwrap()).collect();
+            let &bat1 = batteries[..batteries.len() - 1].iter().max().unwrap();
+            let pos = batteries.iter().position(|i| *i == bat1).unwrap();
+            let &bat2 = batteries[pos + 1..].iter().max().unwrap();
+            bat1 * 10 + bat2
+        })
+        .sum()
 }
 
-fn part1() -> u32 {
-    file::input(3, 1).lines().map(joltage).sum()
+fn part2() -> u32 {
+    0
 }
 
 fn main() {
     dbg!(part1());
+    dbg!(part2());
 }
