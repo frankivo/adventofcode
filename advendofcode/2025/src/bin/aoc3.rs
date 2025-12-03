4f7115c0ce3b7652608b579dbb54ce69e17979b8
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-03 17:13:03 +0100

    part1

--- advendofcode/2025/src/bin/aoc3.rs
+++ advendofcode/2025/src/bin/aoc3.rs
@@ -1,25 +1,16 @@
 use aoc::util::file;
 
-fn find(battery: &str) -> i16 {
-
+fn joltage(bank: &str) -> i16 {
+    let batteries: Vec<u32> = bank.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let bat1 = batteries[..batteries.len() - 1].iter().max().unwrap();
+    let pos = batteries.iter().position(|i| i == bat1).unwrap();
+    let bat2 = batteries[pos + 1..].iter().max().unwrap();
+    let jolt = bat1.to_string() + &bat2.to_string();
+    jolt.parse::<i16>().unwrap()
 }
 
 fn part1() -> i16 {
-    let binding = file::input(3, 1);
-    let first: Vec<u32> = binding
-        .lines()
-        .collect::<Vec<_>>()
-        .first()
-        .unwrap()
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect();
-    dbg!(&first);
-    let max = first.iter().max().unwrap();
-    let pos = first.iter().position(|i| i == max).unwrap();
-    let max2 = first[pos+1..].iter().max().unwrap();
-    dbg!(max, max2);
-    0
+    file::input(3, 1).lines().map(joltage).sum()
 }
 
 fn main() {
