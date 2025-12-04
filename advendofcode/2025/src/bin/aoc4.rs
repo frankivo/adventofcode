830b7c33a011e11b8195e19ec1f0a079173b39c3
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-04 14:18:24 +0100

    map

--- advendofcode/2025/src/bin/aoc4.rs
+++ advendofcode/2025/src/bin/aoc4.rs
@@ -1,15 +1,28 @@
 use aoc::util::file;
 
 fn part1() -> i16 {
-    let binding = file::input(4,1);
-    let first = binding.lines().next().unwrap();
-    let x :Vec<_>= first.chars().into_iter().enumerate().filter_map(|(i,c)| {
-        match c {
-            '@' => Some(i),
-            _ => None
-        }
-    } ).collect();
-    dbg!(first, x);
+    let binding = file::input(4, 1);
+    let y: Vec<_> = binding
+        .lines()
+        .enumerate()
+        .map(|(y, line)| {
+            line.chars()
+                .into_iter()
+                .enumerate()
+                .filter_map(|(x, c)| match c {
+                    '@' => Some((y, x)),
+                    _ => None,
+                })
+                .collect::<Vec<(usize, usize)>>()
+        })
+        .collect();
+    let flattened: Vec<(usize,usize)> = y
+        .iter()
+        .flat_map(|inner_vec| inner_vec.iter())
+        .cloned()
+        .collect();
+
+    dbg!(flattened.len());
     0
 }
 
