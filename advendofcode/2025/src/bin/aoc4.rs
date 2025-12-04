ff98515724179ac6a6b9e6c61ac18eb1fd75a161
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-04 16:27:54 +0100

    cleanup

--- advendofcode/2025/src/bin/aoc4.rs
+++ advendofcode/2025/src/bin/aoc4.rs
@@ -5,7 +5,7 @@ use std::collections::HashSet;
 type Point = (i16, i16);
 type Map = HashSet<Point>;
 
-fn build_map(raw: &str) -> Map {
+fn map_create(raw: &str) -> Map {
     raw.lines()
         .enumerate()
         .flat_map(|(y, line)| {
@@ -20,7 +20,7 @@ fn build_map(raw: &str) -> Map {
         .collect()
 }
 
-fn adjacent(map: &Map, (y, x): (i16, i16)) -> usize {
+fn adjacent_count(map: &Map, (y, x): (i16, i16)) -> usize {
     (-1..=1)
         .flat_map(|yy| (-1..=1).map(move |xx| (yy, xx)))
         .filter(|&(yy, xx)| !(yy == 0 && xx == 0))
@@ -28,28 +28,28 @@ fn adjacent(map: &Map, (y, x): (i16, i16)) -> usize {
         .count()
 }
 
+fn map_remove(map: &Map) -> Map {
+    map.iter()
+        .copied()
+        .filter(|&(y, x)| adjacent_count(map, (y, x)) >= 4)
+        .collect()
+}
+
 fn part1(map: &Map, size: i16) -> i16 {
     (0..size).fold(0, |sum, y| {
         sum + (0..size).fold(0, |sum, x| {
-            sum + (map.contains(&(y, x)) && adjacent(&map, (y, x)) < 4) as i16
+            sum + (map.contains(&(y, x)) && adjacent_count(&map, (y, x)) < 4) as i16
         })
     })
 }
 
-fn remove(map: &Map, size: i16) -> Map {
-    map.iter()
-        .copied()
-        .filter(|&(y, x)| adjacent(map, (y, x)) >= 4)
-        .collect()
-}
-
-fn part2(map: &Map, size: i16) -> i16 {
+fn part2(map: &Map) -> i16 {
     let papers = map.len();
     let mut map = map.clone();
 
     loop {
         let prev = map.len();
-        map = remove(&map, size);
+        map = map_remove(&map);
         if map.len() == prev {
             return (papers - prev) as i16;
         }
@@ -58,8 +58,8 @@ fn part2(map: &Map, size: i16) -> i16 {
 
 fn main() {
     let binding = file::input(4, 1);
-    let map: Map = build_map(&binding);
+    let map: Map = map_create(&binding);
     let size: i16 = binding.lines().count() as i16;
     dbg!(part1(&map, size));
-    dbg!(part2(&map, size));
+    dbg!(part2(&map));
 }
