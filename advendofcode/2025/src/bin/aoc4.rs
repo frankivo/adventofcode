375e3f8bf3a70f286bb028e7869cc07eb2bf3664
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2025-12-04 16:22:01 +0100

    speed2

--- advendofcode/2025/src/bin/aoc4.rs
+++ advendofcode/2025/src/bin/aoc4.rs
@@ -8,56 +8,41 @@ type Map = HashSet<Point>;
 fn build_map(raw: &str) -> Map {
     raw.lines()
         .enumerate()
-        .map(|(y, line)| {
+        .flat_map(|(y, line)| {
             line.chars()
                 .into_iter()
                 .enumerate()
-                .filter_map(|(x, c)| match c {
+                .filter_map(move |(x, c)| match c {
                     '@' => Some((y as i16, x as i16)),
                     _ => None,
                 })
-                .collect::<Map>()
         })
-        .collect::<Vec<_>>()
-        .iter()
-        .flat_map(|inner_vec| inner_vec.iter())
-        .cloned()
         .collect()
 }
-fn adjacent(map: &Map, point: Point) -> Map {
-    let mut adj: Map = HashSet::new();
-    let (y, x) = point;
-    for yy in -1..=1 {
-        for xx in -1..=1 {
-            if yy != 0 || xx != 0 {
-                let y = y + yy;
-                let x = x + xx;
-                if map.contains(&(y, x)) {
-                    adj.insert((y, x));
-                }
-            }
-        }
-    }
-    adj
+
+fn adjacent(map: &Map, (y, x): (i16, i16)) -> usize {
+    (-1..=1)
+        .flat_map(|yy| (-1..=1).map(move |xx| (yy, xx)))
+        .filter(|&(yy, xx)| !(yy == 0 && xx == 0))
+        .filter(|&(yy, xx)| map.contains(&(y + yy, x + xx)))
+        .count()
 }
+
 fn part1(map: &Map, size: i16) -> i16 {
     (0..size).fold(0, |sum, y| {
         sum + (0..size).fold(0, |sum, x| {
-            sum + (map.contains(&(y, x)) && adjacent(&map, (y, x)).len() < 4) as i16
+            sum + (map.contains(&(y, x)) && adjacent(&map, (y, x)) < 4) as i16
         })
     })
 }
+
 fn remove(map: &Map, size: i16) -> Map {
-    let mut clone = map.clone();
-    for y in 0..size {
-        for x in 0..size {
-            if clone.contains(&(y, x)) && adjacent(&clone, (y, x)).len() < 4 {
-                clone.retain(|&i| i != (y, x));
-            }
-        }
-    }
-    clone
+    map.iter()
+        .copied()
+        .filter(|&(y, x)| adjacent(map, (y, x)) >= 4)
+        .collect()
 }
+
 fn part2(map: &Map, size: i16) -> i16 {
     let items = map.len();
     let mut removed = usize::MAX;
@@ -69,6 +54,7 @@ fn part2(map: &Map, size: i16) -> i16 {
     }
     (items - map.len()) as i16
 }
+
 fn main() {
     let binding = file::input(4, 1);
     let map: Map = build_map(&binding);
