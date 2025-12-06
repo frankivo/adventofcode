46c4a233d2d91f3d160b3655e80fee5960124e2c
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-06 23:25:04 +0100

    parse numbers

--- advendofcode/2025/src/bin/aoc6.rs
+++ advendofcode/2025/src/bin/aoc6.rs
@@ -1,7 +1,28 @@
 use aoc::util::file;
+use regex::Regex;
 
 fn part1() -> u64 {
-0
+    let binding = file::input(6, 1);
+
+    let lines: Vec<_> = binding.lines().collect();
+    let numbers = &lines[0..lines.len() - 1];
+    dbg!(numbers);
+
+    let r_num = Regex::new(r"[\d]+").unwrap();
+
+    let numbers: Vec<Vec<i16>> = numbers
+        .iter()
+        .map(|line| {
+            r_num
+                .find_iter(line)
+                .map(|r| r.as_str().parse().unwrap())
+                .collect()
+        })
+        .collect();
+    dbg!(numbers);
+
+    dbg!(lines.last());
+    0
 }
 
 fn main() {
