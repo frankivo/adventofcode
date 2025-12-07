92b15b6ab1231a17fc6e0afc3725467800f13183
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-08 00:08:11 +0100

    count beams

--- advendofcode/2025/src/bin/aoc7.rs
+++ advendofcode/2025/src/bin/aoc7.rs
@@ -4,17 +4,29 @@ use std::collections::HashSet;
 fn part1() -> u64 {
     let binding = file::input(7, 1);
 
-    let mut beams: HashSet<usize> = HashSet::new();
+    let mut count = 0;
 
     for line in binding.lines() {
         dbg!(line);
 
-        if let Some(s) = line.chars().position(|c| c == 'S') {
-            beams.insert(s);
+        if let Some(start) = line.chars().position(|c| c == 'S') {
+            count += 1;
+        } else {
+            let mut beams: HashSet<usize> = HashSet::new();
+            let splitters: Vec<usize> = line
+                .char_indices()
+                .filter_map(|(i, c)| (c == '^').then_some(i))
+                .collect();
+            for s in splitters {
+                beams.insert(s - 1);
+                beams.insert(s + 1);
+            }
+            dbg!(&beams.iter().count());
+            count += beams.iter().count();
         }
     }
 
-    dbg!(beams);
+    dbg!(count);
     0
 }
 
