01b2425843520727171120491ca7da2c69a7d20f
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-08 00:21:25 +0100

    count, but too many

--- advendofcode/2025/src/bin/aoc7.rs
+++ advendofcode/2025/src/bin/aoc7.rs
@@ -5,24 +5,31 @@ fn part1() -> u64 {
     let binding = file::input(7, 1);
 
     let mut count = 0;
+    let mut beams: HashSet<usize> = HashSet::new();
 
     for line in binding.lines() {
         dbg!(line);
 
         if let Some(start) = line.chars().position(|c| c == 'S') {
             count += 1;
+            beams.insert(start);
         } else {
-            let mut beams: HashSet<usize> = HashSet::new();
+            let mut new_beams: HashSet<usize> = HashSet::new();
             let splitters: Vec<usize> = line
                 .char_indices()
                 .filter_map(|(i, c)| (c == '^').then_some(i))
                 .collect();
             for s in splitters {
-                beams.insert(s - 1);
-                beams.insert(s + 1);
+                let (a,b) = (s-1, s+1);
+                if !beams.contains(&a) {
+                    new_beams.insert(a);
+                }
+                if !beams.contains(&b) {
+                    new_beams.insert(b);
+                }
             }
-            dbg!(&beams.iter().count());
-            count += beams.iter().count();
+            count += new_beams.iter().count();
+            beams = new_beams;
         }
     }
 
