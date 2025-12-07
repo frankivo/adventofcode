dae466d3b1a6b8db693971843b206ceb4cac4622
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-07 23:47:02 +0100

    start

--- advendofcode/2025/src/bin/aoc7.rs
+++ advendofcode/2025/src/bin/aoc7.rs
@@ -1,6 +1,20 @@
 use aoc::util::file;
+use std::collections::HashSet;
 
 fn part1() -> u64 {
+    let binding = file::input(7, 1);
+
+    let mut beams: HashSet<usize> = HashSet::new();
+
+    for line in binding.lines() {
+        dbg!(line);
+
+        if let Some(s) = line.chars().position(|c| c == 'S') {
+            beams.insert(s);
+        }
+    }
+
+    dbg!(beams);
     0
 }
 
