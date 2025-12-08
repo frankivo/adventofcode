7fef837753c47e784612b759228e63a897aa3e9f
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-08 23:27:52 +0100

    init part2

--- advendofcode/2025/src/bin/aoc7.rs
+++ advendofcode/2025/src/bin/aoc7.rs
@@ -1,23 +1,10 @@
 use aoc::util::file;
 use std::collections::HashSet;
 
-fn part1() -> u16 {
-    let binding = file::input(7, 1);
-
-    let start = HashSet::from([binding
-        .lines()
-        .next()
-        .unwrap()
-        .chars()
-        .position(|c| c == 'S')
-        .unwrap()]);
-
-    let lines = binding
-        .lines()
-        .enumerate()
-        .filter_map(|(i, l)| (i % 2 == 0).then_some(l));
-
+fn part1(lines: &Vec<&str>, start: usize) -> u16 {
+    let start = HashSet::from([start]);
     lines
+        .iter()
         .fold((start, 0), |(beams, split_count), line| {
             let splitters: Vec<_> = line
                 .char_indices()
@@ -36,11 +23,27 @@ fn part1() -> u16 {
         .1
 }
 
-fn part2() -> u64 {
+fn part2(lines: &Vec<&str>, start: usize) -> u16 {
     0
 }
 
 fn main() {
-    dbg!(part1());
-    dbg!(part2());
+    let binding = file::input(7, 1);
+
+    let lines: Vec<&str> = binding
+        .lines()
+        .enumerate()
+        .filter_map(|(i, l)| (i % 2 == 0).then_some(l))
+        .collect();
+
+    let start = binding
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .position(|c| c == 'S')
+        .unwrap();
+
+    dbg!(part1(&lines, start));
+    dbg!(part2(&lines, start));
 }
