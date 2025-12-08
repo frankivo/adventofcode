aac4c5c7952ae459a8188513e0876dcf038fe7d6
Frank Oosterhuis <frank+github@scriptzone.nl>
2025-12-08 12:03:09 +0100

    fold

--- advendofcode/2025/src/bin/aoc7.rs
+++ advendofcode/2025/src/bin/aoc7.rs
@@ -1,41 +1,44 @@
 use aoc::util::file;
 use std::collections::HashSet;
 
-fn part1() -> u64 {
+fn part1() -> u16 {
     let binding = file::input(7, 1);
-    let mut count = 0;
-    let mut beams_active: HashSet<usize> = HashSet::new();
+
+    let start = binding
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .position(|c| c == 'S')
+        .unwrap();
+    dbg!(start);
 
     let lines = binding
         .lines()
         .enumerate()
         .filter_map(|(i, l)| (i % 2 == 0).then_some(l));
 
-    for line in lines {
-        if let Some(start) = line.chars().position(|c| c == 'S') {
-            beams_active = HashSet::from([start]);
-            continue;
-        }
-
+    let count = lines.fold((HashSet::from([start]), 0), |(beams, count), line| {
         let splitters: Vec<_> = line
             .char_indices()
             .filter_map(|(i, c)| (c == '^').then_some(i))
             .collect();
 
+        let mut beams_active = beams.clone();
+        let mut tmp = 0;
+
         for s in &splitters {
             if beams_active.contains(&s) {
-                if beams_active.contains(&s) {
-                    count += 1;
-                }
-
+                tmp += 1;
                 beams_active.insert(s - 1);
                 beams_active.insert(s + 1);
                 beams_active.remove(&s);
             }
         }
-    }
-
-    count
+        dbg!(count);
+        (beams_active, count + tmp)
+    });
+    count.1
 }
 
 fn part2() -> u64 {
