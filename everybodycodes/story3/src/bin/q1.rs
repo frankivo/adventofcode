79bc57c6a55186f28962e6d32c9041504dd903cf
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2026-03-05 11:24:51 +0100

    functional

--- everybodycodes/story3/src/bin/q1.rs
+++ everybodycodes/story3/src/bin/q1.rs
@@ -11,20 +11,17 @@ fn col2bin(col: &str) -> i8 {
     i8::from_str_radix(&fixed, 2).unwrap()
 }
 
-fn main() {
-    let lines = file::input(1, 1);
-
-    let mut sum: u16 = 0;
-
-    for line in lines.lines() {
+fn part1() -> u16 {
+    file::input(1, 1).lines().flat_map(|line| {
         let id: u16 = line[..4].parse().unwrap();
         let red = col2bin(&line[5..11]);
         let green = col2bin(&line[12..18]);
         let blue = col2bin(&line[19..]);
 
-        if green > red && green > blue {
-            sum += id;
-        }
-    }
-    dbg!(sum);
+        (green > red && green > blue).then_some(id)
+    }).sum()
+}
+
+fn main() {
+    dbg!(part1());
 }
