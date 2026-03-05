c1df140839abd551d89d49ccb1c466a9c0002b71
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2026-03-05 11:17:59 +0100

    bin parse

--- everybodycodes/story3/src/bin/q1.rs
+++ everybodycodes/story3/src/bin/q1.rs
@@ -1,3 +1,31 @@
+use everybodycodes::util::file;
+
+fn col2bin(col: &str) -> i8 {
+    let fixed: String = col
+        .chars()
+        .map(|c| match c {
+            'G' | 'R' | 'B' => '1',
+            _ => '0',
+        })
+        .collect();
+    i8::from_str_radix(&fixed, 2).unwrap()
+}
+
 fn main() {
-    println!("My name is: {}", "Frank");
+    let lines = file::input(1, 1);
+
+    let mut sum: u16 = 0;
+
+    for line in lines.lines() {
+        let id: u16 = line[..4].parse().unwrap();
+        let red = col2bin(&line[5..11]);
+        let green = col2bin(&line[12..18]);
+        let blue = col2bin(&line[19..]);
+        dbg!(id, red, green, blue);
+
+        if green > red && green > blue {
+            sum += id;
+        }
+    }
+    dbg!(sum);
 }
