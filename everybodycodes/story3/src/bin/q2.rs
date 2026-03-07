23ae877235dfd9c4c8ae600633af6052d228cdf0
Frank Oosterhuis <frank@scriptzone.nl>
2026-03-07 20:43:54 +0100

    part 1 mutable

--- everybodycodes/story3/src/bin/q2.rs
+++ everybodycodes/story3/src/bin/q2.rs
@@ -1,22 +1,50 @@
 use everybodycodes::util::file;
+use std::collections::HashSet;
 
 fn find_pos(needle: char) -> Option<(usize, usize)> {
     file::input(2, 1)
         .lines()
         .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars()
-                .position(|c| c == needle)
-                .map(|x| (y, x))
-        })
+        .flat_map(|(y, line)| line.chars().position(|c| c == needle).map(|x| (y, x)))
         .nth(0)
 }
 
-fn part1() -> u32 {
-    let (source, bone) = (find_pos('@'), find_pos('#'));
-    dbg!(source, bone);
+fn part1() -> usize {
+    let (source, bone) = (find_pos('@').unwrap(), find_pos('#').unwrap());
 
-    0
+    let mut pos = source;
+    let mut direction = 'U';
+
+    let mut matrix: HashSet<(usize, usize)> = HashSet::from([source]);
+
+    loop {
+        let tmp_pos = match direction {
+            'U' => (pos.0 -1, pos.1),
+            'R' => (pos.0 , pos.1 + 1),
+            'D' => (pos.0 +1, pos.1),
+            'L' => (pos.0, pos.1 -1),
+            _ => unimplemented!(),
+        };
+
+         if tmp_pos == bone {
+            break;
+        }
+
+        if !matrix.contains(&tmp_pos) {
+            matrix.insert(tmp_pos);
+            pos = tmp_pos;
+        }
+
+        direction = match direction {
+            'U' => 'R',
+            'R' => 'D',
+            'D' => 'L',
+            'L' => 'U',
+            _ => unimplemented!(),
+        };
+    }
+
+    matrix.len()
 }
 
 fn main() {
